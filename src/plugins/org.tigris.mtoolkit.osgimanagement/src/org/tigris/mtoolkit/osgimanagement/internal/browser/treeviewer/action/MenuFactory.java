2691cfb497659d7748dd985cf13d2ec03847e640
yDelcheva <yDelcheva@8c0469b3-3b32-4850-87dc-fad729056409>
2009-04-30 15:12:47 +0000

    -update the structure of calls to InstallBundle() and installDp() with the new ones.

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/treeviewer/action/MenuFactory.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/treeviewer/action/MenuFactory.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.tigris.mtoolkit.osgimanagement.internal.browser.treeviewer.action;
 
+import java.io.File;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -35,221 +36,228 @@ import org.tigris.mtoolkit.osgimanagement.internal.browser.properties.ui.Install
 import org.tigris.mtoolkit.osgimanagement.internal.browser.properties.ui.PropertiesDialog;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.properties.ui.PropertySheet;
 
-
 public class MenuFactory {
 
-  public static void addFrameworkAction(TreeRoot treeRoot, TreeViewer parentView) {
-    String frameworkName = generateName(treeRoot);
-    FrameWork newFrameWork = new FrameWork(frameworkName, treeRoot, false);
-    PropertySheet sheet = new PropertySheet(parentView, newFrameWork, true);
-    sheet.open();
-  }
-  
-  // generates unique name for the new FrameWork
-  public static String generateName(TreeRoot treeRoot) {
-    HashMap frameWorkMap = treeRoot.getFrameWorkMap();
-    int index = 1;
-    String frameWorkName;
-    do {
-      frameWorkName = Messages.new_framework_default_name + '_' + index;
-      index++;
-    } while (frameWorkMap.containsKey(frameWorkName));
+	public static void addFrameworkAction(TreeRoot treeRoot, TreeViewer parentView) {
+		String frameworkName = generateName(treeRoot);
+		FrameWork newFrameWork = new FrameWork(frameworkName, treeRoot, false);
+		PropertySheet sheet = new PropertySheet(parentView, newFrameWork, true);
+		sheet.open();
+	}
 
-    return frameWorkName;
-  }
-  
-  public static void bundlePropertiesAction(Bundle bundle, TreeViewer parentView) {
-    try {
-      RemoteBundle rBundle = bundle.getRemoteBundle();
-      Shell shell = parentView.getTree().getShell();
-      PropertiesDialog propertiesDialog = new PropertiesDialog(shell, true);
-      Dictionary headers = rBundle.getHeaders(null);
-      propertiesDialog.open();
-      propertiesDialog.getMainControl().setData(headers);
-    } catch (IAgentException e) {
-      e.printStackTrace();
-      BrowserErrorHandler.processError(e, true);
-      return;
-    }
-  }
-  
-  public static void dpPropertiesAction(DeploymentPackage dp, TreeViewer parentView) {
-    try {
-      RemoteDP rdp = dp.getRemoteDP();
+	// generates unique name for the new FrameWork
+	public static String generateName(TreeRoot treeRoot) {
+		HashMap frameWorkMap = treeRoot.getFrameWorkMap();
+		int index = 1;
+		String frameWorkName;
+		do {
+			frameWorkName = Messages.new_framework_default_name + '_' + index;
+			index++;
+		} while (frameWorkMap.containsKey(frameWorkName));
 
-      Shell shell = parentView.getTree().getShell();
-      PropertiesDialog propertiesDialog = new PropertiesDialog(shell, false);
-      Dictionary headers = new Hashtable();
-      headers.put("DeploymentPackage-SymbolicName", rdp.getHeader("DeploymentPackage-SymbolicName")); //$NON-NLS-1$ //$NON-NLS-2$
-      headers.put("DeploymentPackage-Version", rdp.getHeader("DeploymentPackage-Version")); //$NON-NLS-1$ //$NON-NLS-2$
-      
-      String header = "DeploymentPackage-FixPack"; //$NON-NLS-1$
-      String value = rdp.getHeader(header);
-      if (value != null) headers.put(header, value);
-      
-      header = "DeploymentPackage-Copyright"; value = rdp.getHeader(header);  //$NON-NLS-1$
-      if (value != null) headers.put(header, value);
-      
-      header = "DeploymentPackage-ContactAddress"; value = rdp.getHeader(header);  //$NON-NLS-1$
-      if (value != null) headers.put(header, value);
-      
-      header = "DeploymentPackage-Description"; value = rdp.getHeader(header);  //$NON-NLS-1$
-      if (value != null) headers.put(header, value);
-      
-      header = "DeploymentPackage-DocURL"; value = rdp.getHeader(header);  //$NON-NLS-1$
-      if (value != null) headers.put(header, value);
-      
-      header = "DeploymentPackage-Vendor"; value = rdp.getHeader(header);  //$NON-NLS-1$
-      if (value != null) headers.put(header, value);
-      
-      header = "DeploymentPackage-License"; value = rdp.getHeader(header);  //$NON-NLS-1$
-      if (value != null) headers.put(header, value);
-      
-      header = "DeploymentPackage-Icon"; value = rdp.getHeader(header);  //$NON-NLS-1$
-      if (value != null) headers.put(header, value);
-      
-      propertiesDialog.open();
-      propertiesDialog.getMainControl().setData(headers);
-      
-    } catch (IAgentException e) {
-      BrowserErrorHandler.processError(e, true);
-      return;
-    }
-  }
-  
-  public static void deinstallBundleAction(Bundle bundle) {
-    FrameworkConnectorFactory.deinstallBundle(bundle);
-    
-    // Switch to console
-    if (FrameWorkView.getConsoleStatus()) {
-      IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
-      if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
-        ConsoleView.setActiveServer(bundle.findFramework().getName());
-      }
-    }
-  }
-  
-  public static void deinstallDPAction(DeploymentPackage dpNode) {
-    FrameworkConnectorFactory.deinstallDP(dpNode);
+		return frameWorkName;
+	}
 
-    // Switch to console
-    if (FrameWorkView.getConsoleStatus()) {
-      IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
-      if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
-        ConsoleView.setActiveServer(dpNode.findFramework().getName());
-      }
-    }  
-  }
-  
-  public static void installBundleAction(FrameWork framework, TreeViewer parentView) {
-    InstallDialog installDialog = new InstallDialog(parentView, InstallDialog.INSTALL_BUNDLE_TYPE);
-    installDialog.open();
-    final String result = installDialog.getResult();
-    if ((installDialog.getReturnCode() > 0) || (result == null) || result.trim().equals("")) { //$NON-NLS-1$
-      return;
-    }
-    
-    FrameworkConnectorFactory.installBundle(result, framework, false);
-    
-    // Switch to console
-    if (FrameWorkView.getConsoleStatus()) {
-      IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
-      if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
-        ConsoleView.setActiveServer(framework.getName());
-      }
-    }  
-  }
-  
-  public static void installDPAction(FrameWork framework, TreeViewer parentView) {
-    InstallDialog installDialog = new InstallDialog(parentView, InstallDialog.INSTALL_DP_TYPE);
-    installDialog.open();
-    final String result = installDialog.getResult();
-    if ((installDialog.getReturnCode() > 0) || (result == null) || result.trim().equals("")) { //$NON-NLS-1$
-      return;
-    }
+	public static void bundlePropertiesAction(Bundle bundle, TreeViewer parentView) {
+		try {
+			RemoteBundle rBundle = bundle.getRemoteBundle();
+			Shell shell = parentView.getTree().getShell();
+			PropertiesDialog propertiesDialog = new PropertiesDialog(shell, true);
+			Dictionary headers = rBundle.getHeaders(null);
+			propertiesDialog.open();
+			propertiesDialog.getMainControl().setData(headers);
+		} catch (IAgentException e) {
+			e.printStackTrace();
+			BrowserErrorHandler.processError(e, true);
+			return;
+		}
+	}
 
-    FrameworkConnectorFactory.installDP(result, framework);
-    
-    // Switch to console
-    if (FrameWorkView.getConsoleStatus()) {
-      IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
-      if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
-        ConsoleView.setActiveServer(framework.getName());
-      }
-    }
-  }
-  
-  public static void frameworkPropertiesAction(FrameWork framework, TreeViewer parentView) {
-    PropertySheet sheet = new PropertySheet(parentView, framework, false);
-    sheet.open();
-  }
-  
-  public static void removeFrameworkAction(FrameWork framework) {
-    if (framework.isConnected()) {
-      framework.disconnect();
-    }
+	public static void dpPropertiesAction(DeploymentPackage dp, TreeViewer parentView) {
+		try {
+			RemoteDP rdp = dp.getRemoteDP();
 
-    framework.dispose();
+			Shell shell = parentView.getTree().getShell();
+			PropertiesDialog propertiesDialog = new PropertiesDialog(shell, false);
+			Dictionary headers = new Hashtable();
+			headers.put("DeploymentPackage-SymbolicName", rdp.getHeader("DeploymentPackage-SymbolicName")); //$NON-NLS-1$ //$NON-NLS-2$
+			headers.put("DeploymentPackage-Version", rdp.getHeader("DeploymentPackage-Version")); //$NON-NLS-1$ //$NON-NLS-2$
 
-    if (FrameWorkView.getConsoleStatus()) {
-      ConsoleView.removeServerConsole(framework.getName());
-    }
-  }
+			String header = "DeploymentPackage-FixPack"; //$NON-NLS-1$
+			String value = rdp.getHeader(header);
+			if (value != null)
+				headers.put(header, value);
 
-  public static void startBundleAction(Bundle bundle) {
-    FrameworkConnectorFactory.startBundle(bundle);
-    // Switch to console
-    if (FrameWorkView.getConsoleStatus()) {
-      IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
-      if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
-        ConsoleView.setActiveServer(bundle.findFramework().getName());
-      }
-    }
-  }
-  
-  public static void stopBundleAction(Bundle bundle) {
-    FrameworkConnectorFactory.stopBundle(bundle);
-    // Switch to console
-    if (FrameWorkView.getConsoleStatus()) {
-      IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
-      if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
-        ConsoleView.setActiveServer(bundle.findFramework().getName());
-      }
-    }
-  }
-  
-  public static void updateBundleAction(final Bundle bundle, TreeViewer parentView) {
-    InstallDialog installDialog = new InstallDialog(parentView, InstallDialog.UPDATE_BUNDLE_TYPE);
-    installDialog.open();
-    final String result = installDialog.getResult();
-    if ((installDialog.getReturnCode() > 0) || (result == null) || result.trim().equals("")) { //$NON-NLS-1$
-      return;
-    }
-    FrameworkConnectorFactory.updateBundle(result, bundle);
-    
-    // Switch to console
-    if (FrameWorkView.getConsoleStatus()) {
-      IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
-      if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
-        ConsoleView.setActiveServer(bundle.findFramework().getName());
-      }
-    }
-  }
-  
-  public static void disconnectFrameworkAction(FrameWork fw) {
-    FrameworkConnectorFactory.disconnectFramework(fw);
-  }
-  
-  public static void connectFrameworkAction(FrameWork framework) {
-    FrameworkConnectorFactory.connectFrameWork(framework);
-  }
-  
-  public static void refreshFrameworkAction(FrameWork fw) {
-    fw.refreshAction();
-  }
-  
-  public static void refreshBundleAction(Bundle bundle) {
-    bundle.findFramework().refreshBundleAction(bundle);
-  }
-  
+			header = "DeploymentPackage-Copyright";value = rdp.getHeader(header); //$NON-NLS-1$
+			if (value != null)
+				headers.put(header, value);
+
+			header = "DeploymentPackage-ContactAddress";value = rdp.getHeader(header); //$NON-NLS-1$
+			if (value != null)
+				headers.put(header, value);
+
+			header = "DeploymentPackage-Description";value = rdp.getHeader(header); //$NON-NLS-1$
+			if (value != null)
+				headers.put(header, value);
+
+			header = "DeploymentPackage-DocURL";value = rdp.getHeader(header); //$NON-NLS-1$
+			if (value != null)
+				headers.put(header, value);
+
+			header = "DeploymentPackage-Vendor";value = rdp.getHeader(header); //$NON-NLS-1$
+			if (value != null)
+				headers.put(header, value);
+
+			header = "DeploymentPackage-License";value = rdp.getHeader(header); //$NON-NLS-1$
+			if (value != null)
+				headers.put(header, value);
+
+			header = "DeploymentPackage-Icon";value = rdp.getHeader(header); //$NON-NLS-1$
+			if (value != null)
+				headers.put(header, value);
+
+			propertiesDialog.open();
+			propertiesDialog.getMainControl().setData(headers);
+
+		} catch (IAgentException e) {
+			BrowserErrorHandler.processError(e, true);
+			return;
+		}
+	}
+
+	public static void deinstallBundleAction(Bundle bundle) {
+		FrameworkConnectorFactory.deinstallBundle(bundle);
+
+		// Switch to console
+		if (FrameWorkView.getConsoleStatus()) {
+			IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
+			if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
+				ConsoleView.setActiveServer(bundle.findFramework().getName());
+			}
+		}
+	}
+
+	public static void deinstallDPAction(DeploymentPackage dpNode) {
+		FrameworkConnectorFactory.deinstallDP(dpNode);
+
+		// Switch to console
+		if (FrameWorkView.getConsoleStatus()) {
+			IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
+			if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
+				ConsoleView.setActiveServer(dpNode.findFramework().getName());
+			}
+		}
+	}
+
+	public static void installBundleAction(FrameWork framework, TreeViewer parentView) {
+		InstallDialog installDialog = new InstallDialog(parentView, InstallDialog.INSTALL_BUNDLE_TYPE);
+		installDialog.open();
+		final String result = installDialog.getResult();
+		if ((installDialog.getReturnCode() > 0) || (result == null) || result.trim().equals("")) { //$NON-NLS-1$
+			return;
+		}
+
+		FrameworkConnectorFactory.installBundle(new File(result), framework);
+
+		// Switch to console
+		if (FrameWorkView.getConsoleStatus()) {
+			IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
+			if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
+				ConsoleView.setActiveServer(framework.getName());
+			}
+		}
+	}
+
+	public static void installDPAction(FrameWork framework, TreeViewer parentView) {
+		InstallDialog installDialog = new InstallDialog(parentView, InstallDialog.INSTALL_DP_TYPE);
+		installDialog.open();
+		final String result = installDialog.getResult();
+		if ((installDialog.getReturnCode() > 0) || (result == null) || result.trim().equals("")) { //$NON-NLS-1$
+			return;
+		}
+
+		FrameworkConnectorFactory.installDP(new File(result), framework);
+
+		// Switch to console
+		if (FrameWorkView.getConsoleStatus()) {
+			IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
+			if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
+				ConsoleView.setActiveServer(framework.getName());
+			}
+		}
+	}
+
+	public static void frameworkPropertiesAction(FrameWork framework, TreeViewer parentView) {
+		PropertySheet sheet = new PropertySheet(parentView, framework, false);
+		sheet.open();
+	}
+
+	public static void removeFrameworkAction(FrameWork framework) {
+		if (framework.isConnected()) {
+			framework.disconnect();
+		}
+
+		framework.dispose();
+
+		if (FrameWorkView.getConsoleStatus()) {
+			ConsoleView.removeServerConsole(framework.getName());
+		}
+	}
+
+	public static void startBundleAction(Bundle bundle) {
+		FrameworkConnectorFactory.startBundle(bundle);
+		// Switch to console
+		if (FrameWorkView.getConsoleStatus()) {
+			IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
+			if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
+				ConsoleView.setActiveServer(bundle.findFramework().getName());
+			}
+		}
+	}
+
+	public static void stopBundleAction(Bundle bundle) {
+		FrameworkConnectorFactory.stopBundle(bundle);
+		// Switch to console
+		if (FrameWorkView.getConsoleStatus()) {
+			IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
+			if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
+				ConsoleView.setActiveServer(bundle.findFramework().getName());
+			}
+		}
+	}
+
+	public static void updateBundleAction(final Bundle bundle, TreeViewer parentView) {
+		InstallDialog installDialog = new InstallDialog(parentView, InstallDialog.UPDATE_BUNDLE_TYPE);
+		installDialog.open();
+		final String result = installDialog.getResult();
+		if ((installDialog.getReturnCode() > 0) || (result == null) || result.trim().equals("")) { //$NON-NLS-1$
+			return;
+		}
+		FrameworkConnectorFactory.updateBundle(result, bundle);
+
+		// Switch to console
+		if (FrameWorkView.getConsoleStatus()) {
+			IPreferenceStore store = FrameworkPlugin.getDefault().getPreferenceStore();
+			if (store.getBoolean(StoreConstants.SWITCH_FOCUS_KEY)) {
+				ConsoleView.setActiveServer(bundle.findFramework().getName());
+			}
+		}
+	}
+
+	public static void disconnectFrameworkAction(FrameWork fw) {
+		FrameworkConnectorFactory.disconnectFramework(fw);
+	}
+
+	public static void connectFrameworkAction(FrameWork framework) {
+		FrameworkConnectorFactory.connectFrameWork(framework);
+	}
+
+	public static void refreshFrameworkAction(FrameWork fw) {
+		fw.refreshAction();
+	}
+
+	public static void refreshBundleAction(Bundle bundle) {
+		bundle.findFramework().refreshBundleAction(bundle);
+	}
+
 }
