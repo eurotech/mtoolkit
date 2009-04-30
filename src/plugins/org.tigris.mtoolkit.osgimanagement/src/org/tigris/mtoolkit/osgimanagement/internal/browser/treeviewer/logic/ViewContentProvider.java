c8103c52a1fdf61a6e1b3e6c72e341e94e1c30c6
yDelcheva <yDelcheva@8c0469b3-3b32-4850-87dc-fad729056409>
2009-04-30 15:16:08 +0000

    - add Implementation of the new method elementChanged(), defined in ConntenChangeListener: if some element is changed the viewers are updated properly

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/treeviewer/logic/ViewContentProvider.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/treeviewer/logic/ViewContentProvider.java
@@ -20,77 +20,99 @@ import org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeLi
 import org.tigris.mtoolkit.osgimanagement.internal.browser.model.Model;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.model.TreeRoot;
 
-
 public class ViewContentProvider implements ITreeContentProvider, ContentChangeListener, ConstantsDistributor {
 
-  private TreeViewer viewer;
-  private boolean canUpdate;
-  private Display display;
+	private TreeViewer viewer;
+	private boolean canUpdate;
+	private Display display;
 
-  public ViewContentProvider() {
-    canUpdate = true;
-  }
+	public ViewContentProvider() {
+		canUpdate = true;
+	}
 
-  // Returns the child elements of the given parent element.
-  public Object[] getChildren(Object parentElement) {
-    return ((Model)parentElement).getChildren();
-  }
-  
-  // Returns the parent for the given element, or null indicating that the parent can't be computed.
-  public Object getParent(Object element) {
-    return ((Model)element).getParent();
-  }
+	// Returns the child elements of the given parent element.
+	public Object[] getChildren(Object parentElement) {
+		return ((Model) parentElement).getChildren();
+	}
 
-  // Returns whether the given element has children. 
-  public boolean hasChildren(Object element) {
-    return ((Model)element).getSize() > 0;
-  }
+	// Returns the parent for the given element, or null indicating that the
+	// parent can't be computed.
+	public Object getParent(Object element) {
+		return ((Model) element).getParent();
+	}
 
-  // Returns the elements to display in the viewer when its input is set to the given element.
-  public Object[] getElements(Object inputElement) {
-    return getChildren(inputElement);
-  }
-  
-  // Disposes of this content provider. 
-  public void dispose() {
-    canUpdate = false;
-  }
+	// Returns whether the given element has children.
+	public boolean hasChildren(Object element) {
+		return ((Model) element).getSize() > 0;
+	}
 
-  // Notifies this content provider that the given viewer's input has been switched to a different element.
-  public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-    this.viewer = (TreeViewer)viewer;
+	// Returns the elements to display in the viewer when its input is set to
+	// the given element.
+	public Object[] getElements(Object inputElement) {
+		return getChildren(inputElement);
+	}
 
-    if(oldInput != null) {
-      ((TreeRoot)oldInput).removeListener(this);
-    }
+	// Disposes of this content provider.
+	public void dispose() {
+		canUpdate = false;
+	}
 
-    if(newInput != null) {
-      ((TreeRoot)newInput).addListener(this);
-    }
-  }
+	// Notifies this content provider that the given viewer's input has been
+	// switched to a different element.
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		this.viewer = (TreeViewer) viewer;
 
-  public void elementAdded(final ContentChangeEvent event) {
-    if (canUpdate) {
-      display = Display.getCurrent();
-      if (display == null) display = Display.getDefault();
-      display.asyncExec(new Runnable() {
-        public void run() {
-          viewer.add(event.getTarget().getParent(), event.getTarget());
-        }
-      });
-    }
-  }
+		if (oldInput != null) {
+			((TreeRoot) oldInput).removeListener(this);
+		}
 
-  public void elementRemoved(ContentChangeEvent event) {
-    if (canUpdate) {
-      final Model target = event.getTarget();
-      display = Display.getCurrent();
-      if (display == null) display = Display.getDefault();
-      display.asyncExec(new Runnable() {
-        public void run() {
-          viewer.remove(target);
-        }
-      });
-    }
-  }
+		if (newInput != null) {
+			((TreeRoot) newInput).addListener(this);
+		}
+	}
+
+	public void elementAdded(final ContentChangeEvent event) {
+		if (canUpdate) {
+			display = Display.getCurrent();
+			if (display == null)
+				display = Display.getDefault();
+			display.asyncExec(new Runnable() {
+				public void run() {
+					viewer.add(event.getTarget().getParent(), event.getTarget());
+				}
+			});
+		}
+	}
+
+	public void elementChanged(final ContentChangeEvent event) {
+		if (canUpdate) {
+			display = Display.getCurrent();
+			if (display == null)
+				display = Display.getDefault();
+			display.asyncExec(new Runnable() {
+				public void run() {
+					// is this really needed: ?
+					viewer.update(event.getTarget(), null);
+					if (event.getTarget().getParent() != null) {
+						viewer.refresh(event.getTarget().getParent());
+					}
+					viewer.refresh(event.getTarget());
+				}
+			});
+		}
+	}
+
+	public void elementRemoved(ContentChangeEvent event) {
+		if (canUpdate) {
+			final Model target = event.getTarget();
+			display = Display.getCurrent();
+			if (display == null)
+				display = Display.getDefault();
+			display.asyncExec(new Runnable() {
+				public void run() {
+					viewer.remove(target);
+				}
+			});
+		}
+	}
 }
