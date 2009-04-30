897c65215d06ed491de9d486e5f5885c7478b8bb
yDelcheva <yDelcheva@8c0469b3-3b32-4850-87dc-fad729056409>
2009-04-30 15:39:51 +0000

    -add some methods for handle errors and for creating an IStatus Objects.

--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/UtilitiesPlugin.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/UtilitiesPlugin.java
@@ -10,46 +10,56 @@
  *******************************************************************************/
 package org.tigris.mtoolkit.common;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 public class UtilitiesPlugin extends AbstractUIPlugin {
- 
-  public static final String PLUGIN_ID = "org.tigris.mtoolkit.common"; //$NON-NLS-1$
-    
-  private static UtilitiesPlugin inst;
-   
-  /**
-   * Creates the Utilities plugin and caches its default instance
-   *
-   * @param descriptor  the plugin descriptor which the receiver is made from
-   */  
-  public UtilitiesPlugin() {
-    super();
-    if (inst == null) inst = this;
-  }
-    
-  public String getId() {
-    return PLUGIN_ID;
-  }
-    
-  /**
-   * Gets the plugin singleton.
-   *
-   * @return the default UtilitiesPlugin instance
-   */  
-  public static UtilitiesPlugin getDefault() {
-    return inst;
-  }
-    
-  public IWorkbenchPage getActivePage() {
+
+	public static final String PLUGIN_ID = "org.tigris.mtoolkit.common"; //$NON-NLS-1$
+
+	private static UtilitiesPlugin inst;
+
+	/**
+	 * Creates the Utilities plugin and caches its default instance
+	 * 
+	 * @param descriptor
+	 *            the plugin descriptor which the receiver is made from
+	 */
+	public UtilitiesPlugin() {
+		super();
+		if (inst == null)
+			inst = this;
+	}
+
+	public String getId() {
+		return PLUGIN_ID;
+	}
+
+	/**
+	 * Gets the plugin singleton.
+	 * 
+	 * @return the default UtilitiesPlugin instance
+	 */
+	public static UtilitiesPlugin getDefault() {
+		return inst;
+	}
+
+	public IWorkbenchPage getActivePage() {
 		IWorkbenchWindow window = getWorkbench().getActiveWorkbenchWindow();
 		if (window == null)
 			return null;
 		return getWorkbench().getActiveWorkbenchWindow().getActivePage();
 	}
-  
- 
+
+	public static IStatus newStatus(int severity, String message, Throwable t) {
+		return new Status(severity, PLUGIN_ID, message, t);
+	}
+
+	public static void error(String message, Throwable t) {
+		getDefault().getLog().log(newStatus(IStatus.ERROR, message, t));
+	}
 
 }
