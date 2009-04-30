62ee64ac1f67156911a734b6308c0382d52cc256
yDelcheva <yDelcheva@8c0469b3-3b32-4850-87dc-fad729056409>
2009-04-30 14:55:22 +0000

    -an abstract class that handles all operations under bundles( install, uninstall, start, stop, update) and execute the righ option according to user desire

---
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/logic/RemoteBundleOperation.java
@@ -0,0 +1,55 @@
+package org.tigris.mtoolkit.osgimanagement.internal.browser.logic;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.ui.statushandlers.StatusManager;
+import org.tigris.mtoolkit.iagent.IAgentException;
+import org.tigris.mtoolkit.osgimanagement.internal.FrameworkPlugin;
+import org.tigris.mtoolkit.osgimanagement.internal.browser.model.Bundle;
+
+public abstract class RemoteBundleOperation extends Job {
+
+	private Bundle bundle;
+
+	public RemoteBundleOperation(String taskName, Bundle bundle) {
+		super(taskName);
+		setUser(true);
+		this.bundle = bundle;
+	}
+
+	protected IStatus run(IProgressMonitor monitor) {
+		monitor.beginTask(getName(), 1);
+		IStatus operationResult = Status.OK_STATUS;
+		try {
+			monitor.beginTask(getName(), 1);
+			operationResult = doOperation(monitor);
+		} catch (IAgentException e) {
+			// refresh the bundle state
+			if (getBundle() != null)
+				getBundle().refreshStateFromRemote();
+			operationResult = handleException(e);
+		} finally {
+			if (bundle != null)
+				bundle.updateElement();
+			monitor.done();
+		}
+		if (!operationResult.isOK())
+			StatusManager.getManager().handle(FrameworkPlugin.newStatus(getMessage(operationResult), operationResult),
+				StatusManager.SHOW | StatusManager.LOG);
+		return monitor.isCanceled() ? Status.CANCEL_STATUS : Status.OK_STATUS;
+	}
+
+	protected Bundle getBundle() {
+		return bundle;
+	}
+
+	protected abstract IStatus doOperation(IProgressMonitor monitor) throws IAgentException;
+
+	protected IStatus handleException(IAgentException e) {
+		return FrameworkPlugin.handleIAgentException(e);
+	}
+
+	protected abstract String getMessage(IStatus operationStatus);
+}
