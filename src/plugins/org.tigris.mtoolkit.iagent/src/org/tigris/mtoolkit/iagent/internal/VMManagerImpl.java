15473522aceb2c63d5d89afa84c94938bc978e8e
yDelcheva <yDelcheva@8c0469b3-3b32-4850-87dc-fad729056409>
2009-04-30 15:49:31 +0000

    -implements the getFrameworkStartLevel() method.

--- src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/internal/VMManagerImpl.java
+++ src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/internal/VMManagerImpl.java
@@ -20,91 +20,101 @@ import org.tigris.mtoolkit.iagent.internal.connection.PMPConnection;
 import org.tigris.mtoolkit.iagent.internal.utils.DebugUtils;
 import org.tigris.mtoolkit.iagent.pmp.RemoteObject;
 
-
 /**
  * Implementation of VMManager
- *
+ * 
  */
 public class VMManagerImpl implements VMManager {
 
-  private DeviceConnectorImpl connector;
-  
-  private RemoteObject lastKnownRemoteConsole;
+	private DeviceConnectorImpl connector;
 
-  /**
-   * Creates new runtime commands with specified transport
-   * @param aTransport
-   */
-  public VMManagerImpl(DeviceConnectorImpl connector) {
-    if (connector == null)
-      throw new IllegalArgumentException();
-    this.connector = connector;
-  }
+	private RemoteObject lastKnownRemoteConsole;
 
-  public boolean isVMActive() throws IAgentException {
-    return isVMConnectable();
-  }
+	/**
+	 * Creates new runtime commands with specified transport
+	 * 
+	 * @param aTransport
+	 */
+	public VMManagerImpl(DeviceConnectorImpl connector) {
+		if (connector == null)
+			throw new IllegalArgumentException();
+		this.connector = connector;
+	}
 
-  public PMPConnection getPMPConnection() throws IAgentException {
-    log("[getPMPConnection] >>>");
-    if (!connector.isActive()) {
-      log("[getPMPConnection] DeviceConnector is closed");
-      throw new IAgentException("Associated DeviceConnector is closed", IAgentErrors.ERROR_DISCONNECTED);
-    }
-    ConnectionManager connManager = connector.getConnectionManager();
-    PMPConnection pmpConnection = (PMPConnection) connManager.getActiveConnection(ConnectionManager.PMP_CONNECTION);
-    if (pmpConnection == null) {
-      log("[getPMPConnection] No PMP active connection. Creating new one...");
-      pmpConnection = (PMPConnection) connManager.createConnection(ConnectionManager.PMP_CONNECTION);
-    }
-    log("[getPMPConnection] connection: " + pmpConnection);
-    return pmpConnection;
-  }
+	public boolean isVMActive() throws IAgentException {
+		return isVMConnectable();
+	}
 
-  public void executeFrameworkCommand(String command) throws IAgentException {
-    log("[executeFrameworkCommand] >>> command: " + command);
-    Utils.callRemoteMethod(getPMPConnection().getRemoteParserService(), Utils.EXECUTE_METHOD, new Object[] { command });
-  }
+	public PMPConnection getPMPConnection() throws IAgentException {
+		log("[getPMPConnection] >>>");
+		if (!connector.isActive()) {
+			log("[getPMPConnection] DeviceConnector is closed");
+			throw new IAgentException("Associated DeviceConnector is closed", IAgentErrors.ERROR_DISCONNECTED);
+		}
+		ConnectionManager connManager = connector.getConnectionManager();
+		PMPConnection pmpConnection = (PMPConnection) connManager.getActiveConnection(ConnectionManager.PMP_CONNECTION);
+		if (pmpConnection == null) {
+			log("[getPMPConnection] No PMP active connection. Creating new one...");
+			pmpConnection = (PMPConnection) connManager.createConnection(ConnectionManager.PMP_CONNECTION);
+		}
+		log("[getPMPConnection] connection: " + pmpConnection);
+		return pmpConnection;
+	}
 
-  public void redirectFrameworkOutput(OutputStream os) throws IAgentException {
-    log("[redirectFrameworkOutput] >>> os: " + os);
-    PMPConnection connection = getPMPConnection();
-    if (os != null) {
-      RemoteObject parser = connection.getRemoteParserService();
-      Utils.callRemoteMethod(parser, Utils.REGISTER_METHOD, new Object[] { os });
-      if (lastKnownRemoteConsole != parser) {
-        lastKnownRemoteConsole = parser;
-      }
-    } else {
-      connection.releaseRemoteParserService();
-    }
-  }
+	public void executeFrameworkCommand(String command) throws IAgentException {
+		log("[executeFrameworkCommand] >>> command: " + command);
+		Utils.callRemoteMethod(getPMPConnection().getRemoteParserService(),
+			Utils.EXECUTE_METHOD,
+			new Object[] { command });
+	}
 
-  public boolean isVMConnectable() throws IAgentException {
-    if (!connector.isActive()) {
-      log("[getPMPConnection] DeviceConnector is closed");
-      throw new IAgentException("Associated DeviceConnector is closed", IAgentErrors.ERROR_DISCONNECTED);
-    }
-    try {
-      PMPConnection connection = getPMPConnection();
-      if(connection == null || !connection.isConnected()){
-        log("[isVMConnectable] VM is not connectable");
-        return false;
-      } else {
-        log("[isVMConnectable] VM is connectable");
-        return true;
-      }
-    } catch (IAgentException e) {
-      log("[isVMConnectable] VM is not connectable",e);
-      return false;
-    }
-  }
+	public void redirectFrameworkOutput(OutputStream os) throws IAgentException {
+		log("[redirectFrameworkOutput] >>> os: " + os);
+		PMPConnection connection = getPMPConnection();
+		if (os != null) {
+			RemoteObject parser = connection.getRemoteParserService();
+			Utils.callRemoteMethod(parser, Utils.REGISTER_METHOD, new Object[] { os });
+			if (lastKnownRemoteConsole != parser) {
+				lastKnownRemoteConsole = parser;
+			}
+		} else {
+			connection.releaseRemoteParserService();
+		}
+	}
 
-  private final void log(String message) {
-    log(message, null);
-  }
+	public boolean isVMConnectable() throws IAgentException {
+		if (!connector.isActive()) {
+			log("[getPMPConnection] DeviceConnector is closed");
+			throw new IAgentException("Associated DeviceConnector is closed", IAgentErrors.ERROR_DISCONNECTED);
+		}
+		try {
+			PMPConnection connection = getPMPConnection();
+			if (connection == null || !connection.isConnected()) {
+				log("[isVMConnectable] VM is not connectable");
+				return false;
+			} else {
+				log("[isVMConnectable] VM is connectable");
+				return true;
+			}
+		} catch (IAgentException e) {
+			log("[isVMConnectable] VM is not connectable", e);
+			return false;
+		}
+	}
 
-  private final void log(String message, Throwable e) {
-    DebugUtils.log(this, message, e);
-  }
+	private final void log(String message) {
+		log(message, null);
+	}
+
+	private final void log(String message, Throwable e) {
+		DebugUtils.log(this, message, e);
+	}
+
+	public int getFrameworkStartLevel() throws IAgentException {
+		log("[getFrameworkStartLevel] >>> ");
+		Integer fwStartLevel = (Integer) Utils.callRemoteMethod(getPMPConnection().getRemoteBundleAdmin(),
+			Utils.GET_FW_START_LEVEL,
+			new Object[0]);
+		return fwStartLevel.intValue();
+	}
 }
