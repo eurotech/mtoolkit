83992a65b49722a86ffc20cc15d0516e82c20028
yDelcheva <yDelcheva@8c0469b3-3b32-4850-87dc-fad729056409>
2009-04-30 15:07:04 +0000

    - Re-implements the way elements are added and removed in such way that the bundles are separate in special case -Replace method UpdateViewers() with updateElement(), the new method send events to allRegistered classes and they take care for the update according to the real type of the element

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/model/Model.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/model/Model.java
@@ -12,180 +12,176 @@ package org.tigris.mtoolkit.osgimanagement.internal.browser.model;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.TreeSet;
 
-import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IActionFilter;
-import org.tigris.mtoolkit.osgimanagement.internal.FrameWorkView;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ConstantsDistributor;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeEvent;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeListener;
 
-
-public abstract class Model implements Comparable, IActionFilter,
-                                           ConstantsDistributor {
+public abstract class Model implements Comparable, IActionFilter, ConstantsDistributor {
 
 	protected String name;
-  protected Model parent;
-  private Display display;
-  protected TreeSet elementList;
+	protected Model parent;
+	protected Set elementList;
 
-  
-  public Model(String name, Model parent) {
-    this.name = name;
-    this.parent = parent;
-    elementList = new TreeSet();
-  }
+	public Model(String name, Model parent) {
+		this.name = name;
+		this.parent = parent;
+		elementList = new TreeSet();
+	}
 
-  public void addElement(Model element) {
-    if (elementList.add(element)) {
-      fireElementAdded(element);
-    }
-  }
+	public void addElement(Model element) {
+		if (element instanceof Bundle) {
+			if (elementList.add((Bundle) element)) {
+				fireElementAdded(element);
+			}
+		} else if (elementList.add(element))
+			fireElementAdded(element);
+	}
 
+	public void removeElement(Model element) {
+		if (element instanceof Bundle) {
+			if (elementList.remove((Bundle) element)) {
+				fireElementRemoved((Bundle) element);
+			}
+		} else if (elementList.remove(element)) {
+			fireElementRemoved(element);
+		}
+	}
 
-  public void removeElement(Model element) {
-    if (elementList.remove(element)) {
-      fireElementRemoved(element);
-    }
-  }
+	public Model[] getChildren() {
+		if (elementList == null) {
+			return new Model[0];
+		}
+		Model[] resultArray = new Model[elementList.size()];
+		elementList.toArray(resultArray);
+		return resultArray;
+	}
 
+	public int getSize() {
+		if (elementList == null) {
+			return 0;
+		}
+		return elementList.size();
+	}
 
-  public Model[] getChildren() {
-    if (elementList == null) {
-      return new Model[0];
-    }
-    Model[] resultArray = new Model[elementList.size()];
-    elementList.toArray(resultArray);
-    return resultArray;
-  }
+	protected ArrayList getListeners() {
+		return parent.getListeners();
+	}
 
+	protected void fireElementAdded(Model target) {
+		ArrayList listeners = this.getListeners();
+		if (listeners == null) {
+			return;
+		}
 
-  public int getSize() {
-    if (elementList == null) {
-      return 0;
-    }
-    return elementList.size();
-  }
- 
-  protected ArrayList getListeners() {
-    return parent.getListeners();
-  }
-
-  protected void fireElementAdded(Model target) {
-    ArrayList listeners = this.getListeners();
-    if (listeners == null) {
-      return;
-    }
-
-    Iterator iter = listeners.iterator();
-    while (iter.hasNext()) {
-			ContentChangeListener listener = (ContentChangeListener)iter.next();
-      ContentChangeEvent event = new ContentChangeEvent(target);
+		Iterator iter = listeners.iterator();
+		while (iter.hasNext()) {
+			ContentChangeListener listener = (ContentChangeListener) iter.next();
+			ContentChangeEvent event = new ContentChangeEvent(target);
 			listener.elementAdded(event);
 		}
-  }
+	}
 
-  protected void fireElementRemoved(Model target) {
-    ArrayList listeners = this.getListeners();
-    if (listeners == null) {
-      return;
-    }
+	protected void fireElementChanged(Model target) {
+		ArrayList listeners = this.getListeners();
+		if (listeners == null) {
+			return;
+		}
 
-    Iterator iter = listeners.iterator();
-    while (iter.hasNext()) {
-      ContentChangeListener listener = (ContentChangeListener)iter.next();
-      ContentChangeEvent event = new ContentChangeEvent(target);
-      listener.elementRemoved(event);
-    }
-  }
+		Iterator iter = listeners.iterator();
+		while (iter.hasNext()) {
+			ContentChangeListener listener = (ContentChangeListener) iter.next();
+			ContentChangeEvent event = new ContentChangeEvent(target);
+			listener.elementChanged(event);
+		}
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	protected void fireElementRemoved(Model target) {
+		ArrayList listeners = this.getListeners();
+		if (listeners == null) {
+			return;
+		}
 
-  public String getName() {
-    return name;
-  }
+		Iterator iter = listeners.iterator();
+		while (iter.hasNext()) {
+			ContentChangeListener listener = (ContentChangeListener) iter.next();
+			ContentChangeEvent event = new ContentChangeEvent(target);
+			listener.elementRemoved(event);
+		}
+	}
 
-  public Model getParent() {
-    return parent;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public String toString() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public int compareTo(Object obj) {
-    return this.toString().compareTo(obj.toString());
-  }
+	public Model getParent() {
+		return parent;
+	}
 
-  public void removeChildren() {
-    if (getSize() < 1) return;
-    Model[] elements = getChildren();
-    for (int i=0; i < elements.length; i++) {
-      removeElement(elements[i]);
-    }
-  }
+	public String toString() {
+		return name;
+	}
 
+	public int compareTo(Object obj) {
+		return this.toString().compareTo(obj.toString());
+	}
 
-  public boolean testAttribute(Object target, String name, String value) {
-    if (!(target instanceof Model)) {
-      return false;
-    }
+	public void removeChildren() {
+		if (getSize() < 1)
+			return;
+		Model[] elements = getChildren();
+		for (int i = 0; i < elements.length; i++) {
+			removeElement(elements[i]);
+		}
+	}
 
-    if (name.equalsIgnoreCase(NODE_NAME)) {
-      if (value.equalsIgnoreCase(this.name)) {
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  public FrameWork findFramework() {
-    FrameWork fw = null;
-    Model model = this;
-    while (model != null && !(model instanceof FrameWork)) {
-      model = model.getParent();
-    }
-    if (model != null) {
-      fw = (FrameWork) model;
-    }
-    
-    return fw;
-  }
-  
-  public void updateViewers() {
-    display = Display.getCurrent();
-    if (display == null) display = Display.getDefault();
-    display.asyncExec(new Runnable() {
-      public void run() {
-        TreeViewer[] viewers = FrameWorkView.getTreeViewers();
-        if (viewers != null) {
-          for (int i=0; i < viewers.length; i++) {
-            viewers[i].update(Model.this, null);
-            if (getParent() != null) {
-              viewers[i].refresh(getParent());
-              viewers[i].refresh(Model.this);
-            } else {
-              viewers[i].refresh(Model.this);
-            }
-          }
-        }
-      }
-    });
-  }
-  
-  public int indexOf(Model child) {
-    Iterator iterator = elementList.iterator();
-    int index = 0;
-    while (iterator.hasNext()) {
-      Model node = (Model) iterator.next();
-      if (node == child) return index;
-      index++;
-    }
-    return -1;
-  }
+	public boolean testAttribute(Object target, String name, String value) {
+		if (!(target instanceof Model)) {
+			return false;
+		}
+
+		if (name.equalsIgnoreCase(NODE_NAME)) {
+			if (value.equalsIgnoreCase(this.name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public FrameWork findFramework() {
+		FrameWork fw = null;
+		Model model = this;
+		while (model != null && !(model instanceof FrameWork)) {
+			model = model.getParent();
+		}
+		if (model != null) {
+			fw = (FrameWork) model;
+		}
+
+		return fw;
+	}
+
+	public void updateElement() {
+		fireElementChanged(this);
+	}
+
+	public int indexOf(Model child) {
+		Iterator iterator = elementList.iterator();
+		int index = 0;
+		while (iterator.hasNext()) {
+			Model node = (Model) iterator.next();
+			if (node == child)
+				return index;
+			index++;
+		}
+		return -1;
+	}
 
 }
