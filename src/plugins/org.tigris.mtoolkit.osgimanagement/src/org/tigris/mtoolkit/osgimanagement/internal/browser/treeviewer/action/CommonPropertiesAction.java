f61e76e9002a6afd590132537acf713280108859
yDelcheva <yDelcheva@8c0469b3-3b32-4850-87dc-fad729056409>
2009-05-25 15:15:33 +0000

    - create a new class that represents an action. This action provides common way for showing a model property(the model should be instance of Framework, deployment package or bundle)

---
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/treeviewer/action/CommonPropertiesAction.java
@@ -0,0 +1,63 @@
+package org.tigris.mtoolkit.osgimanagement.internal.browser.treeviewer.action;
+
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.ui.actions.SelectionProviderAction;
+import org.tigris.mtoolkit.osgimanagement.internal.Messages;
+import org.tigris.mtoolkit.osgimanagement.internal.browser.model.Bundle;
+import org.tigris.mtoolkit.osgimanagement.internal.browser.model.DeploymentPackage;
+import org.tigris.mtoolkit.osgimanagement.internal.browser.model.FrameWork;
+import org.tigris.mtoolkit.osgimanagement.internal.browser.model.ObjectClass;
+
+public class CommonPropertiesAction extends SelectionProviderAction {
+
+	private TreeViewer parentView;
+	private SelectionProviderAction action;
+
+	public CommonPropertiesAction(ISelectionProvider provider, String text) {
+		super(provider, text);
+		this.parentView = (TreeViewer) provider;
+		this.setEnabled(false);
+		this.setText(text + "@Ctrl+Enter");
+	}
+
+	public void run() {
+		IStructuredSelection selection = getStructuredSelection();
+
+		Object element = selection.getFirstElement();
+
+		if (element instanceof ObjectClass)
+			action = new ServicePropertiesAction(parentView, Messages.property_action_label);
+		else if (element instanceof Bundle)
+			action = new BundlePropertiesAction(parentView, Messages.property_action_label);
+		else if (element instanceof DeploymentPackage)
+			action = new DPPropertiesAction(parentView, Messages.property_action_label);
+		else if (element instanceof FrameWork)
+			action = new PropertyAction(parentView, Messages.property_action_label);
+		else
+			return;
+		action.run();
+	}
+
+	public void selectionChanged(IStructuredSelection selection) {
+		updateState(selection);
+	}
+
+	public void updateState(IStructuredSelection selection) {
+		if (selection.size() != 1) {
+			this.setEnabled(false);
+		} else {
+			Object element = selection.getFirstElement();
+			if ((element instanceof ObjectClass)
+							|| (element instanceof Bundle)
+							|| (element instanceof DeploymentPackage)
+							|| (element instanceof FrameWork)) {
+				this.setEnabled(true);
+			} else {
+				this.setEnabled(false);
+			}
+		}
+
+	}
+}
