af8c8b109e7f383c69a274ec5d9f966dd333289f
yDelcheva <yDelcheva@8c0469b3-3b32-4850-87dc-fad729056409>
2009-05-25 15:58:01 +0000

    -replace the call of some methods with the newly added actions because of the toolbar, -add images for some of the menu items

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/console/ConsolePopupMenu.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/console/ConsolePopupMenu.java
@@ -10,24 +10,20 @@
  *******************************************************************************/
 package org.tigris.mtoolkit.osgimanagement.internal.console;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.events.MenuEvent;
 import org.eclipse.swt.events.MenuListener;
-import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.texteditor.ITextEditorActionConstants;
-import org.tigris.mtoolkit.osgimanagement.internal.FrameworkPlugin;
 import org.tigris.mtoolkit.osgimanagement.internal.Messages;
+import org.tigris.mtoolkit.osgimanagement.internal.browser.treeviewer.action.SaveAsConsoleAction;
+import org.tigris.mtoolkit.osgimanagement.internal.images.ImageHolder;
 
 public class ConsolePopupMenu implements MenuListener, MenuItemListener {
 
@@ -46,31 +42,46 @@ public class ConsolePopupMenu implements MenuListener, MenuItemListener {
 	private MenuItem printItem;
 	private Console console;
 	private Clipboard cb;
-	private FileDialog fdialog;
-	private MessageBox mbox;
 
 	public ConsolePopupMenu(Console cons, IActionBars actionBars) {
 		console = cons;
 		popupMenu = new MenuManager(Messages.ConsoleM_N);
 		editMenu = new MenuManager(Messages.edit_action_N);
 		popupMenu.add(editMenu);
+
 		stateType = new MenuItem(Messages.edit_action_mark_N, MenuItem.AS_CHECK_BOX);
+		stateType.setChecked(console.getState() == Console.COMMAND_MODE);
 		editMenu.add(stateType);
 		editMenu.add(new Separator());
+
 		cutItem = new MenuItem(Messages.edit_action_cut_N);
+		cutItem.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_CUT));
 		editMenu.add(cutItem);
+
 		copyItem = new MenuItem(Messages.edit_action_copy_N);
+		copyItem.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_COPY));
 		editMenu.add(copyItem);
+
 		pasteItem = new MenuItem(Messages.edit_action_paste_N);
+		pasteItem.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_PASTE));
 		editMenu.add(pasteItem);
+
 		deleteItem = new MenuItem(Messages.edit_action_delete_N);
+		deleteItem.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_DELETE));
 		editMenu.add(deleteItem);
+
 		clearItem = new MenuItem(Messages.clear_action_N);
+		clearItem.setImageDescriptor(ImageHolder.getImageDescriptor("clear_action.gif"));
 		popupMenu.add(clearItem);
+
 		saveasItem = new MenuItem(Messages.file_action_saveas_N);
+		saveasItem.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_ETOOL_SAVEAS_EDIT));
 		popupMenu.add(saveasItem);
+
 		printItem = new MenuItem(Messages.file_action_print_N);
+		printItem.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_ETOOL_PRINT_EDIT));
 		popupMenu.add(printItem);
+
 		popupMenu.add(new Separator());
 		mainMenu = popupMenu.createContextMenu(cons);
 		mainMenu.addMenuListener(this);
@@ -97,6 +108,7 @@ public class ConsolePopupMenu implements MenuListener, MenuItemListener {
 	public void menuSelected(MenuItem mi) {
 		if (mi == stateType) {
 			console.switchState();
+			console.getToolBar().updateCheck(console.getState());
 		} else if (mi == cutItem) {
 			console.getStyledText().cut();
 		} else if (mi == copyItem) {
@@ -106,53 +118,11 @@ public class ConsolePopupMenu implements MenuListener, MenuItemListener {
 		} else if (mi == deleteItem) {
 			console.getStyledText().insert(""); //$NON-NLS-1$
 		} else if (mi == saveasItem) {
-			saveasAction();
+			new SaveAsConsoleAction("Save as", console).run();
 		} else if (mi == clearItem) {
 			console.clear();
 		} else if (mi == printItem) {
 			print();
-		}
-	}
-
-	void saveasAction() {
-		String consoleText = console.getText();
-		if (consoleText.trim().length() == 0) {
-			return;
-		}
-		initFileDialog();
-		fdialog.setFileName("console.txt"); //$NON-NLS-1$
-		fdialog.setFilterPath(FrameworkPlugin.fileDialogLastSelection);
-		String file = fdialog.open();
-		if (file != null) {
-			File theFile = new File(file);
-			FrameworkPlugin.fileDialogLastSelection = theFile.getAbsolutePath();
-			if (theFile.exists()) {
-				initMessageBox();
-				if (mbox.open() != SWT.YES)
-					return;
-			}
-			try {
-				FileOutputStream fos = new FileOutputStream(file);
-				fos.write(consoleText.getBytes());
-				fos.close();
-			} catch (IOException _) {
-			}
-		}
-	}
-
-	private void initFileDialog() {
-		if (fdialog == null) {
-			fdialog = new FileDialog(console.getShell(), SWT.SAVE);
-			fdialog.setFilterExtensions(new String[] { "*.txt", "*.*" }); //$NON-NLS-1$ //$NON-NLS-2$
-			fdialog.setFilterNames(new String[] { Messages.text_files_filter_label, Messages.all_files_filter_label });
-		}
-	}
-
-	private void initMessageBox() {
-		if (mbox == null) {
-			mbox = new MessageBox(console.getShell(), SWT.ICON_QUESTION + SWT.YES | SWT.NO);
-			mbox.setMessage(Messages.overwriting_file_confirmation_text);
-			mbox.setText(Messages.question_dialog_title);
 		}
 	}
 
@@ -165,6 +135,8 @@ public class ConsolePopupMenu implements MenuListener, MenuItemListener {
 
 	public void menuShown(MenuEvent e) {
 		boolean eState = console.hasSelection() && console.getState() == Console.COMMAND_MODE;
+		stateType.setChecked(console.getState() == Console.COMMAND_MODE);
+
 		String text = console.getText();
 		boolean isEmpty = (text.length() == 0);
 		cutItem.setEnabled(eState);
