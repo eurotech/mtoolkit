5110125678e34096858d514a3afb9ccf35711fb8
s_topalov <s_topalov@8c0469b3-3b32-4850-87dc-fad729056409>
2009-06-23 12:50:12 +0000

    Initial version

---
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/IPluginExporter.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2009 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
+package org.tigris.mtoolkit.common;
+
+import org.eclipse.core.runtime.IStatus;
+
+public interface IPluginExporter {
+
+	void exportPlugins(Object info);
+
+	boolean hasFinished();
+
+	IStatus getResult();
+
+	String getQualifier();
+}
