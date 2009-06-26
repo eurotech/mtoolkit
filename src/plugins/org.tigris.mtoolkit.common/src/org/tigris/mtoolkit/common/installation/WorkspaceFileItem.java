3c45f82289d309d1d4968b2520b79b85d802bc24
s_topalov <s_topalov@8c0469b3-3b32-4850-87dc-fad729056409>
2009-06-26 14:50:25 +0000

    moved from osgimanagement plugin

---
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/installation/WorkspaceFileItem.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2009 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
+package org.tigris.mtoolkit.common.installation;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.osgi.util.NLS;
+import org.tigris.mtoolkit.common.UtilitiesPlugin;
+
+public class WorkspaceFileItem implements InstallationItem {
+
+	protected IFile file;
+	protected String mimeType;
+
+	public WorkspaceFileItem(IFile file, String mimeType) {
+		this.file = file;
+		this.mimeType = mimeType;
+	}
+
+	public InputStream getInputStream() throws IOException {
+		try {
+			return file.getContents();
+		} catch (CoreException e) {
+			UtilitiesPlugin.error(NLS.bind("Failed to retrieve contents of file: {0}", file.getFullPath()), e);
+			return null;
+		}
+	}
+
+	public String getMimeType() {
+		return mimeType;
+	}
+
+	public String getName() {
+		return file.getName();
+	}
+
+	public IStatus prepare(IProgressMonitor monitor) {
+		try {
+			file.refreshLocal(IFile.DEPTH_ZERO, monitor);
+		} catch (CoreException e) {
+			return UtilitiesPlugin.newStatus(IStatus.ERROR, "Failed to prepare file for installation", e);
+		}
+		return Status.OK_STATUS;
+	}
+
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/installation/WorkspaceFileProvider.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2009 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
+package org.tigris.mtoolkit.common.installation;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IStatus;
+import org.tigris.mtoolkit.common.UtilitiesPlugin;
+import org.tigris.mtoolkit.common.installation.InstallationItem;
+import org.tigris.mtoolkit.common.installation.InstallationItemProvider;
+
+/**
+ * Generic implementation of the {@link InstallationItemProvider}, which
+ * supports workspace files with specific extension.
+ * <p>
+ * The file extensions, handled by this provider and their MIME type are
+ * specified as additional attributes in the configuration element:<br>
+ * <ul>
+ * <li><b>extension</b> - the extension of the files (without the leading dot).
+ * Example: if you want to specify provider for *.jar files, you need to specify
+ * <code>extension="jar"</code>.</li>
+ * <li><b>type</b> - the MIME type of the files, handled by this provider.</li>
+ * </ul>
+ * </p>
+ * 
+ */
+public abstract class WorkspaceFileProvider implements InstallationItemProvider {
+
+	protected String extension;
+	protected String mimeType;
+
+	public abstract InstallationItem getInstallationItem(Object resource);
+
+	public void init(IConfigurationElement element) throws CoreException {
+		extension = element.getAttribute("extension");
+		if (extension == null)
+			throw new CoreException(UtilitiesPlugin.newStatus(IStatus.ERROR,
+				"Installation item provider must specify 'extension' attribute",
+				null));
+		mimeType = element.getAttribute("type");
+		if (mimeType == null)
+			throw new CoreException(UtilitiesPlugin.newStatus(IStatus.ERROR,
+				"Installation item provider must specify 'type' attribute",
+				null));
+		// successful
+	}
+
+	public boolean isCapable(Object resource) {
+		IFile file = getFileFromGeneric(resource);
+		if (file != null && extension.equals(file.getFileExtension())) {
+			return true;
+		}
+		return false;
+	}
+
+	protected IFile getFileFromGeneric(Object resource) {
+		if (resource instanceof IFile) {
+			return (IFile) resource;
+		} else if (resource instanceof IAdaptable) {
+			return (IFile) ((IAdaptable) resource).getAdapter(IFile.class);
+		}
+		return null;
+	}
+
+}
