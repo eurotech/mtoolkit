af13e5d131fb7e47b7dbcb02504f858bf67ade43
d_nachev <d_nachev@8c0469b3-3b32-4850-87dc-fad729056409>
2009-09-01 13:23:58 +0000

    DONE changed the batch file to accept some parameters from outside

--- build/build.bat
+++ build/build.bat
@@ -1,7 +1,7 @@
-set BASEBUILDER_CVSROOT=:pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse
-set BASEBUILDER_MODULE=org.eclipse.releng.basebuilder
-set BASEBUILDER_TAG=HEAD
-set BASEBUILDER_DIR=%~dp0\..\basebuilder
+if not defined BASEBUILDER_CVSROOT set BASEBUILDER_CVSROOT=:pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse
+if not defined BASEBUILDER_MODULE set BASEBUILDER_MODULE=org.eclipse.releng.basebuilder
+if not defined BASEBUILDER_TAG set BASEBUILDER_TAG=HEAD
+if not defined BASEBUILDER_DIR set BASEBUILDER_DIR=%~dp0\..\basebuilder
 
 set BUILDER=%~dp0
 
@@ -17,4 +17,4 @@ cvs -d%BASEBUILDER_CVSROOT% export -r %BASEBUILDER_TAG% -d %BASEBUILDER_DIR% %BA
 for /f "tokens=*" %%i in ('%~dp0bin\date.exe -u +%%Y%%m%%d-%%H00') do set TIMESTAMP=%%i
 java -jar %BASEBUILDER_DIR%\plugins\%LAUNCHER_JAR% -application org.eclipse.ant.core.antRunner -buildfile build.xml -Dbuilder=%BUILDER% -Dtimestamp=%TIMESTAMP%
 
-pause
+if not defined DONT_PAUSE pause
