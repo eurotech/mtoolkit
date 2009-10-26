bf1393e8b3d7d41cbb07ecf888088c270219da2e
s_topalov <s_topalov@8c0469b3-3b32-4850-87dc-fad729056409>
2009-10-26 09:37:16 +0000

    fix for  	 TOOLKIT-325 plugin project can be installed multiple times

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/logic/InstallBundleOperation.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/logic/InstallBundleOperation.java
@@ -9,11 +9,14 @@ import java.util.Set;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.statushandlers.StatusManager;
 import org.tigris.mtoolkit.iagent.DeviceConnector;
 import org.tigris.mtoolkit.iagent.IAgentException;
 import org.tigris.mtoolkit.iagent.RemoteBundle;
+import org.tigris.mtoolkit.osgimanagement.internal.FrameWorkView;
 import org.tigris.mtoolkit.osgimanagement.internal.FrameworkPlugin;
 import org.tigris.mtoolkit.osgimanagement.internal.Messages;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.model.FrameWork;
@@ -36,26 +39,36 @@ public class InstallBundleOperation extends RemoteBundleOperation {
 			monitor.beginTask(getName(), work);
 			input = new ProgressInputStream(new FileInputStream(bundle), monitor);
 			DeviceConnector connector = framework.getConnector();
-			if (connector == null) return FrameworkPlugin.newStatus(IStatus.ERROR, "Connection lost", null);
+			if (connector == null)
+				return FrameworkPlugin.newStatus(IStatus.ERROR, "Connection lost", null);
 			rBundle = connector.getDeploymentManager().installBundle("remote:" + bundle.getName(), input);
 
 			Set bundleIds = framework.getBundlesKeys();
 
 			// bundle already exists, in which case, we need to update it
 			if (bundleIds.contains(new Long(rBundle.getBundleId()))) {
-				monitor.beginTask(Messages.update_bundle, work);
 				try {
 					// close the old input stream and try again
 					input.close();
 				} catch (IOException e) {
 				}
-				input = new ProgressInputStream(new FileInputStream(bundle), monitor);
-				rBundle.update(input);
+
+				final boolean update[] = new boolean []{false};
+				Display.getDefault().syncExec(new Runnable() {
+					public void run() {
+						update[0] = MessageDialog.openConfirm(FrameWorkView.getShell(), Messages.update_dialog_title,
+								"Bundle \"" + bundle.getName() + "\" is already installed.\n Do you want to update bundle?");
+					}
+				});
+				if (update[0]) {
+					monitor.beginTask(Messages.update_bundle, work);
+					input = new ProgressInputStream(new FileInputStream(bundle), monitor);
+					rBundle.update(input);
+				}
 			}
 		} catch (IOException e) {
-			return FrameworkPlugin.newStatus(IStatus.ERROR,
-				NLS.bind(Messages.update_file_not_found, bundle.getName()),
-				e);
+			return FrameworkPlugin.newStatus(IStatus.ERROR, NLS.bind(Messages.update_file_not_found, bundle.getName()),
+					e);
 		} finally {
 			if (input != null)
 				try {
