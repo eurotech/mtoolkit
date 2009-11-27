d271ac2d0dc7a50bb3c8d1daa02424b4ba4ecb25
s_topalov <s_topalov@8c0469b3-3b32-4850-87dc-fad729056409>
2009-11-27 13:28:12 +0000

    changes related to  	 TOOLKIT-478 Refactor additional contents support API

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/properties/ui/SocketTypeProvider.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/properties/ui/SocketTypeProvider.java
@@ -4,15 +4,23 @@ import java.net.InetAddress;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IMemento;
 import org.tigris.mtoolkit.osgimanagement.DeviceTypeProvider;
+import org.tigris.mtoolkit.osgimanagement.DeviceTypeProviderValidator;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ConstantsDistributor;
 
 public class SocketTypeProvider implements DeviceTypeProvider, ConstantsDistributor {
@@ -20,6 +28,8 @@ public class SocketTypeProvider implements DeviceTypeProvider, ConstantsDistribu
 	private final String TRANSPORT_TYPE = "socket";
 
 	private Text idText;
+	
+	private DeviceTypeProviderValidator validator;
 
 	// Initialize ui values from storage
 	public void setProperties(IMemento config) {
@@ -30,7 +40,8 @@ public class SocketTypeProvider implements DeviceTypeProvider, ConstantsDistribu
 		idText.setText(idString);
 	}
 
-	public Control createPanel(Composite parent) {
+	public Control createPanel(Composite parent, DeviceTypeProviderValidator validator) {
+		this.validator = validator;
 		Composite contentPanel = new Composite(parent, SWT.NULL);
 		GridLayout groupLayout = new GridLayout(2, false);
 		contentPanel.setLayout(groupLayout);
@@ -42,20 +53,54 @@ public class SocketTypeProvider implements DeviceTypeProvider, ConstantsDistribu
 
 		idText = new Text(contentPanel, SWT.SINGLE | SWT.BORDER);
 		idText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		if (validator != null) {
+			idText.addModifyListener(new ModifyListener() {
+				public void modifyText(ModifyEvent e) {
+					validateJob = new Job("Validate address") {
+						protected IStatus run(IProgressMonitor monitor) {
+							final String ip[] = new String[1];
+							Display.getDefault().syncExec(new Runnable() {
+								public void run() {
+									ip[0] = getTransportID();
+								}
+							});
+							final String result = validate(ip[0]);
+							if (validateJob == this) {
+								Display.getDefault().syncExec(new Runnable() {
+									public void run() {
+										SocketTypeProvider.this.validator.setValidState(result);
+									}
+								});
+							}
+							return Status.OK_STATUS;
+						}
+					};
+					validateJob.schedule();
+				}
+			});
+		}
 		return contentPanel;
 	}
 
-	public boolean validate() {
+	private Job validateJob;
+	
+	public String validate() {
 		String ip = getTransportID();
+		return validate(ip);
+	}
+	
+	public String validate(String ip) {
 		try {
 			// just check if address is correct
 			InetAddress.getByName(ip);
 		} catch (Exception e) {
-			return false;
+			return "Invalid address\n"+e.getMessage();
 		}
-		return true;
+		return null;
 	}
 
+	
+	
 	public String getTransportID() {
 		return idText.getText().trim();
 	}
