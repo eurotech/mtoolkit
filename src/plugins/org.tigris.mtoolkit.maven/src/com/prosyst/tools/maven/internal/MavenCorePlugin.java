dd8ee75f9108862ecf4bbb94ea8ddc34297db2b7
d_nachev <d_nachev@8c0469b3-3b32-4850-87dc-fad729056409>
2011-02-03 16:26:02 +0000

    Added standard EPL header

--- src/plugins/org.tigris.mtoolkit.maven/src/com/prosyst/tools/maven/internal/MavenCorePlugin.java
+++ src/plugins/org.tigris.mtoolkit.maven/src/com/prosyst/tools/maven/internal/MavenCorePlugin.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
 package com.prosyst.tools.maven.internal;
 
 import java.util.List;
@@ -25,11 +35,13 @@ public class MavenCorePlugin extends AbstractUIPlugin {
 	// The shared instance
 	private static MavenCorePlugin plugin;
 
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
@@ -90,7 +102,7 @@ public class MavenCorePlugin extends AbstractUIPlugin {
 	}
 
 	public static IStatus newStatus(String message, Throwable t, List<IStatus> children) {
-		IStatus[] statuses = (IStatus[]) children.toArray(new IStatus[children.size()]);
+		IStatus[] statuses = children.toArray(new IStatus[children.size()]);
 		return new MultiStatus(PLUGIN_ID, 0, statuses, message, t);
 	}
 
