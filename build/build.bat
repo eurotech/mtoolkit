d92591db79f88cf2e3e593ac4c9a676536594fa3
i_karabashev <i_karabashev@8c0469b3-3b32-4850-87dc-fad729056409>
2011-04-20 11:53:38 +0000

    Fixed https://devzone.prosyst.bg/jira/browse/TOOLKIT-1292.

--- build/build.bat
+++ build/build.bat
@@ -1,6 +1,7 @@
+setlocal
 if not defined BASEBUILDER_CVSROOT set BASEBUILDER_CVSROOT=:pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse
 if not defined BASEBUILDER_MODULE set BASEBUILDER_MODULE=org.eclipse.releng.basebuilder
-if not defined BASEBUILDER_TAG set BASEBUILDER_TAG=HEAD
+if not defined BASEBUILDER_TAG set BASEBUILDER_TAG=R3_6_2
 if not defined BASEBUILDER_DIR set BASEBUILDER_DIR=%~dp0\..\basebuilder
 
 set BUILDER=%~dp0
@@ -10,11 +11,14 @@ set LAUNCHER_JAR=org.eclipse.equinox.launcher.jar
 if exist %BASEBUILDER_DIR%\plugins\%LAUNCHER_JAR% goto buildOK
 
 :fetchBaseBuilder
-cvs -d%BASEBUILDER_CVSROOT% export -r %BASEBUILDER_TAG% -d %BASEBUILDER_DIR% %BASEBUILDER_MODULE%
+md %BASEBUILDER_DIR%
+cd %BASEBUILDER_DIR%
+cvs -d%BASEBUILDER_CVSROOT% co -r %BASEBUILDER_TAG% %BASEBUILDER_MODULE%
+cd %~dp0
 
 :buildOK
-
 for /f "tokens=*" %%i in ('%~dp0bin\date.exe -u +%%Y%%m%%d-%%H%%M') do set TIMESTAMP=%%i
-java -jar %BASEBUILDER_DIR%\plugins\%LAUNCHER_JAR% -application org.eclipse.ant.core.antRunner -buildfile %~dp0\build.xml -Dbuilder=%BUILDER% -Dtimestamp=%TIMESTAMP% %BUILDER_ARGUMENTS%
+java -jar %BASEBUILDER_DIR%\org.eclipse.releng.basebuilder\plugins\%LAUNCHER_JAR% -application org.eclipse.ant.core.antRunner -buildfile %~dp0\build.xml -Dbuilder=%BUILDER% -Dtimestamp=%TIMESTAMP% %BUILDER_ARGUMENTS%
 
 if not defined DONT_PAUSE pause
+endlocal
