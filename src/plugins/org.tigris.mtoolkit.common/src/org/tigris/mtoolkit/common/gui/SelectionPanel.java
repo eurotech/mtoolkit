536fbacf6404b718ebdcd11b0676709d23d10c90
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2011-06-27 11:50:19 +0000

    fixed https://devzone.prosyst.bg/jira/browse/TOOLKIT-1566

--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/gui/SelectionPanel.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/gui/SelectionPanel.java
@@ -35,241 +35,244 @@ import org.eclipse.swt.widgets.Text;
  */
 public class SelectionPanel extends Composite {
 
-	private CheckboxTableViewer list;
-	private Text filterText;
-	private ListItem[] listItems = new ListItem[0];
-	private ListenerList checkStateListeners = new ListenerList();
+  private CheckboxTableViewer list;
+  private Text filterText;
+  private ListItem[] listItems = new ListItem[0];
+  private ListenerList checkStateListeners = new ListenerList();
 
-	public SelectionPanel(Composite parent, int style) {
-		super(parent, style);
+  public SelectionPanel(Composite parent, int style) {
+    super(parent, style);
 
-		setLayout(new GridLayout());
-		list = CheckboxTableViewer.newCheckList(this, SWT.BORDER);
-		list.setContentProvider(new IStructuredContentProvider() {
-			public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-			}
+    setLayout(new GridLayout());
+    list = CheckboxTableViewer.newCheckList(this, SWT.BORDER);
+    list.setContentProvider(new IStructuredContentProvider() {
+      public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+      }
 
-			public void dispose() {
-			}
+      public void dispose() {
+      }
 
-			public Object[] getElements(Object inputElement) {
-				return inputElement instanceof Object[] ? (Object[]) inputElement : new Object[0];
-			}
-		});
-		list.setSorter(new ViewerSorter() {
-			public int compare(Viewer viewer, Object e1, Object e2) {
-				String name1 = null;
-				String name2 = null;
-				IBaseLabelProvider labelProvider = ((TableViewer) viewer).getLabelProvider();
-				if (labelProvider instanceof ITableLabelProvider) {
-					ITableLabelProvider tlp = (ITableLabelProvider) labelProvider;
-					name1 = tlp.getColumnText(e1, 0);
-					name2 = tlp.getColumnText(e2, 0);
-				} else if (labelProvider instanceof ILabelProvider) {
-					ILabelProvider lp = (ILabelProvider) labelProvider;
-					name1 = lp.getText(e1);
-					name2 = lp.getText(e2);
-				}
-				if (name1 == null) {
-					name1 = "";
-				}
-				if (name2 == null) {
-					name2 = "";
-				}
-				return name1.compareTo(name2);
-			}
-		});
-		list.setLabelProvider(new LabelProvider());
-		list.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
-		filterText = new Text(this, SWT.BORDER | SWT.SINGLE);
-		filterText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		filterText.setMessage("<type filter here>");
+      public Object[] getElements(Object inputElement) {
+        return inputElement instanceof Object[] ? (Object[]) inputElement : new Object[0];
+      }
+    });
+    list.setSorter(new ViewerSorter() {
+      public int compare(Viewer viewer, Object e1, Object e2) {
+        String name1 = null;
+        String name2 = null;
+        IBaseLabelProvider labelProvider = ((TableViewer) viewer).getLabelProvider();
+        if (labelProvider instanceof ITableLabelProvider) {
+          ITableLabelProvider tlp = (ITableLabelProvider) labelProvider;
+          name1 = tlp.getColumnText(e1, 0);
+          name2 = tlp.getColumnText(e2, 0);
+        } else if (labelProvider instanceof ILabelProvider) {
+          ILabelProvider lp = (ILabelProvider) labelProvider;
+          name1 = lp.getText(e1);
+          name2 = lp.getText(e2);
+        }
+        if (name1 == null) {
+          name1 = "";
+        }
+        if (name2 == null) {
+          name2 = "";
+        }
+        return name1.compareTo(name2);
+      }
+    });
+    list.setLabelProvider(new LabelProvider());
+    list.getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
+    filterText = new Text(this, SWT.BORDER | SWT.SINGLE);
+    filterText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+    filterText.setMessage("<type filter here>");
 
-		list.addFilter(new ViewerFilter() {
-			public Object[] filter(Viewer viewer, Object parent, Object[] elements) {
-				String filter = filterText.getText().trim().toLowerCase();
-				if ("".equals(filter)) {
-					return elements;
-				}
-				ArrayList out = new ArrayList();
-				for (int i = 0; i < elements.length; i++) {
-					if (match(elements[i], filter, viewer)) {
-						out.add(elements[i]);
-					}
-				}
-				return out.toArray();
-			}
+    list.addFilter(new ViewerFilter() {
+      public Object[] filter(Viewer viewer, Object parent, Object[] elements) {
+        String filter = filterText.getText().trim().toLowerCase();
+        if ("".equals(filter)) {
+          return elements;
+        }
+        ArrayList out = new ArrayList();
+        for (int i = 0; i < elements.length; i++) {
+          if (match(elements[i], filter, viewer)) {
+            out.add(elements[i]);
+          }
+        }
+        return out.toArray();
+      }
 
-			public boolean select(Viewer viewer, Object parentElement, Object element) {
-				String filter = filterText.getText().trim().toLowerCase();
-				return match(element, filter, viewer);
-			}
+      public boolean select(Viewer viewer, Object parentElement, Object element) {
+        String filter = filterText.getText().trim().toLowerCase();
+        return match(element, filter, viewer);
+      }
 
-			private boolean match(Object element, String filter, Viewer viewer) {
-				IBaseLabelProvider labelProvider = ((TableViewer) viewer).getLabelProvider();
-				if (labelProvider instanceof ITableLabelProvider) {
-					ITableLabelProvider tlp = (ITableLabelProvider) labelProvider;
-					int numCols = ((TableViewer) viewer).getTable().getColumnCount();
-					if (numCols == 0) {
-						// in case the table is used as list
-						numCols = 1;
-					}
-					for (int i = 0; i < numCols; i++) {
-						String txt = tlp.getColumnText(element, i);
-						if (txt != null
-								&& txt.toLowerCase().indexOf(filter) != -1) {
-							return true;
-						}
-					}
-				} else if (labelProvider instanceof ILabelProvider) {
-					ILabelProvider lp = (ILabelProvider) labelProvider;
-					String txt = lp.getText(element);
-					if (txt != null && txt.toLowerCase().indexOf(filter) != -1) {
-						return true;
-					}
-				}
-				return false;
-			}
-		});
+      private boolean match(Object element, String filter, Viewer viewer) {
+        IBaseLabelProvider labelProvider = ((TableViewer) viewer).getLabelProvider();
+        if (labelProvider instanceof ITableLabelProvider) {
+          ITableLabelProvider tlp = (ITableLabelProvider) labelProvider;
+          int numCols = ((TableViewer) viewer).getTable().getColumnCount();
+          if (numCols == 0) {
+            // in case the table is used as list
+            numCols = 1;
+          }
+          for (int i = 0; i < numCols; i++) {
+            String txt = tlp.getColumnText(element, i);
+            if (txt != null && txt.toLowerCase().indexOf(filter) != -1) {
+              return true;
+            }
+          }
+        } else if (labelProvider instanceof ILabelProvider) {
+          ILabelProvider lp = (ILabelProvider) labelProvider;
+          String txt = lp.getText(element);
+          if (txt != null && txt.toLowerCase().indexOf(filter) != -1) {
+            return true;
+          }
+        }
+        return false;
+      }
+    });
 
-		list.setCheckStateProvider(new ICheckStateProvider() {
-			public boolean isGrayed(Object element) {
-				return false;
-			}
+    list.setCheckStateProvider(new ICheckStateProvider() {
+      public boolean isGrayed(Object element) {
+        return false;
+      }
 
-			public boolean isChecked(Object element) {
-				return ((ListItem) element).checked;
-			}
-		});
+      public boolean isChecked(Object element) {
+        return ((ListItem) element).checked;
+      }
+    });
 
-		list.addCheckStateListener(new ICheckStateListener() {
-			public void checkStateChanged(CheckStateChangedEvent event) {
-				((ListItem) event.getElement()).checked = event.getChecked();
-				fireCheckStateChanged(event);
-			}
-		});
-		final FilterJob filterJob = new FilterJob("", list);
-		filterText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				filterJob.cancel();
-				filterJob.schedule(300);
-				TableItem[] items = ((Table) list.getControl()).getItems();
-				for (int i = 0; i < items.length; i++) {
-					list.setChecked(items[i].getData(), ((ListItem) items[i].getData()).checked);
-				}
-			}
-		});
+    list.addCheckStateListener(new ICheckStateListener() {
+      public void checkStateChanged(CheckStateChangedEvent event) {
+        ((ListItem) event.getElement()).checked = event.getChecked();
+        fireCheckStateChanged(event);
+      }
+    });
+    final FilterJob filterJob = new FilterJob("", list);
+    filterText.addModifyListener(new ModifyListener() {
+      public void modifyText(ModifyEvent e) {
+        filterJob.cancel();
+        filterJob.schedule(300);
+        TableItem[] items = ((Table) list.getControl()).getItems();
+        for (int i = 0; i < items.length; i++) {
+          list.setChecked(items[i].getData(), ((ListItem) items[i].getData()).checked);
+        }
+      }
+    });
 
-		Composite buttonsPanel = new Composite(this, SWT.NONE);
-		buttonsPanel.setLayout(new GridLayout(2, true));
-		buttonsPanel.setLayoutData(new GridData());
+    Composite buttonsPanel = new Composite(this, SWT.NONE);
+    buttonsPanel.setLayout(new GridLayout(2, true));
+    buttonsPanel.setLayoutData(new GridData());
 
-		Button btnSelect = new Button(buttonsPanel, SWT.PUSH);
-		btnSelect.setText("Select all");
-		btnSelect.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent event) {
-				selectAll(true);
-				fireCheckStateChanged(new CheckStateChangedEvent2(list, listItems, true));
-			}
-		});
-		GridData gridData = new GridData(SWT.FILL, SWT.CENTER, false, false);
-		btnSelect.setLayoutData(gridData);
+    Button btnSelect = new Button(buttonsPanel, SWT.PUSH);
+    btnSelect.setText("Select all");
+    btnSelect.addSelectionListener(new SelectionAdapter() {
+      public void widgetSelected(SelectionEvent event) {
+        if (listItems.length > 0) {
+          selectAll(true);
+          fireCheckStateChanged(new CheckStateChangedEvent2(list, listItems, true));
+        }
+      }
+    });
+    GridData gridData = new GridData(SWT.FILL, SWT.CENTER, false, false);
+    btnSelect.setLayoutData(gridData);
 
-		Button btnDeselect = new Button(buttonsPanel, SWT.PUSH);
-		btnDeselect.setText("Deselect all");
-		btnDeselect.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent event) {
-				selectAll(false);
-				fireCheckStateChanged(new CheckStateChangedEvent2(list, listItems, false));
-			}
-		});
-		gridData = new GridData(SWT.FILL, SWT.CENTER, false, false);
-		btnDeselect.setLayoutData(gridData);
-	}
+    Button btnDeselect = new Button(buttonsPanel, SWT.PUSH);
+    btnDeselect.setText("Deselect all");
+    btnDeselect.addSelectionListener(new SelectionAdapter() {
+      public void widgetSelected(SelectionEvent event) {
+        if (listItems.length > 0) {
+          selectAll(false);
+          fireCheckStateChanged(new CheckStateChangedEvent2(list, listItems, false));
+        }
+      }
+    });
+    gridData = new GridData(SWT.FILL, SWT.CENTER, false, false);
+    btnDeselect.setLayoutData(gridData);
+  }
 
-	/**
-	 * Sets to the given value the checked state for all elements. Does not fire
-	 * events to check state listeners.
-	 * 
-	 * @param state
-	 */
-	public void selectAll(boolean state) {
-		list.setAllChecked(state);
-		for (int i = 0; i < listItems.length; i++) {
-			listItems[i].checked = state;
-		}
-	}
+  /**
+   * Sets to the given value the checked state for all elements. Does not fire
+   * events to check state listeners.
+   * 
+   * @param state
+   */
+  public void selectAll(boolean state) {
+    list.setAllChecked(state);
+    for (int i = 0; i < listItems.length; i++) {
+      listItems[i].checked = state;
+    }
+  }
 
-	public void setItems(Object items[]) {
-		listItems = new ListItem[items.length];
-		for (int i = 0; i < listItems.length; i++) {
-			listItems[i] = new ListItem();
-			listItems[i].element = items[i];
-		}
-		list.setInput(listItems);
-	}
+  public void setItems(Object items[]) {
+    listItems = new ListItem[items.length];
+    for (int i = 0; i < listItems.length; i++) {
+      listItems[i] = new ListItem();
+      listItems[i].element = items[i];
+    }
+    list.setInput(listItems);
+  }
 
-	public Object[] getSelected() {
-		int size = listItems.length;
-		ArrayList out = new ArrayList(size);
-		for (int i = 0; i < size; ++i) {
-			if (listItems[i].checked) {
-				out.add(listItems[i].element);
-			}
-		}
-		return out.toArray();
-	}
+  public Object[] getSelected() {
+    int size = listItems.length;
+    ArrayList out = new ArrayList(size);
+    for (int i = 0; i < size; ++i) {
+      if (listItems[i].checked) {
+        out.add(listItems[i].element);
+      }
+    }
+    return out.toArray();
+  }
 
-	/**
-	 * Returns the viewer used for displaying items. It uses
-	 * <code>ListItem</code> for holding items. The returned viewer can be used
-	 * to specify custom label providers, etc.
-	 * 
-	 * @return the viewer
-	 * @see ListItem
-	 */
-	public CheckboxTableViewer getViewer() {
-		return list;
-	}
+  /**
+   * Returns the viewer used for displaying items. It uses
+   * <code>ListItem</code> for holding items. The returned viewer can be used
+   * to specify custom label providers, etc.
+   * 
+   * @return the viewer
+   * @see ListItem
+   */
+  public CheckboxTableViewer getViewer() {
+    return list;
+  }
 
-	public ListItem[] getListItems() {
-		return listItems;
-	}
+  public ListItem[] getListItems() {
+    return listItems;
+  }
 
-	/**
-	 * Adds a listener for changes to the checked state of elements in this
-	 * panel. Has no effect if an identical listener is already registered. This
-	 * panel supports sending events with multiple check changes at once (i.e.
-	 * from Select All/Deselect All buttons)
-	 * 
-	 * @param listener
-	 *            a check state listener
-	 * @see CheckStateChangedEvent2
-	 */
-	public void addCheckStateListener(ICheckStateListener listener) {
-		checkStateListeners.add(listener);
-	}
+  /**
+   * Adds a listener for changes to the checked state of elements in this
+   * panel. Has no effect if an identical listener is already registered. This
+   * panel supports sending events with multiple check changes at once (i.e.
+   * from Select All/Deselect All buttons)
+   * 
+   * @param listener
+   *            a check state listener
+   * @see CheckStateChangedEvent2
+   */
+  public void addCheckStateListener(ICheckStateListener listener) {
+    checkStateListeners.add(listener);
+  }
 
-	/**
-	 * Removes the given check state listener. Has no effect if an identical
-	 * listener is not registered.
-	 * 
-	 * @param listener
-	 *            a check state listener
-	 */
-	public void removeCheckStateListener(ICheckStateListener listener) {
-		checkStateListeners.remove(listener);
-	}
+  /**
+   * Removes the given check state listener. Has no effect if an identical
+   * listener is not registered.
+   * 
+   * @param listener
+   *            a check state listener
+   */
+  public void removeCheckStateListener(ICheckStateListener listener) {
+    checkStateListeners.remove(listener);
+  }
 
-	protected void fireCheckStateChanged(final CheckStateChangedEvent event) {
-		Object[] array = checkStateListeners.getListeners();
-		for (int i = 0; i < array.length; i++) {
-			final ICheckStateListener l = (ICheckStateListener) array[i];
-			SafeRunnable.run(new SafeRunnable() {
-				public void run() {
-					l.checkStateChanged(event);
-				}
-			});
-		}
-	}
+  protected void fireCheckStateChanged(final CheckStateChangedEvent event) {
+    Object[] array = checkStateListeners.getListeners();
+    for (int i = 0; i < array.length; i++) {
+      final ICheckStateListener l = (ICheckStateListener) array[i];
+      SafeRunnable.run(new SafeRunnable() {
+        public void run() {
+          l.checkStateChanged(event);
+        }
+      });
+    }
+  }
 }
