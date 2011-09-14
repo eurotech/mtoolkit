a30c77d5de568b9f58edb21901a139f07bc3f348
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2011-09-14 12:51:51 +0000

    fixed https://devzone.prosyst.bg/jira/browse/TOOLKIT-1695

--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/gui/FilterJob.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/gui/FilterJob.java
@@ -5,34 +5,47 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 
 /**
  * @since 6.0
  */
-public class FilterJob extends Job {
+public final class FilterJob extends Job {
+  private final StructuredViewer viewer;
+  private final Runnable postRefreshAction;
 
-	private StructuredViewer viewer;
+  public FilterJob(String name, StructuredViewer viewer) {
+    this(name, viewer, null);
+  }
 
-	public FilterJob(String name, StructuredViewer viewer) {
-		super(name);
-		this.viewer = viewer;
-	}
+  public FilterJob(String name, StructuredViewer aViewer, Runnable apostRefreshAction) {
+    super(name);
+    viewer = aViewer;
+    postRefreshAction = apostRefreshAction;
+  }
 
-	protected IStatus run(IProgressMonitor monitor) {
-		Display display = PlatformUI.getWorkbench().getDisplay();
-		if (!display.isDisposed()) {
-			display.asyncExec(new Runnable() {
-				public void run() {
-					if (viewer.getControl().isDisposed()) {
-						return;
-					}
-					viewer.refresh();
-				}
-			});
-		}
-		return Status.OK_STATUS;
-	}
+  /* (non-Javadoc)
+   * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
+   */
+  protected IStatus run(IProgressMonitor monitor) {
+    Display display = PlatformUI.getWorkbench().getDisplay();
+    if (!display.isDisposed()) {
+      display.asyncExec(new Runnable() {
+        public void run() {
+          Control control = viewer.getControl();
+          if (control.isDisposed()) {
+            return;
+          }
+          viewer.refresh();
+          if (postRefreshAction != null) {
+            postRefreshAction.run();
+          }
+        }
+      });
+    }
+    return Status.OK_STATUS;
+  }
 
 }
