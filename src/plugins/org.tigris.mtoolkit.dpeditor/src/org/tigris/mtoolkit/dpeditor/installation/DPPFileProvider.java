cdf382b720d891a8f3653eead212689e00b9d278
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2011-10-25 06:44:51 +0000

    fixed https://devzone.prosyst.bg/jira/browse/TOOLKIT-1742

--- src/plugins/org.tigris.mtoolkit.dpeditor/src/org/tigris/mtoolkit/dpeditor/installation/DPPFileProvider.java
+++ src/plugins/org.tigris.mtoolkit.dpeditor/src/org/tigris/mtoolkit/dpeditor/installation/DPPFileProvider.java
@@ -33,17 +33,16 @@ import org.tigris.mtoolkit.dpeditor.DPActivator;
 import org.tigris.mtoolkit.dpeditor.util.DPPUtil;
 import org.tigris.mtoolkit.util.DPPFile;
 
-public class DPPFileProvider extends WorkspaceFileProvider {
-  
-  public class DPPFileItem extends WorkspaceFileItem {
+public final class DPPFileProvider extends WorkspaceFileProvider {
 
+  public class DPPFileItem extends WorkspaceFileItem {
     public File dpFile;
     private boolean delete = false;
-    
+
     public DPPFileItem(IFile file, String mimeType) {
       super(file, mimeType);
     }
-    
+
     public void dispose() {
       super.dispose();
       if (delete) {
@@ -55,14 +54,32 @@ public class DPPFileProvider extends WorkspaceFileProvider {
         }
       }
     }
-    
+
     public InputStream getInputStream() throws IOException {
       if (preparedFile != null) {
         return new FileInputStream(preparedFile);
       }
       return new FileInputStream(dpFile);
     }
+
+    public String getLocation() {
+      if (preparedFile != null) {
+        return preparedFile.getAbsolutePath();
+      }
+      if (dpFile != null) {
+        return dpFile.getAbsolutePath();
+      }
+      throw new IllegalStateException("Installation item wasn't initialized correctly, missing location to base file");
+    }
     
+    public void setLocation(File file) {
+      if (preparedFile != null && !preparedFile.equals(file)) {
+        // delete the previous prepared item
+        preparedFile.delete();
+      }
+      preparedFile = file;
+    }
+
     public IStatus prepare(IProgressMonitor monitor, Map properties) {
       try {
         Display dis = PlatformUI.getWorkbench().getDisplay();
@@ -74,26 +91,27 @@ public class DPPFileProvider extends WorkspaceFileProvider {
         DPPFile dppFile = new DPPFile(file.getLocation().toFile(), file.getProject().getLocation().toOSString());
         dpFile = new File(dppFile.getBuildInfo().getBuildLocation() + "/" + dppFile.getBuildInfo().getDpFileName());
         delete = !dpFile.exists();
+        preparedFile = (delete) ? dpFile : null;
         DPPUtil.generateDeploymentPackage(dppFile, monitor, file.getProject(), DPPUtil.TYPE_QUICK_BUILD_DPP);
-        preparedFile = dpFile;
-          if (properties != null && "Dalvik".equalsIgnoreCase((String) properties.get("jvm.name")) && !AndroidUtils.isDpConvertedToDex(dpFile)) {
-            File convertedFile = new File(DPActivator.getDefault().getStateLocation() + "/dex/" + dpFile.getName());
-            convertedFile.getParentFile().mkdirs();
-            AndroidUtils.convertDpToDex(dpFile, convertedFile, monitor);
-            preparedFile = convertedFile;
-          }
-          if (monitor.isCanceled()) {
-            return Status.CANCEL_STATUS;
+        if (properties != null && "Dalvik".equalsIgnoreCase((String) properties.get("jvm.name"))
+            && !AndroidUtils.isDpConvertedToDex(dpFile)) {
+          File convertedFile = new File(DPActivator.getDefault().getStateLocation() + "/dex/" + dpFile.getName());
+          convertedFile.getParentFile().mkdirs();
+          AndroidUtils.convertDpToDex(dpFile, convertedFile, monitor);
+          setLocation(convertedFile);
+        }
+        if (monitor.isCanceled()) {
+          return Status.CANCEL_STATUS;
         }
       } catch (Exception e) {
         return new Status(Status.ERROR, DPActivator.PLUGIN_ID, "Failed to prepare file for installation");
       }
-      
+
       return Status.OK_STATUS;
     }
-    
+
   } // end of DPPFileItem
-  
+
   public IStatus prepareItems(List items, Map properties, IProgressMonitor monitor) {
     if (items != null) {
       for (int i = 0; i < items.size(); i++) {
@@ -105,11 +123,11 @@ public class DPPFileProvider extends WorkspaceFileProvider {
     }
     return Status.OK_STATUS;
   }
-  
+
   public String getName() {
     return "Deployment packages provider";
   }
-  
+
   public InstallationItem getInstallationItem(Object resource) {
     return new DPPFileItem(getFileFromGeneric(resource), mimeType);
   }
