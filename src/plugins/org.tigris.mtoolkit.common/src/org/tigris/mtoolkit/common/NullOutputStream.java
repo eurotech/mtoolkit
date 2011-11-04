1caaf1f78afe3bb453ae2c395e227f0af6e1cdc0
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2011-11-04 10:52:53 +0000

    Added common class NullOutputStream

---
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/NullOutputStream.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2009 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
+package org.tigris.mtoolkit.common;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @since 5.0
+ */
+public final class NullOutputStream extends OutputStream {
+  public void write(byte[] b, int off, int len) throws IOException {
+  }
+
+  public void write(int b) throws IOException {
+  }
+}
