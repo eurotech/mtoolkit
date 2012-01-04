28083354b26df1c1ccd66c2ff322987a05f9d882
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-01-04 08:55:17 +0000

    Fixed https://devzone.prosyst.bg/jira/browse/TOOLKIT-1844

--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/export/PluginExportManager.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/export/PluginExportManager.java
@@ -28,14 +28,14 @@ import org.tigris.mtoolkit.common.UtilitiesPlugin;
 /**
  * @since 5.0
  */
-public class PluginExportManager {
-
+public final class PluginExportManager {
   private static final String EXPORTED_FILENAME_EXTENSION = ".jar";
   private static final String EXPORT_FILENAME_SEPARATOR = "_";
+  private static final String EMPTY_VERSION = Version.emptyVersion.toString();
+  private static final Object NOT_EXPORTED = new Object();
 
   private IPluginExporter exporter;
   private Map/*<IPluginModelBase,String>*/bundlesToExport = new HashMap();
-  private static final Object NOT_EXPORTED = new Object();
 
   private PluginExportManager(IPluginExporter exporter) {
     this.exporter = exporter;
@@ -52,22 +52,21 @@ public class PluginExportManager {
 
   public void addBundle(String symbolicName, String version) throws CoreException {
     IPluginModelBase bundleModel = findModel(symbolicName, version);
-    if (bundleModel == null)
+    if (bundleModel == null) {
       throw UtilitiesPlugin.newException(IStatus.ERROR, "Cannot find bundle " + symbolicName + " (" + version
           + ") in current target platform", null);
+    }
     addBundle(bundleModel);
   }
 
   public static IPluginModelBase findModel(String symbolicName, String version) {
     ModelEntry entry = PluginRegistry.findEntry(symbolicName);
     if (entry == null) {
-      pluginModelNotFound(symbolicName, version);
       return null;
     }
     if (version == null) {
       IPluginModelBase model = entry.getModel();
       if (model == null || !model.isEnabled()) {
-        pluginModelNotFound(symbolicName, version);
         return null;
       }
       return model;
@@ -75,35 +74,33 @@ public class PluginExportManager {
     IPluginModelBase[] activeModels = entry.getActiveModels();
     for (int i = 0; i < activeModels.length; i++) {
       IPluginModelBase model = activeModels[i];
-      if (new Version(version).compareTo(new Version(model.getPluginBase().getVersion())) == 0)
+      if (new Version(version).compareTo(new Version(model.getPluginBase().getVersion())) == 0) {
         return model;
+      }
     }
-    pluginModelNotFound(symbolicName, version);
     return null;
   }
 
-  private static void pluginModelNotFound(String symbolicName, String version) {
-    UtilitiesPlugin.warning("Cannot find bundle " + symbolicName + (version != null ? "_" + version : "")
-        + " in current target platform.", null);
-  }
-
   public String getLocation(IPluginModelBase model) {
-    if (bundlesToExport.get(model) == NOT_EXPORTED)
+    if (bundlesToExport.get(model) == NOT_EXPORTED) {
       return null;
+    }
     return (String) bundlesToExport.get(model);
   }
 
   public String getLocation(String symbolicName, String version) {
     IPluginModelBase bundleModel = findModel(symbolicName, version);
-    if (bundleModel == null)
+    if (bundleModel == null) {
       return null;
+    }
     return getLocation(bundleModel);
   }
 
   public IStatus export(String location, IProgressMonitor monitor) {
     List bundlesForExport = getBundlesForExport();
-    if (bundlesForExport.size() == 0)
+    if (bundlesForExport.size() == 0) {
       return Status.OK_STATUS;
+    }
 
     FeatureExportInfo exportInfo = new FeatureExportInfo();
     exportInfo.toDirectory = true;
@@ -120,47 +117,6 @@ public class PluginExportManager {
       }
     }
     return result;
-  }
-
-  private String determineLocationAfterExport(IPluginModelBase model, FeatureExportInfo info) {
-    IPath path = new Path(info.destinationDirectory).append("plugins");
-    return path.append(determineExportedFilename(model, info)).toString();
-  }
-
-  private String determineExportedFilename(IPluginModelBase model, FeatureExportInfo info) {
-    //String qualifier = info.qualifier;
-    String symbolicName = model.getPluginBase().getId();
-    String version = model.getPluginBase().getVersion();
-    if (version == null)
-      // PDE uses 0.0.0 as default version for exported plugins in the filename
-      version = "0.0.0";
-    version = replaceQualifier(version, info.qualifier);
-    return symbolicName + EXPORT_FILENAME_SEPARATOR + version + EXPORTED_FILENAME_EXTENSION;
-  }
-
-  private static String replaceQualifier(String version, String qualifier) {
-    int qualifierIdx;
-    if ((qualifierIdx = version.indexOf("qualifier")) != -1)
-      version = version.substring(0, qualifierIdx) + qualifier
-          + version.substring(qualifierIdx + "qualifier".length(), version.length());
-    return version;
-  }
-
-  private void setBundlesExportLocation(IPluginModelBase model, String location) {
-    if (bundlesToExport.get(model) == null)
-      throw new IllegalStateException(
-          "Internal error: cannot save export location for bundle, which is not in export list");
-    bundlesToExport.put(model, location);
-  }
-
-  private List getBundlesForExport() {
-    List bundles = new ArrayList();
-    for (Iterator it = bundlesToExport.entrySet().iterator(); it.hasNext();) {
-      Entry entry = (Entry) it.next();
-      if (entry.getValue() == NOT_EXPORTED)
-        bundles.add(entry.getKey());
-    }
-    return bundles;
   }
 
   /**
@@ -196,5 +152,50 @@ public class PluginExportManager {
 
   public static PluginExportManager create() {
     return create(PluginExporter.getInstance());
+  }
+
+  private String determineLocationAfterExport(IPluginModelBase model, FeatureExportInfo info) {
+    IPath path = new Path(info.destinationDirectory).append("plugins");
+    return path.append(determineExportedFilename(model, info)).toString();
+  }
+
+  private String determineExportedFilename(IPluginModelBase model, FeatureExportInfo info) {
+    //String qualifier = info.qualifier;
+    String symbolicName = model.getPluginBase().getId();
+    String version = model.getPluginBase().getVersion();
+    if (version == null) {
+      // PDE uses 0.0.0 as default version for exported plugins in the filename
+      version = EMPTY_VERSION;
+    }
+    version = replaceQualifier(version, info.qualifier);
+    return symbolicName + EXPORT_FILENAME_SEPARATOR + version + EXPORTED_FILENAME_EXTENSION;
+  }
+
+  private static String replaceQualifier(String version, String qualifier) {
+    int qualifierIdx;
+    if ((qualifierIdx = version.indexOf("qualifier")) != -1) {
+      version = version.substring(0, qualifierIdx) + qualifier
+          + version.substring(qualifierIdx + "qualifier".length(), version.length());
+    }
+    return version;
+  }
+
+  private void setBundlesExportLocation(IPluginModelBase model, String location) {
+    if (bundlesToExport.get(model) == null) {
+      throw new IllegalStateException(
+          "Internal error: cannot save export location for bundle, which is not in export list");
+    }
+    bundlesToExport.put(model, location);
+  }
+
+  private List getBundlesForExport() {
+    List bundles = new ArrayList();
+    for (Iterator it = bundlesToExport.entrySet().iterator(); it.hasNext();) {
+      Entry entry = (Entry) it.next();
+      if (entry.getValue() == NOT_EXPORTED) {
+        bundles.add(entry.getKey());
+      }
+    }
+    return bundles;
   }
 }
