477afb02bf9ae1b37089e0a411f6d42e983fefad
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-01-24 14:31:47 +0000

    Removed hard-coded extension points

--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/ResourcePropertyTester.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/ResourcePropertyTester.java
@@ -1,94 +1,66 @@
 package org.tigris.mtoolkit.common;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExtensionPoint;
-import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.core.runtime.Platform;
 import org.tigris.mtoolkit.common.installation.InstallationItem;
 import org.tigris.mtoolkit.common.installation.InstallationItemProcessor;
 import org.tigris.mtoolkit.common.installation.InstallationItemProvider;
+import org.tigris.mtoolkit.common.installation.InstallationRegistry;
 
-public class ResourcePropertyTester extends PropertyTester {
-
-  private IExtensionRegistry registry = null;
+public final class ResourcePropertyTester extends PropertyTester {
   private InstallationItem installationItem;
 
-  public ResourcePropertyTester() {
-    super();
-    registry = Platform.getExtensionRegistry();
-  }
-
+  /* (non-Javadoc)
+   * @see org.eclipse.core.expressions.IPropertyTester#test(java.lang.Object, java.lang.String, java.lang.Object[], java.lang.Object)
+   */
   public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
     IResource resource = (IResource) receiver;
     if (isResourceProviderAvailable(resource) && isResourceProcessorAvailable(resource)) {
       return true;
     }
     return false;
-
   }
 
   private boolean isResourceProcessorAvailable(IResource resource) {
-
-    IExtensionPoint extensionPoint = registry.getExtensionPoint("org.tigris.mtoolkit.common.installationItemProcessors");
-    IConfigurationElement[] elements = extensionPoint.getConfigurationElements();
-    for (int i = 0; i < elements.length; i++) {
-      if (!elements[i].getName().equals("processor")) {
+    Iterator processorsIterator = InstallationRegistry.getInstance().getProcessors().iterator();
+    while (processorsIterator.hasNext()) {
+      InstallationItemProcessor processor = (InstallationItemProcessor) processorsIterator.next();
+      String[] types = processor.getSupportedMimeTypes();
+      if (types == null) {
         continue;
       }
       List mimeTypes = new ArrayList();
-      try {
-        Object processor = elements[i].createExecutableExtension("class");
-        if (processor == null)
-          return false;
-
-        String[] types = ((InstallationItemProcessor) processor).getSupportedMimeTypes();
-        if (types == null)
-          continue;
-        for (int j = 0; j < types.length; j++) {
-          if (!mimeTypes.contains(types[j]))
-            mimeTypes.add(types[j]);
+      for (int j = 0; j < types.length; j++) {
+        if (!mimeTypes.contains(types[j])) {
+          mimeTypes.add(types[j]);
         }
-
-        String curItemMimeType = installationItem.getMimeType();
-        if (mimeTypes.contains(curItemMimeType))
-          return true;
-      } catch (CoreException e) {
-        e.printStackTrace();
+      }
+      String curItemMimeType = installationItem.getMimeType();
+      if (mimeTypes.contains(curItemMimeType)) {
+        return true;
       }
     }
     return false;
   }
 
   private boolean isResourceProviderAvailable(IResource resource) {
-    IExtensionPoint extensionPoint = registry.getExtensionPoint("org.tigris.mtoolkit.common.installationItemProviders");
-    IConfigurationElement[] elements = extensionPoint.getConfigurationElements();
-    for (int i = 0; i < elements.length; i++) {
-      if (!elements[i].getName().equals("provider")) {
+    Iterator providersIterator = InstallationRegistry.getInstance().getProviders().iterator();
+    while (providersIterator.hasNext()) {
+      InstallationItemProvider provider = (InstallationItemProvider) providersIterator.next();
+      if (provider == null) {
         continue;
       }
-      try {
-        Object provider = elements[i].createExecutableExtension("class");
-        if (provider != null) {
-          ((InstallationItemProvider) provider).init(elements[i]);
-          if (((InstallationItemProvider) provider).isCapable(resource)) {
-            installationItem = ((InstallationItemProvider) provider).getInstallationItem(resource);
-            if (installationItem != null) {
-              return true;
-            }
-          }
+      if (provider.isCapable(resource)) {
+        installationItem = ((InstallationItemProvider) provider).getInstallationItem(resource);
+        if (installationItem != null) {
+          return true;
         }
-
-      } catch (CoreException e) {
-        e.printStackTrace();
       }
     }
     return false;
   }
-
 }
