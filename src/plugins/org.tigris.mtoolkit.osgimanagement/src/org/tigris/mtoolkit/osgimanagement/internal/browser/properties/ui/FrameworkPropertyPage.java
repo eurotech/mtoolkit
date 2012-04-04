b07b15acd844d3a1288390f6fd35cf6ff79ae88e
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-04-04 14:25:18 +0000

    Removed unused imports

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/properties/ui/FrameworkPropertyPage.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/properties/ui/FrameworkPropertyPage.java
@@ -1,7 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2012 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
 package org.tigris.mtoolkit.osgimanagement.internal.browser.properties.ui;
 
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -17,70 +26,82 @@ import org.tigris.mtoolkit.osgimanagement.internal.browser.model.FrameworkImpl;
 
 public class FrameworkPropertyPage extends PropertyPage implements ConstantsDistributor, FrameworkPanel.ErrorMonitor {
 
-	private FrameworkPanel fwPanel;
-	private FrameworkImpl fw;
+  private FrameworkPanel fwPanel;
+  private FrameworkImpl fw;
 
-	public FrameworkPropertyPage() {
-		super();
-	}
+  public FrameworkPropertyPage() {
+    super();
+  }
 
-	/**
-	 * @see PreferencePage#createContents(Composite)
-	 */
-	protected Control createContents(Composite parent) {
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, IHelpContextIds.PROPERTY_FRAMEWORK);
-		Composite composite = new Composite(parent, SWT.NONE);
-		GridLayout layout = new GridLayout();
-		layout.marginHeight = 0;
-		layout.marginWidth = 0;
-		composite.setLayout(layout);
-		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.preference.PreferencePage#createContents(org.eclipse.swt.widgets.Composite)
+   */
+  @Override
+  protected Control createContents(Composite parent) {
+    PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, IHelpContextIds.PROPERTY_FRAMEWORK);
+    Composite composite = new Composite(parent, SWT.NONE);
+    GridLayout layout = new GridLayout();
+    layout.marginHeight = 0;
+    layout.marginWidth = 0;
+    composite.setLayout(layout);
+    composite.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-		fw = (FrameworkImpl) getElement();
+    fw = (FrameworkImpl) getElement();
 
-		fwPanel = new FrameworkPanel(composite, fw, fw.getParent(), GridData.FILL_HORIZONTAL);
-		fwPanel.setErrorMonitor(this);
-		fwPanel.initialize(fw.getConfig());
+    fwPanel = new FrameworkPanel(composite, fw, fw.getParent(), GridData.FILL_HORIZONTAL);
+    fwPanel.setErrorMonitor(this);
+    fwPanel.initialize(fw.getConfig());
 
-		return composite;
-	}
+    return composite;
+  }
 
-	protected void performDefaults() {
-		fwPanel.initialize(fw.getConfig());
-	}
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.preference.PreferencePage#performDefaults()
+   */
+  @Override
+  protected void performDefaults() {
+    fwPanel.initialize(fw.getConfig());
+  }
 
-	public boolean performOk() {
-		boolean correct = fwPanel.validate();
-		if (correct) {
-			String oldLabel = fw.getLabel();
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.preference.PreferencePage#performOk()
+   */
+  @Override
+  public boolean performOk() {
+    boolean correct = fwPanel.validate();
+    if (correct) {
+      String oldLabel = fw.getLabel();
 
-			boolean connChanged = fwPanel.save(fw.getConfig());
-			fw.setName(fw.getConfig().getString(FRAMEWORK_NAME));
+      boolean connChanged = fwPanel.save(fw.getConfig());
+      fw.setName(fw.getConfig().getString(FRAMEWORK_NAME));
 
-			updateTitle(oldLabel, fw.getLabel());
+      updateTitle(oldLabel, fw.getLabel());
 
-			DeviceConnector connector = fw.getConnector();
-			if (connector != null) {
-				if (fw.isConnected() && connChanged) {
-					MessageDialog.openInformation(getShell(), Messages.framework_ip_changed_title,
-							Messages.framework_ip_changed_message);
-				}
-			}
-			fw.updateElement();
-		}
+      DeviceConnector connector = fw.getConnector();
+      if (connector != null) {
+        if (fw.isConnected() && connChanged) {
+          MessageDialog.openInformation(getShell(), Messages.framework_ip_changed_title, Messages.framework_ip_changed_message);
+        }
+      }
+      fw.updateElement();
+    }
 
-		return correct;
-	}
+    return correct;
+  }
 
-	public void setErrorMessage(String error) {
-		super.setErrorMessage(error);
-		setValid(error == null);
-	}
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.preference.PreferencePage#setErrorMessage(java.lang.String)
+   */
+  @Override
+  public void setErrorMessage(String error) {
+    super.setErrorMessage(error);
+    setValid(error == null);
+  }
 
-	private void updateTitle(String oldName, String newName) {
-		// there is no other way to update PropertyDialog's title
-		String title = getShell().getText();
-		title = title.replace(oldName, newName);
-		getShell().setText(title);
-	}
+  private void updateTitle(String oldName, String newName) {
+    // there is no other way to update PropertyDialog's title
+    String title = getShell().getText();
+    title = title.replace(oldName, newName);
+    getShell().setText(title);
+  }
 }
--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/properties/ui/SigningPropertyPage.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/properties/ui/SigningPropertyPage.java
@@ -1,6 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2012 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
 package org.tigris.mtoolkit.osgimanagement.internal.browser.properties.ui;
 
-import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -13,44 +22,49 @@ import org.tigris.mtoolkit.osgimanagement.internal.IHelpContextIds;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.model.FrameworkImpl;
 
 public class SigningPropertyPage extends PropertyPage {
+  private CertificatesPanel certificatesPanel;
+  private FrameworkImpl fw;
 
-	private CertificatesPanel certificatesPanel;
-	private FrameworkImpl fw;
+  public SigningPropertyPage() {
+    super();
+  }
 
-	public SigningPropertyPage() {
-		super();
-	}
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.preference.PreferencePage#createContents(org.eclipse.swt.widgets.Composite)
+   */
+  @Override
+  protected Control createContents(Composite parent) {
+    PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, IHelpContextIds.PROPERTY_FRAMEWORK);
+    Composite composite = new Composite(parent, SWT.NONE);
+    GridLayout layout = new GridLayout();
+    layout.marginHeight = 0;
+    layout.marginWidth = 0;
+    composite.setLayout(layout);
+    composite.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-	/**
-	 * @see PreferencePage#createContents(Composite)
-	 */
-	@Override
-	protected Control createContents(Composite parent) {
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(parent, IHelpContextIds.PROPERTY_FRAMEWORK);
-		Composite composite = new Composite(parent, SWT.NONE);
-		GridLayout layout = new GridLayout();
-		layout.marginHeight = 0;
-		layout.marginWidth = 0;
-		composite.setLayout(layout);
-		composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+    fw = (FrameworkImpl) getElement();
 
-		fw = (FrameworkImpl) getElement();
+    certificatesPanel = new CertificatesPanel(composite, 1, 1, GridData.FILL_BOTH);// HORIZONTAL);
+    certificatesPanel.initialize(fw.getSignCertificateUids(fw.getConfig()));
 
-		certificatesPanel = new CertificatesPanel(composite, 1, 1, GridData.FILL_BOTH);// HORIZONTAL);
-		certificatesPanel.initialize(fw.getSignCertificateUids(fw.getConfig()));
+    return composite;
+  }
 
-		return composite;
-	}
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.preference.PreferencePage#performDefaults()
+   */
+  @Override
+  protected void performDefaults() {
 
-	@Override
-	protected void performDefaults() {
+  }
 
-	}
-
-	@Override
-	public boolean performOk() {
-		fw.setSignCertificateUids(fw.getConfig(), certificatesPanel.getSignCertificateUids());
-		return true;
-	}
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.preference.PreferencePage#performOk()
+   */
+  @Override
+  public boolean performOk() {
+    fw.setSignCertificateUids(fw.getConfig(), certificatesPanel.getSignCertificateUids());
+    return true;
+  }
 
 }
