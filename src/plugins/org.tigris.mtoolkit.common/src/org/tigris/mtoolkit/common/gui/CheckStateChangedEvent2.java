d1ca5052cf0fc46a155598e443d264a2a877f4d9
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-04-05 07:13:47 +0000

    Added missing copyright information

--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/gui/CheckStateChangedEvent2.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/gui/CheckStateChangedEvent2.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2012 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
 package org.tigris.mtoolkit.common.gui;
 
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
@@ -11,49 +21,49 @@ import org.eclipse.jface.viewers.ICheckable;
  * @see CheckStateChangedEvent
  * @since 6.0
  */
-public class CheckStateChangedEvent2 extends CheckStateChangedEvent {
+public final class CheckStateChangedEvent2 extends CheckStateChangedEvent {
 
-	private static final long serialVersionUID = 5932685219565428075L;
+  private static final long serialVersionUID = 5932685219565428075L;
 
-	private Object[] elements;
-	private boolean[] states;
+  private Object[] elements;
+  private boolean[] states;
 
-	public CheckStateChangedEvent2(ICheckable source, Object element, boolean state) {
-		super(source, element, state);
-	}
+  public CheckStateChangedEvent2(ICheckable source, Object element, boolean state) {
+    super(source, element, state);
+  }
 
-	public CheckStateChangedEvent2(ICheckable source, Object[] elements, boolean state) {
-		super(source, elements[0], state);
-		this.elements = elements;
-		this.states = new boolean[elements.length];
-		for (int i = 0; i < elements.length; i++) {
-			this.states[i] = state;
-		}
-	}
+  public CheckStateChangedEvent2(ICheckable source, Object[] elements, boolean state) {
+    super(source, elements[0], state);
+    this.elements = elements;
+    this.states = new boolean[elements.length];
+    for (int i = 0; i < elements.length; i++) {
+      this.states[i] = state;
+    }
+  }
 
-	public CheckStateChangedEvent2(ICheckable source, Object[] elements, boolean[] states) {
-		super(source, elements[0], states[0]);
-		this.elements = elements;
-		this.states = new boolean[elements.length];
-		for (int i = 0; i < elements.length; i++) {
-			this.states[i] = states[i];
-		}
-	}
+  public CheckStateChangedEvent2(ICheckable source, Object[] elements, boolean[] states) {
+    super(source, elements[0], states[0]);
+    this.elements = elements;
+    this.states = new boolean[elements.length];
+    for (int i = 0; i < elements.length; i++) {
+      this.states[i] = states[i];
+    }
+  }
 
-	public Object[] getElements() {
-		if (elements == null) {
-			return new Object[] { getElement() };
-		}
-		return elements;
-	}
+  public Object[] getElements() {
+    if (elements == null) {
+      return new Object[] { getElement() };
+    }
+    return elements;
+  }
 
-	public boolean getChecked(int index) {
-		if (states == null) {
-			if (index != 0) {
-				throw new IndexOutOfBoundsException();
-			}
-			return getChecked();
-		}
-		return states[index];
-	}
+  public boolean getChecked(int index) {
+    if (states == null) {
+      if (index != 0) {
+        throw new IndexOutOfBoundsException();
+      }
+      return getChecked();
+    }
+    return states[index];
+  }
 }
