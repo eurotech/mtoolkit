d35ce23d17a2ba05b77bd0116898e0fdefa9050e
t_bakardzhiev <t_bakardzhiev@8c0469b3-3b32-4850-87dc-fad729056409>
2012-05-08 14:25:32 +0000

    Added PDEConstants.

---
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/PDEConstants.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2012 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
+package org.tigris.mtoolkit.common;
+
+/**
+ * @since 6.1
+ */
+public interface PDEConstants {
+
+  public static final String SCHEME_TP = "tp"; //$NON-NLS-1$
+  public static final String SCHEME_WS = "ws"; //$NON-NLS-1$
+
+  public static final String PREFIX_TP = "tp:"; //$NON-NLS-1$
+  public static final String PREFIX_WS = "ws:"; //$NON-NLS-1$
+
+}
