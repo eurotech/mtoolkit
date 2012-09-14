8df7aa26fbd2465e12630a4dd9f6df41c7754ce5
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-09-14 08:15:46 +0000

    Done https://devzone.prosyst.bg/jira/browse/TOOLKIT-2061

--- src/plugins/org.tigris.mtoolkit.maven/src/org/tigris/mtoolkit/maven/launching/MavenProcess.java
+++ src/plugins/org.tigris.mtoolkit.maven/src/org/tigris/mtoolkit/maven/launching/MavenProcess.java
@@ -11,198 +11,68 @@
 package org.tigris.mtoolkit.maven.launching;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.core.RefreshUtil;
 import org.eclipse.debug.core.model.IProcess;
-import org.tigris.mtoolkit.common.PlatformUtils;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.tigris.mtoolkit.maven.MavenConstants;
 import org.tigris.mtoolkit.maven.internal.MavenCorePlugin;
 
 public final class MavenProcess {
+  private static final String MAVEN_LAUNCH_TYPE = "org.eclipse.m2e.Maven2LaunchConfigurationType";
+
+  private MavenProcess() {
+  }
 
   public static void launchDefaultBuild(File location, IProgressMonitor monitor) throws CoreException {
     ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
-    ILaunchConfigurationType launchConfigurationType = launchManager
-        .getLaunchConfigurationType("org.eclipse.m2e.Maven2LaunchConfigurationType");
-
-    if (launchConfigurationType != null) {
-      // String goals = "clean install";
-      String goals = "";
-      for (String goal : MavenConstants.DEFAULT_GOALS) {
-        goals += goal + " ";
-      }
-
-      String loc = location.getPath();
-      loc = loc.replace('\\', '-');
-      loc = loc.replace('/', '-');
-      ILaunchConfigurationWorkingCopy workingCopy = launchConfigurationType.newInstance(null, //
-          "Executing " + goals + " in " + loc);
-      workingCopy.setAttribute("org.eclipse.jdt.launching.WORKING_DIRECTORY", location.getPath());
-      workingCopy.setAttribute("M2_GOALS", goals);
-      workingCopy.setAttribute("org.eclipse.debug.core.ATTR_REFRESH_SCOPE", "${project}");
-      workingCopy.setAttribute("org.eclipse.debug.core.ATTR_REFRESH_RECURSIVE", true);
-
-      ILaunch launch = workingCopy.launch(ILaunchManager.RUN_MODE, monitor);
-
-      IProcess[] processes = launch.getProcesses();
-      if (processes == null || processes.length != 1) {
-        throw new CoreException(new Status(IStatus.ERROR, MavenCorePlugin.PLUGIN_ID, "Could not start maven install process!"));
-      }
-      IProcess process = processes[0];
-      while (!process.isTerminated()) {
-        try {
-          Thread.sleep(50);
-        } catch (InterruptedException e) {
-        }
-      }
-      int exitValue = process.getExitValue();
-      if (exitValue != 0) {
-        throw new CoreException(new Status(IStatus.ERROR, MavenCorePlugin.PLUGIN_ID, "Install process exit value is: "
-            + exitValue));
-      }
-    } else {
-      launchGoals(location, MavenConstants.DEFAULT_GOALS, monitor);
+    ILaunchConfigurationType launchConfigurationType = launchManager.getLaunchConfigurationType(MAVEN_LAUNCH_TYPE);
+    if (launchConfigurationType == null) {
+      throw MavenCorePlugin.newException(IStatus.ERROR, "Cannot find maven launch configuration", null);
     }
-  }
-
-  public static void launchGoals(File location, String[] goals, IProgressMonitor monitor) throws CoreException {
-    String[] cmdLine = generateMavenCmdLine(goals);
-    try {
-      Process mvnProcess = executeProcess(cmdLine, location);
-      waitForMavenBuild(mvnProcess, monitor);
-    } catch (IOException e) {
-      throw MavenCorePlugin.newException(IStatus.ERROR, "Cannot launch Maven build process at " + location.getAbsolutePath(), e);
-    }
-  }
-
-  private static String[] generateMavenCmdLine(String[] goals) {
-    List<String> cmdLine = new ArrayList<String>();
-    if (PlatformUtils.isWindows()) {
-      cmdLine.add("cmd.exe");
-      cmdLine.add("/c");
-      cmdLine.add("mvn");
-    } else {
-      cmdLine.add("mvn");
-    }
-    if (goals != null) {
-      for (String goal : goals) {
-        cmdLine.add(goal);
-      }
-    }
-    return cmdLine.toArray(new String[cmdLine.size()]);
-  }
-
-  private static Process executeProcess(String[] cmdLine, File workingDir) throws IOException {
-    return Runtime.getRuntime().exec(cmdLine, null, workingDir);
-  }
-
-  private static void captureOutput(Process process, OutputStream collectOutput, OutputStream collectError) {
-    new CaptureOutputJob(process, process.getInputStream(), collectOutput, process.toString() + "[stdout]").schedule();
-    new CaptureOutputJob(process, process.getErrorStream(), collectError, process.toString() + "[stderr]").schedule();
-  }
-
-  private static void redirectOutputToStdout(Process process) {
-    captureOutput(process, System.out, System.err);
-  }
-
-  private static void waitForMavenBuild(Process process, IProgressMonitor monitor) throws CoreException {
-    // TODO: Redirect the maven output, so we can handle client requests
-    redirectOutputToStdout(process);
-    MonitorProcessJob job = new MonitorProcessJob(process.toString(), process);
-    job.schedule();
-    try {
-      Job.getJobManager().join(process, monitor);
-    } catch (OperationCanceledException e) {
-    } catch (InterruptedException e) {
-    }
-    if (job.getExitValue() != 0)
-      throw MavenCorePlugin.newException(IStatus.ERROR, "Maven build failed with non-zero exit value: " + job.getExitValue(),
-          null);
-  }
-
-  private static class CaptureOutputJob extends Job {
-    private Process process;
-    private InputStream input;
-    private OutputStream output;
-
-    public CaptureOutputJob(Process process, InputStream input, OutputStream output, String processName) {
-      super("Capturing output of " + processName);
-      this.input = input;
-      this.output = output;
-      this.process = process;
-      setSystem(true);
-      setUser(false);
+    // String goals = "clean install";
+    String goals = "";
+    for (String goal : MavenConstants.DEFAULT_GOALS) {
+      goals += goal + " ";
     }
 
-    @Override
-    public boolean belongsTo(Object family) {
-      return process != null && process.equals(family);
-    }
+    String loc = location.getPath();
+    loc = loc.replace('\\', '-');
+    loc = loc.replace('/', '-');
+    ILaunchConfigurationWorkingCopy workingCopy = launchConfigurationType.newInstance(null, //
+        "Executing " + goals + " in " + loc);
+    workingCopy.setAttribute("M2_GOALS", goals);
+    //Unit tests should should be bypassed
+    workingCopy.setAttribute("M2_SKIP_TESTS", true);
 
-    @Override
-    protected IStatus run(IProgressMonitor monitor) {
-      byte[] buffer = new byte[2048];
-      int read;
+    workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_WORKING_DIRECTORY, location.getPath());
+    workingCopy.setAttribute(RefreshUtil.ATTR_REFRESH_SCOPE, "${project}");
+    workingCopy.setAttribute(RefreshUtil.ATTR_REFRESH_RECURSIVE, true);
+
+    ILaunch launch = workingCopy.launch(ILaunchManager.RUN_MODE, monitor);
+
+    IProcess[] processes = launch.getProcesses();
+    if (processes == null || processes.length != 1) {
+      throw MavenCorePlugin.newException(IStatus.ERROR, "Could not start maven install process!", null);
+    }
+    IProcess process = processes[0];
+    while (!process.isTerminated()) {
       try {
-        while ((read = input.read(buffer)) != -1) {
-          output.write(buffer, 0, read);
-          if (monitor.isCanceled())
-            break;
-        }
-      } catch (IOException e) {
-        if (!monitor.isCanceled())
-          return MavenCorePlugin.newStatus(IStatus.ERROR, "IOException while capturing external process output", e);
-      }
-      if (monitor.isCanceled())
-        return Status.CANCEL_STATUS;
-      return Status.OK_STATUS;
-    }
-  }
-
-  private static class MonitorProcessJob extends Job {
-    private Process process;
-    private int exitValue = -1;
-
-    public MonitorProcessJob(String name, Process process) {
-      super("Monitoring " + name);
-      this.process = process;
-      setSystem(true);
-      setUser(false);
-    }
-
-    @Override
-    public boolean belongsTo(Object family) {
-      return process != null && process.equals(family);
-    }
-
-    @Override
-    protected IStatus run(IProgressMonitor monitor) {
-      try {
-        exitValue = process.waitFor();
-        return Status.OK_STATUS;
+        Thread.sleep(50);
       } catch (InterruptedException e) {
-        Thread.currentThread().interrupt();
-        return Status.CANCEL_STATUS;
       }
     }
-
-    public int getExitValue() {
-      return exitValue;
+    int exitValue = process.getExitValue();
+    if (exitValue != 0) {
+      throw MavenCorePlugin.newException(IStatus.ERROR, "Install process exit value is: " + exitValue, null);
     }
   }
 }
