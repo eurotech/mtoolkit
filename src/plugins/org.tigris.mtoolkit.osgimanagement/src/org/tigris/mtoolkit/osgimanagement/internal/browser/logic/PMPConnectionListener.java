60400c07c4a3f75530c1f07b942c56ba31292929
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-09-25 11:55:25 +0000

    Removed dead code

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/logic/PMPConnectionListener.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/logic/PMPConnectionListener.java
@@ -35,130 +35,129 @@ import org.tigris.mtoolkit.osgimanagement.internal.browser.treeviewer.action.Act
 import org.tigris.mtoolkit.osgimanagement.model.Framework;
 
 public class PMPConnectionListener implements ConnectionListener {
+  private FrameworkImpl     fw;
+  private String            frameworkName;
+  protected DeviceConnector connector;
+  private boolean           autoConnected;
+  private boolean           instrumenting = false;
 
-	private FrameworkImpl fw;
-	private String frameworkName;
-	protected DeviceConnector connector;
-	private boolean autoConnected;
-	boolean shouldInstallIAgent = false;
-	private boolean instrumenting = false;
+  public PMPConnectionListener(FrameworkImpl fw, String frameworkName, DeviceConnector connector, boolean autoConnected) {
+    this.fw = fw;
+    this.frameworkName = frameworkName;
+    this.connector = connector;
+    this.autoConnected = autoConnected;
+    ((DeviceConnectorSpi) connector).getConnectionManager().addConnectionListener(this);
+  }
 
-	public PMPConnectionListener(FrameworkImpl fw, String frameworkName, DeviceConnector connector,
-			boolean autoConnected) {
-		this.fw = fw;
-		this.frameworkName = frameworkName;
-		this.connector = connector;
-		this.autoConnected = autoConnected;
-		((DeviceConnectorSpi) connector).getConnectionManager().addConnectionListener(this);
-	}
+  public void connectionChanged(ConnectionEvent e) {
+    if (e.getConnection().getType() != ConnectionManager.PMP_CONNECTION) {
+      return;
+    }
+    BrowserErrorHandler
+        .debug("PMP Connection Changed " + (e.getType() == ConnectionEvent.CONNECTED ? "CONNECTED" : "DISCONNECTED") + " " + connector); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+    if (e.getType() == ConnectionEvent.DISCONNECTED) {
+      disconnected();
+    } else if (e.getType() == ConnectionEvent.CONNECTED) {
+      // in case we are instrumenting the framework, we want to ignore
+      // connection events
+      if (!instrumenting) {
+        connected();
+      }
+    }
+  }
 
-	public void connectionChanged(ConnectionEvent e) {
-		if (e.getConnection().getType() != ConnectionManager.PMP_CONNECTION)
-			return;
-		BrowserErrorHandler
-				.debug("PMP Connection Changed " + (e.getType() == ConnectionEvent.CONNECTED ? "CONNECTED" : "DISCONNECTED") + " " + connector); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-		if (e.getType() == ConnectionEvent.DISCONNECTED) {
-			disconnected();
-		} else if (e.getType() == ConnectionEvent.CONNECTED) {
-			// in case we are instrumenting the framework, we want to ignore
-			// connection events
-			if (!instrumenting)
-				connected();
-		}
-	}
+  public void disconnected() {
+    if (!autoConnected) {
+      ActionsManager.disconnectConsole(fw);
+    }
 
-	public void disconnected() {
-		if (!autoConnected) {
-			ActionsManager.disconnectConsole(fw);
-		}
+    // if disconnect event received while connect thread is running
+    // block disconnect until connect thread is finished/breaked
+    synchronized (Framework.getLockObject(connector)) {
+    }
 
-		// if disconnect event received while connect thread is running
-		// block disconnect until connect thread is finished/breaked
-		synchronized (Framework.getLockObject(connector)) {
-		}
+    fw.disconnect();
+  }
 
-		fw.disconnect();
-	}
+  public void connected() {
+    if (fw.isConnected()) {
+      return;
+    }
+    Job connectJob = new Job(frameworkName) {
+      @Override
+      protected IStatus run(IProgressMonitor monitor) {
+        SubMonitor sMonitor = SubMonitor.convert(monitor, FrameworkConnectorFactory.CONNECT_PROGRESS);
+        try {
+          sMonitor.setTaskName("Connecting " + frameworkName);
+          SubMonitor connectMonitor = sMonitor.newChild(FrameworkConnectorFactory.CONNECT_PROGRESS_CONNECTING);
+          try {
+            // force creation of pmp connection
+            if (!connector.getVMManager().isVMActive()) {
+              return Util.newStatus(IStatus.ERROR, "Connection failed", null);
+            }
+          } catch (IAgentException e) {
+            return Util.newStatus(IStatus.ERROR, "Connection failed", e);
+          }
 
-	public void connected() {
-		if (fw.isConnected())
-			return;
-		Job connectJob = new Job(frameworkName) {
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				SubMonitor sMonitor = SubMonitor.convert(monitor, FrameworkConnectorFactory.CONNECT_PROGRESS);
-				try {
-					sMonitor.setTaskName("Connecting " + frameworkName);
-					SubMonitor connectMonitor = sMonitor
-							.newChild(FrameworkConnectorFactory.CONNECT_PROGRESS_CONNECTING);
-					try {
-						// force creation of pmp connection
-						if (!connector.getVMManager().isVMActive()) {
-							return Util.newStatus(IStatus.ERROR, "Connection failed", null);
-						}
-					} catch (IAgentException e) {
-						return Util.newStatus(IStatus.ERROR, "Connection failed", e);
-					}
+          try {
+            if (!connector.getVMManager().isVMInstrumented(false)) {
+              if (shouldInstallIAgent()) {
+                try {
+                  instrumenting = true;
+                  connector.getVMManager().instrumentVM();
+                } catch (IAgentException iae) {
+                  if (monitor.isCanceled()) {
+                    return Status.CANCEL_STATUS;
+                  }
+                  return new Status(IStatus.ERROR, FrameworkPlugin.PLUGIN_ID, "Unable to instrument VM.", iae);
+                } finally {
+                  instrumenting = false;
+                }
+              } else {
+                return Status.OK_STATUS;
+              }
+            }
+          } catch (IAgentException e) {
+            return Util.newStatus(IStatus.ERROR, "Connection failed", e);
+          }
 
-					try {
-						if (!connector.getVMManager().isVMInstrumented(false)) {
-							if (shouldInstallIAgent()) {
-								try {
-									instrumenting = true;
-									connector.getVMManager().instrumentVM();
-								} catch (IAgentException iae) {
-									if (monitor.isCanceled())
-										return Status.CANCEL_STATUS;
-									return new Status(IStatus.ERROR, FrameworkPlugin.PLUGIN_ID,
-											"Unable to instrument VM.", iae);
-								} finally {
-									instrumenting = false;
-								}
-							} else {
-								return Status.OK_STATUS;
-							}
-						}
-					} catch (IAgentException e) {
-						return Util.newStatus(IStatus.ERROR, "Connection failed", e);
-					}
+          connectMonitor.worked(FrameworkConnectorFactory.CONNECT_PROGRESS_CONNECTING);
 
-					connectMonitor.worked(FrameworkConnectorFactory.CONNECT_PROGRESS_CONNECTING);
+          if (fw != null) {
+            fw.connect(connector, sMonitor);
+          }
+          if (!autoConnected && fw.isConnected()) {
+            ConsoleManager.connectConsole(connector, fw.getName());
+          }
+          return Status.OK_STATUS;
+        } finally {
+          sMonitor.done();
+          if (!fw.isConnected() && fw.autoConnected) {
+            fw.dispose();
+          }
 
-					if (fw != null) {
-						fw.connect(connector, sMonitor);
-					}
-					if (!autoConnected && fw.isConnected()) {
-						ConsoleManager.connectConsole(connector, fw.getName());
-					}
-					return Status.OK_STATUS;
-				} finally {
-					sMonitor.done();
-					if (!fw.isConnected() && fw.autoConnected) {
-						fw.dispose();
-					}
+        }
+      }
+    };
+    connectJob.schedule();
+  }
 
-				}
-			}
-		};
-		connectJob.schedule();
-	}
+  public DeviceConnector getConnector() {
+    return connector;
+  }
 
-	public DeviceConnector getConnector() {
-		return connector;
-	}
-
-	private boolean shouldInstallIAgent() {
-		final Display display = PlatformUI.getWorkbench().getDisplay();
-		final Boolean result[] = new Boolean[1];
-		display.syncExec(new Runnable() {
-			public void run() {
-				Shell shell = display.getActiveShell();
-				boolean install = MessageDialog.openQuestion(shell, Messages.framework_not_instrumented,
-						Messages.framework_not_instrumented_msg);
-				result[0] = new Boolean(install);
-			}
-		});
-		return result[0] != null && result[0].booleanValue();
-	}
+  private boolean shouldInstallIAgent() {
+    final Display display = PlatformUI.getWorkbench().getDisplay();
+    final Boolean result[] = new Boolean[1];
+    display.syncExec(new Runnable() {
+      public void run() {
+        Shell shell = display.getActiveShell();
+        boolean install = MessageDialog.openQuestion(shell, Messages.framework_not_instrumented,
+            Messages.framework_not_instrumented_msg);
+        result[0] = new Boolean(install);
+      }
+    });
+    return result[0] != null && result[0].booleanValue();
+  }
 
 }
