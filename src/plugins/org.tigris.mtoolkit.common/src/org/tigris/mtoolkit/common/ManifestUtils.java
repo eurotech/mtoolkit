06f08b443a9912d92a554aaf47518fa5728107b1
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-09-28 13:10:25 +0000

    Done strings externalization

--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/ManifestUtils.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/ManifestUtils.java
@@ -19,85 +19,86 @@ import org.eclipse.osgi.util.ManifestElement;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.Constants;
 
-public class ManifestUtils {
+public final class ManifestUtils {
+  private static int          MAXLINE        = 511;
+  private static final String LINE_SEPARATOR = "\n "; //$NON-NLS-1$
+  private static final String LIST_SEPARATOR = ",\n "; //$NON-NLS-1$
 
-	public static Map getManifestHeaders(InputStream stream) throws IOException {
-		try {
-			return ManifestElement.parseBundleManifest(stream, null);
-		} catch (BundleException e) {
-			IOException ioe = new IOException("JAR Manifest is invalid: "
-					+ e.toString());
-			ioe.initCause(e);
-			throw ioe;
-		}
-	}
+  private ManifestUtils() {
+  }
 
-	/**
-	 * @since 1.2
-	 */
-	public static String getBundleSymbolicName(Map headers) {
-		try {
-			ManifestElement[] element = ManifestElement.parseHeader(
-					Constants.BUNDLE_SYMBOLICNAME,
-					(String) headers.get(Constants.BUNDLE_SYMBOLICNAME));
-			if (element == null)
-				return null;
-			return element[0].getValueComponents()[0].trim();
-		} catch (BundleException e) {
-			return null;
-		}
-	}
+  public static Map getManifestHeaders(InputStream stream) throws IOException {
+    try {
+      return ManifestElement.parseBundleManifest(stream, null);
+    } catch (BundleException e) {
+      IOException ioe = new IOException(Messages.ManifestUtils_Invalid_JAR_Manifest + e.toString());
+      ioe.initCause(e);
+      throw ioe;
+    }
+  }
 
-	/**
-	 * @since 1.2
-	 */
-	public static String getBundleVersion(Map headers) {
-		try {
-			ManifestElement[] element = ManifestElement.parseHeader(
-					Constants.BUNDLE_VERSION,
-					(String) headers.get(Constants.BUNDLE_VERSION));
-			if (element == null)
-				return null;
-			return element[0].getValueComponents()[0].trim();
-		} catch (BundleException e) {
-			return null;
-		}
-	}
+  /**
+   * @since 1.2
+   */
+  public static String getBundleSymbolicName(Map headers) {
+    try {
+      ManifestElement[] element = ManifestElement.parseHeader(Constants.BUNDLE_SYMBOLICNAME,
+          (String) headers.get(Constants.BUNDLE_SYMBOLICNAME));
+      if (element == null) {
+        return null;
+      }
+      return element[0].getValueComponents()[0].trim();
+    } catch (BundleException e) {
+      return null;
+    }
+  }
 
-	public static void writeManifest(OutputStream stream, Map headers)
-			throws IOException {
-		Object[] keys = headers.keySet().toArray();
-		for (int i = 0; i < keys.length; i++) {
-			String key = (String) keys[i];
-			String value = (String) headers.get(key);
-			writeEntry(stream, key, value);
-		}
-	}
+  /**
+   * @since 1.2
+   */
+  public static String getBundleVersion(Map headers) {
+    try {
+      ManifestElement[] element = ManifestElement.parseHeader(Constants.BUNDLE_VERSION,
+          (String) headers.get(Constants.BUNDLE_VERSION));
+      if (element == null) {
+        return null;
+      }
+      return element[0].getValueComponents()[0].trim();
+    } catch (BundleException e) {
+      return null;
+    }
+  }
 
-	private static void writeEntry(OutputStream out, String key, String value)
-			throws IOException {
-		if (value != null && value.length() > 0) {
-			out.write(splitOnComma(key + ": " + value).getBytes()); //$NON-NLS-1$
-			out.write('\n');
-		}
-	}
+  public static void writeManifest(OutputStream stream, Map headers) throws IOException { // NO_UCD
+    Object[] keys = headers.keySet().toArray();
+    for (int i = 0; i < keys.length; i++) {
+      String key = (String) keys[i];
+      String value = (String) headers.get(key);
+      writeEntry(stream, key, value);
+    }
+  }
 
-	private static int MAXLINE = 511;
-	private static final String LINE_SEPARATOR = "\n "; //$NON-NLS-1$
-	private static final String LIST_SEPARATOR = ",\n "; //$NON-NLS-1$
+  private static void writeEntry(OutputStream out, String key, String value) throws IOException {
+    if (value != null && value.length() > 0) {
+      out.write(splitOnComma(key + ": " + value).getBytes()); //$NON-NLS-1$
+      out.write('\n');
+    }
+  }
 
-	private static String splitOnComma(String value) {
-		if (value.length() < MAXLINE || value.indexOf(LINE_SEPARATOR) >= 0)
-			return value; // assume the line is already split
-		String[] values = ManifestElement.getArrayFromList(value);
-		if (values == null || values.length == 0)
-			return value;
-		StringBuffer sb = new StringBuffer(value.length()
-				+ ((values.length - 1) * LIST_SEPARATOR.length()));
-		for (int i = 0; i < values.length - 1; i++)
-			sb.append(values[i]).append(LIST_SEPARATOR);
-		sb.append(values[values.length - 1]);
-		return sb.toString();
-	}
+  private static String splitOnComma(String value) {
+    if (value.length() < MAXLINE || value.indexOf(LINE_SEPARATOR) >= 0) {
+      return value; // assume the line is already split
+    }
+    String[] values = ManifestElement.getArrayFromList(value);
+    if (values == null || values.length == 0) {
+      return value;
+    }
+    StringBuffer sb = new StringBuffer(value.length() + ((values.length - 1) * LIST_SEPARATOR.length()));
+    for (int i = 0; i < values.length - 1; i++) {
+      sb.append(values[i]).append(LIST_SEPARATOR);
+    }
+    sb.append(values[values.length - 1]);
+    return sb.toString();
+  }
 
 }
--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/Messages.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/Messages.java
@@ -13,53 +13,48 @@ package org.tigris.mtoolkit.common;
 import org.eclipse.osgi.util.NLS;
 
 /**
- * 
+ *
  */
 public class Messages {
-
   private static final String BUNDLE_NAME = "org.tigris.mtoolkit.common.messages";
 
-  public static String MessageDialog_NoDetails;
-  public static String PasswordDialog_lblPassword;
-  public static String plugin_exporter_not_compatible;
-  public static String CertificatesPanel_signContentGroup;
-  public static String CertificatesPanel_lblCertificates;
-  public static String CertificatesPanel_tblCertColAlias;
-  public static String CertificatesPanel_tblCertColLocation;
-  public static String CertificatesPanel_lblNoCertificates;
-  public static String install_to_menu_label;
-  public static String AbstractInstallationItemProcessor_Preparing_Items;
+  public static String        MessageDialog_NoDetails;
+  public static String        PasswordDialog_lblPassword;
+  public static String        plugin_exporter_not_compatible;
+  public static String        CertificatesPanel_signContentGroup;
+  public static String        CertificatesPanel_lblCertificates;
+  public static String        CertificatesPanel_tblCertColAlias;
+  public static String        CertificatesPanel_tblCertColLocation;
+  public static String        CertificatesPanel_lblNoCertificates;
+  public static String        install_to_menu_label;
+  public static String        ManifestUtils_Invalid_JAR_Manifest;
+  public static String        AbstractInstallationItemProcessor_Preparing_Items;
 
-  public static String certs_ColAlias;
-  public static String certs_ColLocation;
-  public static String certs_btnAdd;
-  public static String certs_btnEdit;
-  public static String certs_btnRemove;
-  public static String certs_lblJarsignerLocation;
+  public static String        certs_ColAlias;
+  public static String        certs_ColLocation;
+  public static String        certs_btnAdd;
+  public static String        certs_btnEdit;
+  public static String        certs_btnRemove;
+  public static String        certs_lblJarsignerLocation;
 
-  public static String dlgCertMan_titleAdd;
-  public static String dlgCertMan_titleEdit;
-  public static String dlgCertMan_descr;
-  public static String dlgCertMan_message_add;
-  public static String dlgCertMan_message_edit;
-  public static String dlgCertMan_labelAlias;
-  public static String dlgCertMan_labelLocation;
-  public static String dlgCertMan_labelType;
-  public static String dlgCertMan_labelPass;
-  public static String dlgCertMan_labelKeyPass;
-  public static String dlgCertMan_verifyAliasEmpty;
-  public static String dlgCertMan_verifyLocationEmpty;
-  public static String dlgCertMan_verifyLocationNotExist;
-  public static String dlgCertMan_browseDlgCaption;
+  public static String        dlgCertMan_titleAdd;
+  public static String        dlgCertMan_titleEdit;
+  public static String        dlgCertMan_descr;
+  public static String        dlgCertMan_message_add;
+  public static String        dlgCertMan_message_edit;
+  public static String        dlgCertMan_labelAlias;
+  public static String        dlgCertMan_labelLocation;
+  public static String        dlgCertMan_labelType;
+  public static String        dlgCertMan_labelPass;
+  public static String        dlgCertMan_labelKeyPass;
+  public static String        dlgCertMan_verifyAliasEmpty;
+  public static String        dlgCertMan_verifyLocationEmpty;
+  public static String        dlgCertMan_verifyLocationNotExist;
+  public static String        dlgCertMan_browseDlgCaption;
 
-  public static String browseLabel;
+  public static String        browseLabel;
 
   static {
-    reloadMessages();
-  }
-
-  public static void reloadMessages() {
     NLS.initializeMessages(BUNDLE_NAME, Messages.class);
   }
-
 }
