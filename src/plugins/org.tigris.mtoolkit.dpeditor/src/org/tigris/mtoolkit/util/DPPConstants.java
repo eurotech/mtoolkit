fdee329051c816e33094240248a665c07d686305
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-10-02 09:03:36 +0000

    Removed dead code

--- src/plugins/org.tigris.mtoolkit.dpeditor/src/org/tigris/mtoolkit/util/DPPConstants.java
+++ src/plugins/org.tigris.mtoolkit.dpeditor/src/org/tigris/mtoolkit/util/DPPConstants.java
@@ -13,71 +13,68 @@ package org.tigris.mtoolkit.util;
 /**
  * This class contains several constants for using the the Deployment Package
  * Editor API .
- * 
+ *
  * @author Antonia Avramova
  * @noimplement This interface is not intended to be implemented by clients.
  */
 public interface DPPConstants {
-  /** corresponds to the "Manifest-Version" header */
-  public static String dpManifestVersionHeader = "Manifest-Version";
-
   /** corresponds to the "DeploymentPackage-SymbolicName" header */
-  public static String dpSymbolicNameHeader    = "DeploymentPackage-SymbolicName";
+  public static String dpSymbolicNameHeader = "DeploymentPackage-SymbolicName";
 
   /** corresponds to the "DeploymentPackage-Version" header */
-  public static String dpVersionHeader         = "DeploymentPackage-Version";
+  public static String dpVersionHeader      = "DeploymentPackage-Version";
 
   /** corresponds to the "DeploymentPackage-FixPack" header */
-  public static String dpFixPackHeader         = "DeploymentPackage-FixPack";
+  public static String dpFixPackHeader      = "DeploymentPackage-FixPack";
 
   /** corresponds to the "DeploymentPackage-Copyright" header */
-  public static String dpCopyrightHeader       = "DeploymentPackage-Copyright";
+  public static String dpCopyrightHeader    = "DeploymentPackage-Copyright";
 
   /** corresponds to the "DeploymentPackage-ContactAddress" header */
-  public static String dpAddressHeader         = "DeploymentPackage-ContactAddress";
+  public static String dpAddressHeader      = "DeploymentPackage-ContactAddress";
 
   /** corresponds to the "DeploymentPackage-Description" header */
-  public static String dpDescriptionHeader     = "DeploymentPackage-Description";
+  public static String dpDescriptionHeader  = "DeploymentPackage-Description";
 
   /** corresponds to the "DeploymentPackage-DocURL" header */
-  public static String dpDocURLHeader          = "DeploymentPackage-DocURL";
+  public static String dpDocURLHeader       = "DeploymentPackage-DocURL";
 
   /** corresponds to the "DeploymentPackage-Vendor" header */
-  public static String dpVendorHeader          = "DeploymentPackage-Vendor";
+  public static String dpVendorHeader       = "DeploymentPackage-Vendor";
 
   /** corresponds to the "DeploymentPackage-License" header */
-  public static String dpLicenseHeader         = "DeploymentPackage-License";
+  public static String dpLicenseHeader      = "DeploymentPackage-License";
 
   /** corresponds to the "DeploymentPackage-Icon" header */
-  public static String dpIcon                  = "DeploymentPackage-Icon";
+  public static String dpIcon               = "DeploymentPackage-Icon";
 
   /** corresponds to the "DeploymentPackage-Customizer" header */
-  public static String dpCustomizerHeader      = "DeploymentPackage-Customizer: ";
+  public static String dpCustomizerHeader   = "DeploymentPackage-Customizer: ";
 
   /** corresponds to the "Name" header */
-  public static String nameHeader              = "Name: ";
+  public static String nameHeader           = "Name: ";
 
   /** corresponds to the "Bundle-SymbolicName" header */
-  public static String bundleNameHeader        = "Bundle-SymbolicName: ";
+  public static String bundleNameHeader     = "Bundle-SymbolicName: ";
 
   /** corresponds to the "Bundle-Version" header */
-  public static String bundleVersionHeader     = "Bundle-Version: ";
+  public static String bundleVersionHeader  = "Bundle-Version: ";
 
   /** corresponds to the "DeploymentPackage-Missing" header */
-  public static String dpMissingHeader         = "DeploymentPackage-Missing: ";
+  public static String dpMissingHeader      = "DeploymentPackage-Missing: ";
 
   /**
    * corresponds to the "DeploymentPackage-Name" header
-   * 
+   *
    * @since 1.2
    */
-  public static String dpName                  = "DeploymentPackage-Name";
+  public static String dpName               = "DeploymentPackage-Name";
 
   /**
    * corresponds to the "DeploymentPackage-RequiredStorage" header
-   * 
+   *
    * @since 1.2
    */
-  public static String dpRequiredStorage       = "DeploymentPackage-RequiredStorage";
+  public static String dpRequiredStorage    = "DeploymentPackage-RequiredStorage";
 
 }
