4692e1e6b620f77990cd1eb05b46175294737bbf
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-10-05 07:13:42 +0000

    Removed dead code

--- src/plugins/org.tigris.mtoolkit.dpeditor/src/org/tigris/mtoolkit/dpeditor/editor/base/DPPFormSection.java
+++ src/plugins/org.tigris.mtoolkit.dpeditor/src/org/tigris/mtoolkit/dpeditor/editor/base/DPPFormSection.java
@@ -16,7 +16,6 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.PlatformUI;
-import org.tigris.mtoolkit.common.PlatformUtils;
 import org.tigris.mtoolkit.dpeditor.editor.forms.FormSection;
 import org.tigris.mtoolkit.dpeditor.editor.model.IModelChangedEvent;
 import org.tigris.mtoolkit.dpeditor.editor.model.IModelChangedListener;
@@ -28,31 +27,20 @@ import org.tigris.mtoolkit.dpeditor.util.ResourceManager;
  * section and will be added in to the Form. This is the abstract class, that
  * all form section of the deployment package editor will be extend.
  */
-public abstract class DPPFormSection extends FormSection implements
-IModelChangedListener {
-
+public abstract class DPPFormSection extends FormSection implements IModelChangedListener {
   /** Holds the error message */
-  public static String ERROR_MSG = ResourceManager.getString("DPPEditor.Error", "");
+  public static String ERROR_MSG   = ResourceManager.getString("DPPEditor.Error", "");
   /** Holds the warning message */
   public static String WARNING_MSG = ResourceManager.getString("DPPEditor.Warning", "");
-  /**
-   * Ask the user to enter different values in the table, because there are
-   * two equals values in the table
-   */
-  public static final String EQUAL_VALUES_MSG3 = "DPPEditor.FormSection.EqualValuesMsg3";
-  /** Announce that in the table two of the values are equals */
-  public static final String EQUAL_VALUES_MSG5 = "DPPEditor.FormSection.EqualValuesMsg5";
-  /** Message for empty value in the table */
-  public static final String EQUAL_VALUES_MSG6 = "DPPEditor.FormSection.EqualValuesMsg6";
   /** The page in which this FormSection will be placed */
-  private DPPFormPage formPage;
+  private DPPFormPage  formPage;
 
   /**
    * Creates the new instance of the FormSection and the form which is the
    * parent of this section.
-   * 
+   *
    * @param formPage
-   *            the parent form page
+   *          the parent form page
    */
   public DPPFormSection(DPPFormPage formPage) {
     this.formPage = formPage;
@@ -60,7 +48,7 @@ IModelChangedListener {
 
   /**
    * Gets the parent form of this section
-   * 
+   *
    * @return the parent form page
    */
   public DPPFormPage getFormPage() {
@@ -70,10 +58,10 @@ IModelChangedListener {
   /**
    * Called when there is a change in the model this listener is registered
    * with.
-   * 
+   *
    * @param e
-   *            a change event that describes the kind of the model change
-   * 
+   *          a change event that describes the kind of the model change
+   *
    * @see org.tigris.mtoolkit.dpeditor.editor.model.IModelChangedListener#modelChanged(org.tigris.mtoolkit.dpeditor.editor.model.IModelChangedEvent)
    */
   public void modelChanged(IModelChangedEvent e) {
@@ -82,18 +70,17 @@ IModelChangedListener {
   /**
    * Checks if the given value is exists as a value of the
    * <code>TableItem</code>s of the given <code>TableViewer</code> and if
-   * exists, checks is the <code>TableItem</code> which value is the given
-   * value is the same <code>TableItem</code> as a given. The column from
-   * which will be get the value is the first one. In this case returns the
-   * index of the <code>TableItem</code>, otherwise returns -1.
-   * 
+   * exists, checks is the <code>TableItem</code> which value is the given value
+   * is the same <code>TableItem</code> as a given. The column from which will
+   * be get the value is the first one. In this case returns the index of the
+   * <code>TableItem</code>, otherwise returns -1.
+   *
    * @param table
-   *            the TableViewer, which TableItems will be checked
+   *          the TableViewer, which TableItems will be checked
    * @param item
-   *            the TableItem, which will be compare with the viewer's
-   *            TableItems
+   *          the TableItem, which will be compare with the viewer's TableItems
    * @param newValue
-   *            the new value of the given TableItem
+   *          the new value of the given TableItem
    * @return the index of the TableItem in the given TableViewer or -1 if this
    *         TableItem is not exists in the TableViewer
    */
@@ -104,20 +91,18 @@ IModelChangedListener {
   /**
    * Checks if the given value is exists as a value of the
    * <code>TableItem</code>s of the given <code>TableViewer</code> and if
-   * exists, checks is the <code>TableItem</code> which value is the given
-   * value is the same <code>TableItem</code> as a given. In this case returns
-   * the index of the <code>TableItem</code>, otherwise returns -1.
-   * 
+   * exists, checks is the <code>TableItem</code> which value is the given value
+   * is the same <code>TableItem</code> as a given. In this case returns the
+   * index of the <code>TableItem</code>, otherwise returns -1.
+   *
    * @param table
-   *            the TableViewer, which TableItems will be checked
+   *          the TableViewer, which TableItems will be checked
    * @param item
-   *            the TableItem, which will be compare with the viewer's
-   *            TableItems
+   *          the TableItem, which will be compare with the viewer's TableItems
    * @param newValue
-   *            the new value of the given TableItem
+   *          the new value of the given TableItem
    * @param column
-   *            the column index, which text will be compare with the new
-   *            value
+   *          the column index, which text will be compare with the new value
    * @return the index of the TableItem in the given TableViewer or -1 if this
    *         TableItem is not exists in the TableViewer
    */
@@ -136,97 +121,11 @@ IModelChangedListener {
   }
 
   /**
-   * Checks if the given value is exists as a value of the
-   * <code>TableItem</code>s of the given <code>TableViewer</code> and if
-   * exists, checks is the <code>TableItem</code> which value is the given
-   * value is the same <code>TableItem</code> as a given, as checks every
-   * value in the <code>TableItem</code> of the table with the every value in
-   * the given <code>TableItem</code>. In this case returns the index of the
-   * <code>TableItem</code>, otherwise returns -1.
-   * 
-   * @param table
-   *            the TableViewer, which TableItems will be checked
-   * @param item
-   *            the TableItem, which will be compare with the viewer's
-   *            TableItems
-   * @param newValue
-   *            the new value of the given TableItem
-   * @param column
-   *            the column index, which text will be compare with the new
-   *            value
-   * @return the index of the TableItem in the given TableViewer or -1 if this
-   *         TableItem is not exists in the TableViewer
-   */
-  public static int rowExists(TableViewer table, TableItem item, String newValue, int column) {
-    int n = -1;
-    Table parentTable = table.getTable();
-    for (int i = 0; i < parentTable.getItemCount(); i++) {
-      TableItem currentItem = parentTable.getItem(i);
-      if (currentItem.getText(column).equalsIgnoreCase(newValue)) {
-        if (!currentItem.equals(item)) {
-          int columns = parentTable.getColumnCount();
-          for (int j = 0; j < columns; j++) {
-            if (j != column) {
-              if (currentItem.getText(j).equals(item.getText(j))) {
-                return i;
-              }
-            }
-          }
-        }
-      }
-    }
-    return n;
-  }
-
-  /**
-   * Checks if the given value, which is the file path, is exists as a value
-   * of the <code>TableItem</code>s of the given <code>TableViewer</code> and
-   * if exists, checks is the <code>TableItem</code> which value is the given
-   * value is the same <code>TableItem</code> as a given. In this case returns
-   * the index of the <code>TableItem</code>, otherwise returns -1.
-   * 
-   * @param table
-   *            the TableViewer, which TableItems will be checked
-   * @param item
-   *            the TableItem, which will be compare with the viewer's
-   *            TableItems
-   * @param newValue
-   *            the new value of the given TableItem
-   * @param column
-   *            the column index, which text will be compare with the new
-   *            value
-   * @return the index of the TableItem in the given TableViewer or -1 if this
-   *         TableItem is not exists in the TableViewer
-   */
-  public static int fileExists(TableViewer table, TableItem item, String newValue, int column) {
-    int n = -1;
-    Table parentTable = table.getTable();
-    boolean ignoreCase = PlatformUtils.isWindows();
-    for (int i = 0; i < parentTable.getItemCount(); i++) {
-      TableItem currentItem = parentTable.getItem(i);
-      if (ignoreCase) {
-        if (currentItem.getText(column).equalsIgnoreCase(newValue)) {
-          if (!currentItem.equals(item)) {
-            return i;
-          }
-        }
-      } else {
-        if (currentItem.getText(column).equals(newValue)) {
-          if (!currentItem.equals(item)) {
-            return i;
-          }
-        }
-      }
-    }
-    return n;
-  }
-
-  /**
    * Opens the <code>MessageDialog</code>'s standard warning dialog with the
    * given message.
-   * 
+   *
    * @param message
-   *            the message
+   *          the message
    */
   public static void showWarningTableDialog(final String message) {
     Display display = PlatformUI.getWorkbench().getDisplay();
@@ -240,35 +139,16 @@ IModelChangedListener {
   }
 
   /**
-   * Opens the <code>MessageDialog</code>'s standard error dialog with the
-   * given message.
-   * 
+   * Opens the <code>MessageDialog</code>'s standard error dialog with the given
+   * message.
+   *
    * @param message
-   *            the message
+   *          the message
    */
   public static void showErrorTableDialog(final String message) {
     Display display = PlatformUI.getWorkbench().getDisplay();
     if (!display.isDisposed()) {
       display.asyncExec(new Runnable() {
-        public void run() {
-          MessageDialog.openError(DPPErrorHandler.getAnyShell(), ERROR_MSG, message);
-        }
-      });
-    }
-  }
-
-  /**
-   * Opens the <code>MessageDialog</code>'s standard error dialog with the
-   * given message. The thread which calls this method is suspended until the
-   * runnable completes.
-   * 
-   * @param message
-   *            the message
-   */
-  public static void syncShowErrorTableDialog(final String message) {
-    Display display = PlatformUI.getWorkbench().getDisplay();
-    if (!display.isDisposed()) {
-      display.syncExec(new Runnable() {
         public void run() {
           MessageDialog.openError(DPPErrorHandler.getAnyShell(), ERROR_MSG, message);
         }
