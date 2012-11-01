62d2989f68edb3d8877805f6ff80a875095ae56b
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-11-01 07:28:20 +0000

    Added copyrights info

--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/gui/ListItem.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/gui/ListItem.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2012 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
 package org.tigris.mtoolkit.common.gui;
 
 import java.util.HashMap;
@@ -6,35 +16,37 @@ import java.util.Map;
 /**
  * @since 6.0
  */
-public class ListItem {
+public final class ListItem {
+  public Object  element    = null;
+  public boolean checked    = false;
+  protected Map  properties = new HashMap();
 
-	public Object element = null;
-	public boolean checked = false;
-	protected Map properties = new HashMap();
+  /**
+   * Sets the given property.
+   *
+   * @param key
+   * @param value
+   * @return the old value of the property
+   */
+  public Object setProperty(String key, Object value) {
+    return properties.put(key, value);
+  }
 
-	public String toString() {
-		return element != null ? element.toString() : super.toString();
-	}
+  /**
+   * Returns the value of the property for the given key.
+   *
+   * @param key
+   * @return the value of the property
+   */
+  public Object getProperty(String key) {
+    return properties.get(key);
+  }
 
-	/**
-	 * Sets the given property.
-	 * 
-	 * @param key
-	 * @param value
-	 * @return the old value of the property
-	 */
-	public Object setProperty(String key, Object value) {
-		return properties.put(key, value);
-	}
-
-	/**
-	 * Returns the value of the property for the given key.
-	 * 
-	 * @param key
-	 * @return the value of the property
-	 */
-	public Object getProperty(String key) {
-		return properties.get(key);
-	}
-
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return element != null ? element.toString() : super.toString();
+  }
 }
