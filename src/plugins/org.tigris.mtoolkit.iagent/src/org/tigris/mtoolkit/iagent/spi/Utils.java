af1ac27b0ff82ede9f668e530cb7534c86d9edb9
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2012-12-21 13:49:20 +0000

    Minor code updates

--- src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/spi/Utils.java
+++ src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/spi/Utils.java
@@ -18,107 +18,103 @@ import org.tigris.mtoolkit.iagent.pmp.PMPException;
 import org.tigris.mtoolkit.iagent.pmp.RemoteMethod;
 import org.tigris.mtoolkit.iagent.pmp.RemoteObject;
 
-public class Utils {
+public final class Utils {
 
-	/**
-	 * 
-	 * @param remote
-	 * @param methodSignature
-	 * @param parameters
-	 * @return
-	 * @throws IAgentException
-	 * @deprecated Using this method result in too verbose code. Consider using
-	 *             {@link MethodSignature#call(RemoteObject)} or one of its
-	 *             siblings instead.
-	 */
-	public static Object callRemoteMethod(RemoteObject remote, MethodSignature methodSignature, Object[] parameters)
-			throws IAgentException {
-		try {
-			return callRemoteMethod0(remote, parameters, methodSignature);
-		} catch (PMPException e) {
-			info("[callRemoteMethod] Method invocation failed", e);
-			if (remote instanceof PMPRemoteObjectAdapter) {
-				int verificationResult = ((PMPRemoteObjectAdapter) remote).verifyRemoteReference();
-				if (verificationResult == PMPRemoteObjectAdapter.REPEAT) {
-					debug("[callRemoteMethod] Remote reference verification says REPEAT");
-					try {
-						return callRemoteMethod0(remote, parameters, methodSignature);
-					} catch (PMPException e1) {
-						info("[callRemoteMethod] Method invocation failed", e);
-						throw new IAgentException("Unable to call method: " + methodSignature.name,
-								IAgentErrors.ERROR_INTERNAL_ERROR, e1);
-					}
-				}
-			}
-			info("[callRemoteMethod] Method invocation failed", e);
-			throw new IAgentException("Unable to call method: " + methodSignature.name,
-					IAgentErrors.ERROR_INTERNAL_ERROR, e);
-		}
-	}
+  private Utils() {
+  }
 
-	public static boolean isRemoteMethodDefined(RemoteObject remote, MethodSignature methodSignature)
-			throws IAgentException {
-		try {
-			return getRemoteMethod(remote, methodSignature) != null;
-		} catch (PMPException e) {
-			if (remote instanceof PMPRemoteObjectAdapter) {
-				int verificationResult = ((PMPRemoteObjectAdapter) remote).verifyRemoteReference();
-				if (verificationResult == PMPRemoteObjectAdapter.REPEAT) {
-					debug("[isRemoteMethodDefined] Remote reference verification says REPEAT");
-					try {
-						return getRemoteMethod(remote, methodSignature) != null;
-					} catch (PMPException e1) {
-						info("[isRemoteMethodDefined] Failed to get method again", e1);
-						return false;
-					}
-				}
-			}
-		}
-		return false;
-	}
+  /**
+   *
+   * @param remote
+   * @param methodSignature
+   * @param parameters
+   * @return
+   * @throws IAgentException
+   * @deprecated Using this method result in too verbose code. Consider using
+   *             {@link MethodSignature#call(RemoteObject)} or one of its
+   *             siblings instead.
+   */
+  public static Object callRemoteMethod(RemoteObject remote, MethodSignature methodSignature, Object[] parameters)
+      throws IAgentException {
+    try {
+      return callRemoteMethod0(remote, parameters, methodSignature);
+    } catch (PMPException e) {
+      DebugUtils.info(Utils.class, "[callRemoteMethod] Method invocation failed", e);
+      if (remote instanceof PMPRemoteObjectAdapter) {
+        int verificationResult = ((PMPRemoteObjectAdapter) remote).verifyRemoteReference();
+        if (verificationResult == PMPRemoteObjectAdapter.REPEAT) {
+          DebugUtils.debug(Utils.class, "[callRemoteMethod] Remote reference verification says REPEAT");
+          try {
+            return callRemoteMethod0(remote, parameters, methodSignature);
+          } catch (PMPException e1) {
+            DebugUtils.info(Utils.class, "[callRemoteMethod] Method invocation failed", e);
+            throw new IAgentException("Unable to call method: " + methodSignature.name,
+                IAgentErrors.ERROR_INTERNAL_ERROR, e1);
+          }
+        }
+      }
+      DebugUtils.info(Utils.class, "[callRemoteMethod] Method invocation failed", e);
+      throw new IAgentException("Unable to call method: " + methodSignature.name, IAgentErrors.ERROR_INTERNAL_ERROR, e);
+    }
+  }
 
-	private static Object callRemoteMethod0(RemoteObject remote, Object[] parameters, MethodSignature methodSignature)
-			throws PMPException, IAgentException {
-		RemoteMethod method = getRemoteMethod(remote, methodSignature);
-		if (method == null)
-			throw new IAgentException("Method " + methodSignature + " is not defined.",
-					IAgentErrors.ERROR_INTERNAL_ERROR);
-		Object result = method.invoke(parameters, methodSignature.shouldSerialize);
-		if (DebugUtils.DEBUG_ENABLED)
-			debug("[invokeCachedMethod] remote method invocation result: " + result);
-		return result;
-	}
+  public static boolean isRemoteMethodDefined(RemoteObject remote, MethodSignature methodSignature)
+      throws IAgentException {
+    try {
+      return getRemoteMethod(remote, methodSignature) != null;
+    } catch (PMPException e) {
+      if (remote instanceof PMPRemoteObjectAdapter) {
+        int verificationResult = ((PMPRemoteObjectAdapter) remote).verifyRemoteReference();
+        if (verificationResult == PMPRemoteObjectAdapter.REPEAT) {
+          DebugUtils.debug(Utils.class, "[isRemoteMethodDefined] Remote reference verification says REPEAT");
+          try {
+            return getRemoteMethod(remote, methodSignature) != null;
+          } catch (PMPException e1) {
+            DebugUtils.info(Utils.class, "[isRemoteMethodDefined] Failed to get method again", e1);
+            return false;
+          }
+        }
+      }
+    }
+    return false;
+  }
 
-	private static RemoteMethod getRemoteMethod(RemoteObject remote, MethodSignature methodSignature)
-			throws PMPException {
-		synchronized (methodSignature) {
-			if (!validateMethodCache(remote, methodSignature)) {
-				debug("[invokeCachedMethod] Method wasn't found in the cache. Quering remote site...");
-				// we don't want to left behind incosistence cache, if the
-				// getMethod fails
-				methodSignature.cachedMethod = null;
-				methodSignature.cachedObject = remote;
-				methodSignature.cachedMethod = remote.getMethod(methodSignature.name, methodSignature.parameterTypes);
-			}
-			return methodSignature.cachedMethod;
-		}
-	}
-	
-	private static boolean validateMethodCache(RemoteObject object, MethodSignature signature) {
-		if (signature.cachedMethod == null)
-			return false;
-		if (signature.cachedObject != object)
-			return false;
-		if (!(object instanceof PMPRemoteObjectAdapter))
-			return true;
-		return ((PMPRemoteObjectAdapter)object).validateCache(signature.cachedMethod);
-	}
+  private static Object callRemoteMethod0(RemoteObject remote, Object[] parameters, MethodSignature methodSignature)
+      throws PMPException, IAgentException {
+    RemoteMethod method = getRemoteMethod(remote, methodSignature);
+    if (method == null) {
+      throw new IAgentException("Method " + methodSignature + " is not defined.", IAgentErrors.ERROR_INTERNAL_ERROR);
+    }
+    Object result = method.invoke(parameters, methodSignature.shouldSerialize);
+    if (DebugUtils.DEBUG_ENABLED) {
+      DebugUtils.debug(Utils.class, "[invokeCachedMethod] remote method invocation result: " + result);
+    }
+    return result;
+  }
 
-	private static final void debug(String message) {
-		DebugUtils.debug(Utils.class, message);
-	}
+  private static RemoteMethod getRemoteMethod(RemoteObject remote, MethodSignature methodSignature) throws PMPException {
+    synchronized (methodSignature) {
+      if (!validateMethodCache(remote, methodSignature)) {
+        DebugUtils.debug(Utils.class, "[invokeCachedMethod] Method wasn't found in the cache. Quering remote site...");
+        // we don't want to left behind inconsistency cache, if the getMethod fails
+        methodSignature.cachedMethod = null;
+        methodSignature.cachedObject = remote;
+        methodSignature.cachedMethod = remote.getMethod(methodSignature.name, methodSignature.parameterTypes);
+      }
+      return methodSignature.cachedMethod;
+    }
+  }
 
-	private static final void info(String message, Throwable t) {
-		DebugUtils.info(Utils.class, message, t);
-	}
+  private static boolean validateMethodCache(RemoteObject object, MethodSignature signature) {
+    if (signature.cachedMethod == null) {
+      return false;
+    }
+    if (signature.cachedObject != object) {
+      return false;
+    }
+    if (!(object instanceof PMPRemoteObjectAdapter)) {
+      return true;
+    }
+    return ((PMPRemoteObjectAdapter) object).validateCache(signature.cachedMethod);
+  }
 }
