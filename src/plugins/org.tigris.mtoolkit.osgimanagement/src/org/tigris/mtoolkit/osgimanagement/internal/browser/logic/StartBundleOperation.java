042e83e48cec678153cbc8124da812bbf9307c45
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-05-15 13:28:07 +0000

    Added missing copyrights information

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/logic/StartBundleOperation.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/logic/StartBundleOperation.java
@@ -1,6 +1,13 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2005, 2012 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
 package org.tigris.mtoolkit.osgimanagement.internal.browser.logic;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -15,38 +22,48 @@ import org.tigris.mtoolkit.osgimanagement.internal.browser.model.Bundle;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.model.FrameworkImpl;
 import org.tigris.mtoolkit.osgimanagement.internal.preferences.FrameworkPreferencesPage;
 
-public class StartBundleOperation extends RemoteBundleOperation {
-	
-	public StartBundleOperation(String bName, Bundle bundle) {
-		super(Messages.start_bundle + " " + bName, bundle);
-	}
+public final class StartBundleOperation extends RemoteBundleOperation {
 
-	protected IStatus doOperation(IProgressMonitor monitor) throws IAgentException {
-		RemoteBundle rBundle = getBundle().getRemoteBundle();
-		int flags =  FrameworkPreferencesPage.isActivationPolicyEnabled() ? org.osgi.framework.Bundle.START_ACTIVATION_POLICY : 0;
-		boolean forceRefresh = false; 
-		switch (getBundle().getState()) {
-		case org.osgi.framework.Bundle.ACTIVE :
-			forceRefresh = true;
-		case org.osgi.framework.Bundle.STARTING :
-			flags = 0;
-		}
-		rBundle.start(flags);
-		if (forceRefresh) {
-			getBundle().update();
-		}
-		if (rBundle.getState() == org.osgi.framework.Bundle.RESOLVED) {
-			// the bundle failed to start, most probably because its
-			// start level is too high
-			int bundleStartLevel = rBundle.getBundleStartLevel();
-			int fwStartLevel = ((FrameworkImpl) getBundle().findFramework()).getFrameWorkStartLevel();
-			if (fwStartLevel < bundleStartLevel)
-				return Util.newStatus(IStatus.WARNING, Messages.bundle_start_failure, null);
-		}
-		return Status.OK_STATUS;
-	}
+  public StartBundleOperation(String bName, Bundle bundle) {
+    super(Messages.start_bundle + " " + bName, bundle);
+  }
 
-	protected String getMessage(IStatus operationStatus) {
-		return NLS.bind(Messages.bundle_startup_failure, getBundle().toString());
-	}
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.osgimanagement.internal.browser.logic.RemoteBundleOperation#doOperation(org.eclipse.core.runtime.IProgressMonitor)
+   */
+  @Override
+  protected IStatus doOperation(IProgressMonitor monitor) throws IAgentException {
+    RemoteBundle rBundle = getBundle().getRemoteBundle();
+    int flags = FrameworkPreferencesPage.isActivationPolicyEnabled()
+        ? org.osgi.framework.Bundle.START_ACTIVATION_POLICY : 0;
+    boolean forceRefresh = false;
+    switch (getBundle().getState()) {
+    case org.osgi.framework.Bundle.ACTIVE:
+      forceRefresh = true;
+    case org.osgi.framework.Bundle.STARTING:
+      flags = 0;
+    }
+    rBundle.start(flags);
+    if (forceRefresh) {
+      getBundle().update();
+    }
+    if (rBundle.getState() == org.osgi.framework.Bundle.RESOLVED) {
+      // the bundle failed to start, most probably because its
+      // start level is too high
+      int bundleStartLevel = rBundle.getBundleStartLevel();
+      int fwStartLevel = ((FrameworkImpl) getBundle().findFramework()).getFrameWorkStartLevel();
+      if (fwStartLevel < bundleStartLevel) {
+        return Util.newStatus(IStatus.WARNING, Messages.bundle_start_failure, null);
+      }
+    }
+    return Status.OK_STATUS;
+  }
+
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.osgimanagement.internal.browser.logic.RemoteBundleOperation#getMessage(org.eclipse.core.runtime.IStatus)
+   */
+  @Override
+  protected String getMessage(IStatus operationStatus) {
+    return NLS.bind(Messages.bundle_startup_failure, getBundle().toString());
+  }
 }
--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/logic/StopBundleOperation.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/logic/StopBundleOperation.java
@@ -1,6 +1,13 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2005, 2012 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ *******************************************************************************/
 package org.tigris.mtoolkit.osgimanagement.internal.browser.logic;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -11,17 +18,25 @@ import org.tigris.mtoolkit.iagent.IAgentException;
 import org.tigris.mtoolkit.osgimanagement.internal.Messages;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.model.Bundle;
 
-public class StopBundleOperation extends RemoteBundleOperation {
-	public StopBundleOperation(Bundle bundle) {
-		super(Messages.stop_bundle, bundle);
-	}
+public final class StopBundleOperation extends RemoteBundleOperation {
+  public StopBundleOperation(Bundle bundle) {
+    super(Messages.stop_bundle, bundle);
+  }
 
-	protected IStatus doOperation(IProgressMonitor monitor) throws IAgentException {
-		getBundle().getRemoteBundle().stop(0);
-		return Status.OK_STATUS;
-	}
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.osgimanagement.internal.browser.logic.RemoteBundleOperation#doOperation(org.eclipse.core.runtime.IProgressMonitor)
+   */
+  @Override
+  protected IStatus doOperation(IProgressMonitor monitor) throws IAgentException {
+    getBundle().getRemoteBundle().stop(0);
+    return Status.OK_STATUS;
+  }
 
-	protected String getMessage(IStatus operationStatus) {
-		return NLS.bind(Messages.bundle_stop_failure, getBundle().toString());
-	}
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.osgimanagement.internal.browser.logic.RemoteBundleOperation#getMessage(org.eclipse.core.runtime.IStatus)
+   */
+  @Override
+  protected String getMessage(IStatus operationStatus) {
+    return NLS.bind(Messages.bundle_stop_failure, getBundle().toString());
+  }
 }
