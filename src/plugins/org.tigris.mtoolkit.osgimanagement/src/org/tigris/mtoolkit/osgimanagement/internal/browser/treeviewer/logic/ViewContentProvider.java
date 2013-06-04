2931ce8bb101a4be90af5eafaf0a6225da391706
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-06-04 11:56:12 +0000

    Fixed https://devzone.prosyst.bg/jira/browse/TOOLKIT-2459

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/treeviewer/logic/ViewContentProvider.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/treeviewer/logic/ViewContentProvider.java
@@ -16,145 +16,144 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 import org.tigris.mtoolkit.console.ConsoleManager;
-import org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ConstantsDistributor;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeEvent;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeListener;
 import org.tigris.mtoolkit.osgimanagement.internal.browser.model.TreeRoot;
 import org.tigris.mtoolkit.osgimanagement.model.Framework;
 import org.tigris.mtoolkit.osgimanagement.model.Model;
 
-public final class ViewContentProvider implements ITreeContentProvider, ContentChangeListener, ConstantsDistributor {
-	private TreeViewer viewer;
+public final class ViewContentProvider implements ITreeContentProvider, ContentChangeListener {
+  private TreeViewer viewer;
 
-	// Returns the child elements of the given parent element.
+  // Returns the child elements of the given parent element.
   /* (non-Javadoc)
    * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
    */
-	public Object[] getChildren(Object parentElement) {
-		return ((Model) parentElement).getChildren();
-	}
+  public Object[] getChildren(Object parentElement) {
+    return ((Model) parentElement).getChildren();
+  }
 
-	// Returns the parent for the given element, or null indicating that the
-	// parent can't be computed.
+  // Returns the parent for the given element, or null indicating that the
+  // parent can't be computed.
   /* (non-Javadoc)
    * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
    */
-	public Object getParent(Object element) {
-		return ((Model) element).getParent();
-	}
+  public Object getParent(Object element) {
+    return ((Model) element).getParent();
+  }
 
-	// Returns whether the given element has children.
+  // Returns whether the given element has children.
   /* (non-Javadoc)
    * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
    */
-	public boolean hasChildren(Object element) {
-		return ((Model) element).getSize() > 0;
-	}
+  public boolean hasChildren(Object element) {
+    return ((Model) element).getSize() > 0;
+  }
 
-	// Returns the elements to display in the viewer when its input is set to
-	// the given element.
+  // Returns the elements to display in the viewer when its input is set to
+  // the given element.
   /* (non-Javadoc)
    * @see org.eclipse.jface.viewers.ITreeContentProvider#getElements(java.lang.Object)
    */
-	public Object[] getElements(Object inputElement) {
-		return getChildren(inputElement);
-	}
+  public Object[] getElements(Object inputElement) {
+    return getChildren(inputElement);
+  }
 
-	// Disposes of this content provider.
+  // Disposes of this content provider.
   /* (non-Javadoc)
    * @see org.eclipse.jface.viewers.IContentProvider#dispose()
    */
-	public void dispose() {
-	}
+  public void dispose() {
+  }
 
-	// Notifies this content provider that the given viewer's input has been
-	// switched to a different element.
+  // Notifies this content provider that the given viewer's input has been
+  // switched to a different element.
   /* (non-Javadoc)
    * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
    */
-	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		this.viewer = (TreeViewer) viewer;
+  public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+    this.viewer = (TreeViewer) viewer;
 
-		if (oldInput != null) {
-			((TreeRoot) oldInput).removeListener(this);
-		}
+    if (oldInput != null) {
+      ((TreeRoot) oldInput).removeListener(this);
+    }
 
-		if (newInput != null) {
-			((TreeRoot) newInput).addListener(this);
-		}
-	}
+    if (newInput != null) {
+      ((TreeRoot) newInput).addListener(this);
+    }
+  }
 
   /* (non-Javadoc)
    * @see org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeListener#elementAdded(org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeEvent)
    */
-	public void elementAdded(final ContentChangeEvent event) {
-		if (!canUpdate()) {
-			return;
-		}
-		Display display = PlatformUI.getWorkbench().getDisplay();
-		if (display.isDisposed()) {
-			return;
-		}
-		display.asyncExec(new Runnable() {
-			public void run() {
-				if (canUpdate()) {
-					if (event.getTarget().getParent() != null) {
-						viewer.add(event.getTarget().getParent(), event.getTarget());
-					}
-				}
-			}
-		});
-	}
+  public void elementAdded(final ContentChangeEvent event) {
+    if (!canUpdate()) {
+      return;
+    }
+    Display display = PlatformUI.getWorkbench().getDisplay();
+    if (display.isDisposed()) {
+      return;
+    }
+    display.asyncExec(new Runnable() {
+      public void run() {
+        if (canUpdate()) {
+          if (event.getTarget().getParent() != null) {
+            viewer.add(event.getTarget().getParent(), event.getTarget());
+          }
+        }
+      }
+    });
+  }
 
   /* (non-Javadoc)
    * @see org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeListener#elementChanged(org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeEvent)
    */
-	public void elementChanged(final ContentChangeEvent event) {
-		if (!canUpdate()) {
-			return;
-		}
-		Display display = PlatformUI.getWorkbench().getDisplay();
-		if (display.isDisposed()) {
-			return;
-		}
-		display.asyncExec(new Runnable() {
-			public void run() {
-				if (canUpdate()) {
-					if (event.getTarget() instanceof Framework) {
-						Framework fw = (Framework) event.getTarget();
-						ConsoleManager.setName(fw.getConnector(), fw.getName());
-						viewer.refresh();
-					} else {
-						viewer.refresh(event.getTarget());
-					}
-				}
-			}
-		});
-	}
+  public void elementChanged(final ContentChangeEvent event) {
+    if (!canUpdate()) {
+      return;
+    }
+    Display display = PlatformUI.getWorkbench().getDisplay();
+    if (display.isDisposed()) {
+      return;
+    }
+    display.asyncExec(new Runnable() {
+      public void run() {
+        if (canUpdate()) {
+          if (event.getTarget() instanceof Framework) {
+            Framework fw = (Framework) event.getTarget();
+            ConsoleManager.setName(fw.getConnector(), fw.getName());
+            viewer.refresh();
+          } else {
+            viewer.refresh(event.getTarget());
+          }
+        }
+      }
+    });
+  }
 
   /* (non-Javadoc)
    * @see org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeListener#elementRemoved(org.tigris.mtoolkit.osgimanagement.internal.browser.logic.ContentChangeEvent)
    */
-	public void elementRemoved(ContentChangeEvent event) {
-		if (!canUpdate()) {
-			return;
-		}
-		final Model target = event.getTarget();
-		Display display = PlatformUI.getWorkbench().getDisplay();
-		if (display.isDisposed()) {
-			return;
-		}
-		display.asyncExec(new Runnable() {
-			public void run() {
-				if (canUpdate()) {
-					viewer.remove(target);
-				}
-			}
-		});
+  public void elementRemoved(ContentChangeEvent event) {
+    if (!canUpdate()) {
+      return;
+    }
+    final Model target = event.getTarget();
+    Display display = PlatformUI.getWorkbench().getDisplay();
+    if (display.isDisposed()) {
+      return;
+    }
+    display.asyncExec(new Runnable() {
+      public void run() {
+        if (canUpdate()) {
+          viewer.remove(target);
+        }
+      }
+    });
 
-	}
+  }
 
-	private boolean canUpdate() {
-		return viewer != null && !viewer.getControl().isDisposed();
-	}
+  private boolean canUpdate() {
+    return viewer != null && !viewer.getControl().isDisposed() && viewer.getInput() != null;
+  }
 }
