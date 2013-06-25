2f635755576ae08f12d64beaa12971a4bac0c9cf
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-06-25 13:23:51 +0000

    Formatting

--- src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/spi/MethodSignature.java
+++ src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/spi/MethodSignature.java
@@ -17,154 +17,184 @@ import org.tigris.mtoolkit.iagent.internal.utils.DebugUtils;
 import org.tigris.mtoolkit.iagent.pmp.RemoteMethod;
 import org.tigris.mtoolkit.iagent.pmp.RemoteObject;
 
-public class MethodSignature {
+public final class MethodSignature {
+  /**
+   * @since 4.0
+   */
+  public static final String   INPUT_STREAM_TYPE = "java.io.InputStream";
+  /**
+   * @since 4.0
+   */
+  public static final String   STRING_TYPE       = "java.lang.String";
 
-	/**
-	 * @since 4.0
-	 */
-	public static final String INPUT_STREAM_TYPE = "java.io.InputStream";
-	/**
-	 * @since 4.0
-	 */
-	public static final String STRING_TYPE = "java.lang.String";
-	
-	/**
-	 * @since 4.0
-	 */
-	public static final String[] NO_ARGS = new String[0];
-	/**
-	 * @since 4.0
-	 */
-	public static final String[] BID_ARGS = new String[] { "long" };
-	/**
-	 * @since 4.0
-	 */
-	public static final String[] SERVICEID_ARGS = new String[] { "long" };
-	
-	String name;
-	String[] parameterTypes;
-	boolean shouldSerialize;
-	RemoteMethod cachedMethod;
-	RemoteObject cachedObject;
+  /**
+   * @since 4.0
+   */
+  public static final String[] NO_ARGS           = new String[0];
+  /**
+   * @since 4.0
+   */
+  public static final String[] BID_ARGS          = new String[] {
+                                                   "long"
+                                                 };
+  /**
+   * @since 4.0
+   */
+  public static final String[] SERVICEID_ARGS    = new String[] {
+                                                   "long"
+                                                 };
 
-	public MethodSignature(String name) {
-		this(name, new String[0], true);
-	}
+  String                       name;
+  String[]                     parameterTypes;
+  boolean                      shouldSerialize;
+  RemoteMethod                 cachedMethod;
+  RemoteObject                 cachedObject;
 
-	public MethodSignature(String name, String[] parameterNames) {
-		this(name, parameterNames, true);
-	}
-	
-	public MethodSignature(String name, Class[] classes) {
-		this(name, getClassNames(classes));
-	}
-	
-	public MethodSignature(String name, Class parameterType) {
-		this(name, getClassNames(new Class[] { parameterType }));
-	}
-	
-	private static String[] getClassNames(Class[] classes) {
-		if (classes == null)
-			return null;
-		if (classes.length == 0)
-			return new String[0];
-		String[] classNames = new String[classes.length];
-		for (int i = 0; i < classes.length; i++) {
-			classNames[i] = classes[i].getName();
-		}
-		return classNames;
-	}
+  public MethodSignature(String name) {
+    this(name, new String[0], true);
+  }
 
-	public MethodSignature(String name, String[] parameterNames, boolean shouldSerialize) {
-		if (name == null)
-			throw new IllegalArgumentException("name cannot be null");
-		if (parameterNames != null)
-			for (int i = 0; i < parameterNames.length; i++) {
-				if (parameterNames[i] == null)
-					throw new IllegalArgumentException("parameterNames contain null element at index " + i);
-			}
-		this.name = name;
-		this.parameterTypes = parameterNames;
-		if (this.parameterTypes == null)
-			this.parameterTypes = new String[0];
-		this.shouldSerialize = shouldSerialize;
-	}
+  public MethodSignature(String name, String[] parameterNames) {
+    this(name, parameterNames, true);
+  }
 
-	public MethodSignature(RemoteMethod rMethod) {
-		this(rMethod.getName(), rMethod.getArgTypes());
-	}
+  public MethodSignature(String name, Class[] classes) {
+    this(name, getClassNames(classes));
+  }
 
-	public String toString() {
-		return name + "[" + DebugUtils.flattenStringArray(parameterTypes) + "]";
-	}
+  public MethodSignature(String name, Class parameterType) {
+    this(name, getClassNames(new Class[] {
+      parameterType
+    }));
+  }
 
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + MethodSignature.hashCode(parameterTypes);
-		return result;
-	}
+  public MethodSignature(String name, String[] parameterNames, boolean shouldSerialize) {
+    if (name == null) {
+      throw new IllegalArgumentException("name cannot be null");
+    }
+    if (parameterNames != null) {
+      for (int i = 0; i < parameterNames.length; i++) {
+        if (parameterNames[i] == null) {
+          throw new IllegalArgumentException("parameterNames contain null element at index " + i);
+        }
+      }
+    }
+    this.name = name;
+    this.parameterTypes = parameterNames;
+    if (this.parameterTypes == null) {
+      this.parameterTypes = new String[0];
+    }
+    this.shouldSerialize = shouldSerialize;
+  }
 
-	private static int hashCode(Object[] array) {
-		int prime = 31;
-		if (array == null)
-			return 0;
-		int result = 1;
-		for (int index = 0; index < array.length; index++) {
-			result = prime * result + (array[index] == null ? 0 : array[index].hashCode());
-		}
-		return result;
-	}
+  public MethodSignature(RemoteMethod rMethod) {
+    this(rMethod.getName(), rMethod.getArgTypes());
+  }
 
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MethodSignature other = (MethodSignature) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (!Arrays.equals(parameterTypes, other.parameterTypes))
-			return false;
-		return true;
-	}
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  public String toString() {
+    return name + "[" + DebugUtils.flattenStringArray(parameterTypes) + "]";
+  }
 
-	public String getName() {
-		return name;
-	}
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + MethodSignature.hashCode(parameterTypes);
+    return result;
+  }
 
-	public String[] getParameterTypes() {
-		return parameterTypes;
-	}
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    MethodSignature other = (MethodSignature) obj;
+    if (name == null) {
+      if (other.name != null) {
+        return false;
+      }
+    } else if (!name.equals(other.name)) {
+      return false;
+    }
+    if (!Arrays.equals(parameterTypes, other.parameterTypes)) {
+      return false;
+    }
+    return true;
+  }
 
-	public Object call(RemoteObject obj) throws IAgentException {
-		return call(obj, new Object[0]);
-	}
+  public String getName() {
+    return name;
+  }
 
-	public Object call(RemoteObject obj, Object parameter) throws IAgentException {
-		return call(obj, new Object[] { parameter });
-	}
+  public String[] getParameterTypes() {
+    return parameterTypes;
+  }
 
-	public Object call(RemoteObject obj, Object[] parameters) throws IAgentException {
-		if (parameters == null)
-			throw new IllegalArgumentException(
-					"parameters array cannot be null, it must be empty if no args are passed");
-		if (parameterTypes.length != parameters.length)
-			throw new IllegalArgumentException("method signature expects " + parameterTypes.length + " arguments, but "
-					+ parameters.length + " arguments are provided.");
-		return Utils.callRemoteMethod(obj, this, parameters);
-	}
-	
-	public boolean isDefined(RemoteObject obj) throws IAgentException {
-		if (obj == null)
-			throw new IllegalArgumentException("Remote object cannot be null");
-		return Utils.isRemoteMethodDefined(obj, this);
-	}
+  public Object call(RemoteObject obj) throws IAgentException {
+    return call(obj, new Object[0]);
+  }
 
+  public Object call(RemoteObject obj, Object parameter) throws IAgentException {
+    return call(obj, new Object[] {
+      parameter
+    });
+  }
+
+  public Object call(RemoteObject obj, Object[] parameters) throws IAgentException {
+    if (parameters == null) {
+      throw new IllegalArgumentException("parameters array cannot be null, it must be empty if no args are passed");
+    }
+    if (parameterTypes.length != parameters.length) {
+      throw new IllegalArgumentException("method signature expects " + parameterTypes.length + " arguments, but "
+          + parameters.length + " arguments are provided.");
+    }
+    return Utils.callRemoteMethod(obj, this, parameters);
+  }
+
+  public boolean isDefined(RemoteObject obj) throws IAgentException {
+    if (obj == null) {
+      throw new IllegalArgumentException("Remote object cannot be null");
+    }
+    return Utils.isRemoteMethodDefined(obj, this);
+  }
+
+  private static int hashCode(Object[] array) {
+    int prime = 31;
+    if (array == null) {
+      return 0;
+    }
+    int result = 1;
+    for (int index = 0; index < array.length; index++) {
+      result = prime * result + (array[index] == null ? 0 : array[index].hashCode());
+    }
+    return result;
+  }
+
+  private static String[] getClassNames(Class[] classes) {
+    if (classes == null) {
+      return null;
+    }
+    if (classes.length == 0) {
+      return new String[0];
+    }
+    String[] classNames = new String[classes.length];
+    for (int i = 0; i < classes.length; i++) {
+      classNames[i] = classes[i].getName();
+    }
+    return classNames;
+  }
 }
