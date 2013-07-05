f29b73993bcc3b23c54b8ffb9b69913b552b8a7d
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-07-05 08:34:33 +0000

    Minor code updates

--- src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/treeviewer/action/ShowFrameworkConsole.java
+++ src/plugins/org.tigris.mtoolkit.osgimanagement/src/org/tigris/mtoolkit/osgimanagement/internal/browser/treeviewer/action/ShowFrameworkConsole.java
@@ -18,42 +18,49 @@ import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.ui.actions.SelectionProviderAction;
 import org.tigris.mtoolkit.console.ConsoleManager;
 import org.tigris.mtoolkit.osgimanagement.IStateAction;
-import org.tigris.mtoolkit.osgimanagement.internal.browser.model.FrameworkImpl;
+import org.tigris.mtoolkit.osgimanagement.model.Framework;
 import org.tigris.mtoolkit.osgimanagement.model.Model;
 
-public class ShowFrameworkConsole extends SelectionProviderAction implements IStateAction {
-
-  private TreeViewer tree;
+public final class ShowFrameworkConsole extends SelectionProviderAction implements IStateAction {
+  private final TreeViewer tree;
 
   public ShowFrameworkConsole(ISelectionProvider provider, String label, TreeViewer tree) {
     super(provider, label);
     this.tree = tree;
   }
 
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.action.Action#run()
+   */
   @Override
   public void run() {
     IStructuredSelection selection = (IStructuredSelection) tree.getSelection();
     if (selection.isEmpty()) {
       return; // don't execute for empty selection
     }
-    final FrameworkImpl fw = (FrameworkImpl) ((Model) selection.getFirstElement()).findFramework();
+    final Framework fw = ((Model) selection.getFirstElement()).findFramework();
     if (fw == null) {
       return; // don't execute when fw has been disconnected
     }
     ConsoleManager.showConsole(fw.getConnector(), fw.getName());
   }
 
-  // override to react properly to selection change
+  /* (non-Javadoc)
+   * @see org.eclipse.ui.actions.SelectionProviderAction#selectionChanged(org.eclipse.jface.viewers.IStructuredSelection)
+   */
   @Override
   public void selectionChanged(IStructuredSelection selection) {
     updateState(selection);
   }
 
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.osgimanagement.IStateAction#updateState(org.eclipse.jface.viewers.IStructuredSelection)
+   */
   public void updateState(IStructuredSelection selection) {
     boolean enabled = false;
-    FrameworkImpl fw = null;
+    Framework fw = null;
     for (Iterator it = selection.iterator(); it.hasNext();) {
-      FrameworkImpl next = (FrameworkImpl) ((Model) it.next()).findFramework();
+      Framework next = ((Model) it.next()).findFramework();
       if (fw == null) {
         // we found a framework, enable console
         fw = next;
@@ -64,7 +71,6 @@ public class ShowFrameworkConsole extends SelectionProviderAction implements ISt
         break;
       }
     }
-    // TODO: Disable the button when the framework is disconnected
-    setEnabled(enabled && fw != null && fw.isConnected());// && !fw.autoConnected);
+    setEnabled(enabled && fw != null && fw.isConnected());
   }
 }
