42990d8b440f38a36ca176c3b6fb6d40f72adcdb
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-07-16 07:54:00 +0000

    Removed unneeded comments

--- src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/pmp/PMPConnection.java
+++ src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/pmp/PMPConnection.java
@@ -14,100 +14,93 @@ package org.tigris.mtoolkit.iagent.pmp;
  * Contains the methods for getting references to the services registered in the
  * remote Framework.
  */
-// TODO: Rename this class to PMPSession
 public interface PMPConnection {
+  public static final String FRAMEWORK_DISCONNECTED = "framework_disconnected_event";
 
-	public static final String FRAMEWORK_DISCONNECTED = "framework_disconnected_event";
+  /**
+   * Gets reference to a service registered in the Framework.
+   *
+   * @param clazz
+   *          Specifies the interface under which the service was regisered.
+   * @param filter
+   *          Specifies the search filter (exactly as
+   *          <code>BundleContext.getService</code> filter)
+   * @return a reference to the service.
+   * @exception PMPException
+   *              If an IOException or protocol error occured, if the user does
+   *              not have access rigths for this service, or if there was no
+   *              such service registered in the Framework.
+   * @see RemoteObject
+   */
 
-	/**
-	 * Gets reference to a service registered in the Framework.
-	 * 
-	 * @param clazz
-	 *            Specifies the interface under which the service was regisered.
-	 * @param filter
-	 *            Specifies the search filter (exactly as
-	 *            <code>BundleContext.getService</code> filter)
-	 * @return a reference to the service.
-	 * @exception PMPException
-	 *                If an IOException or protocol error occured, if the user
-	 *                does not have access rigths for this service, or if there
-	 *                was no such service registered in the Framework.
-	 * @see RemoteObject
-	 */
+  public RemoteObject getReference(String clazz, String filter) throws PMPException;
 
-	public RemoteObject getReference(String clazz, String filter) throws PMPException;
+  /**
+   * Gets reference to a service registered in the Framework.
+   *
+   * @param clazz
+   *          Specifies the interface under which the service was regisered.
+   * @param filter
+   *          Specifies the search filter (exactly as
+   *          <code>BundleContext getService</code> filter)
+   * @param bid
+   *          The id of the bundle that registered the service.
+   * @return a reference to the service.
+   * @exception PMPException
+   *              If an IOException or protocol error occured, if the user does
+   *              not have access rigths for this service, or if there was no
+   *              such service registered in the Framework.
+   * @see RemoteObject
+   */
+  public RemoteObject getReference(String clazz, String filter, long bid) throws PMPException;
 
-	/**
-	 * Gets reference to a service registered in the Framework.
-	 * 
-	 * @param clazz
-	 *            Specifies the interface under which the service was regisered.
-	 * @param filter
-	 *            Specifies the search filter (exactly as
-	 *            <code>BundleContext getService</code> filter)
-	 * @param bid
-	 *            The id of the bundle that registered the service.
-	 * @return a reference to the service.
-	 * @exception PMPException
-	 *                If an IOException or protocol error occured, if the user
-	 *                does not have access rigths for this service, or if there
-	 *                was no such service registered in the Framework.
-	 * @see RemoteObject
-	 */
-	public RemoteObject getReference(String clazz, String filter, long bid) throws PMPException;
+  /**
+   * Disconnects this PMPConnection from the remote Framework
+   *
+   * @param message
+   *          The dissconnection message
+   */
+  public void disconnect(String message);
 
-	/**
-	 * Disconnects this PMPConnection from the remote Framework
-	 * 
-	 * @param message
-	 *            The dissconnection message
-	 */
+  /**
+   * Checks whenever this this connection is bind
+   *
+   * @return remote pmpservice uri
+   */
+  public boolean isConnected();
 
-	public void disconnect(String message);
+  /**
+   * Gets the identification of this session
+   *
+   * @return SessionID associated with this session
+   */
+  public String getSessionID();
 
-	/**
-	 * Checks whenever this this connection is bind
-	 * 
-	 * @return remote pmpservice uri
-	 */
+  /**
+   * Registers an EventListener
+   *
+   * @param el
+   *          the EventListener
+   * @param eventTypes
+   *          Array with elements all types of the events which this listener
+   *          wants to receive. Custom event types are in the fallowing format
+   *          EventListener.CUSTOM_EVENT=custom_event_type;
+   *
+   * @exception IllegalArgumentException
+   */
+  public void addEventListener(EventListener el, String[] eventTypes);
 
-	public boolean isConnected();
-
-	/**
-	 * Gets the identification of this session
-	 * 
-	 * @return SessionID associated with this session
-	 */
-
-	public String getSessionID();
-
-	/**
-	 * Registers an EventListener
-	 * 
-	 * @param el
-	 *            the EventListener
-	 * @param eventTypes
-	 *            Array with elements all types of the events which this
-	 *            listener wants to receive. Custom event types are in the
-	 *            fallowing format EventListener.CUSTOM_EVENT=custom_event_type;
-	 * 
-	 * @exception IllegalArgumentException
-	 */
-
-	public void addEventListener(EventListener el, String[] eventTypes);
-
-	/**
-	 * Unregisters an EventListener
-	 * 
-	 * @param el
-	 *            the EventListener
-	 * @param eventTypes
-	 *            Array with elements all types of the events which this
-	 *            listener wants to receive. Custom event types are in the
-	 *            fallowing format EventListener.CUSTOM_EVENT=custom_event_type;
-	 * 
-	 * @exception IllegalArgumentException
-	 */
-	public void removeEventListener(EventListener el, String[] eventTypes);
-
+  /**
+   * Unregisters an EventListener
+   *
+   * @param el
+   *          the EventListener
+   * @param eventTypes
+   *          Array with elements all types of the events which this listener
+   *          wants to receive. Custom event types are in the fallowing format
+   *          EventListener.CUSTOM_EVENT=custom_event_type;
+   *
+   * @exception IllegalArgumentException
+   */
+  public void removeEventListener(EventListener el, String[] eventTypes);
 }
