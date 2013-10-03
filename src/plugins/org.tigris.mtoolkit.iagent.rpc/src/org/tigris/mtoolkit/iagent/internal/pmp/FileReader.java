dbc8075e7a46364307597323e7264c28189714b0
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-10-03 08:52:42 +0000

    Fixed -never use public finalizers

--- src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/internal/pmp/FileReader.java
+++ src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/internal/pmp/FileReader.java
@@ -18,96 +18,108 @@ import java.io.InputStream;
  * the PMPInputStream until the reading is finished.
  */
 public final class FileReader extends InputStream {
+  private short          length = 0;
+  private boolean        locked = true;
+  private PMPInputStream is;
 
-	private short length = 0;
-	private boolean locked = true;
-	private PMPInputStream is;
+  /**
+   * Constructs a FileReader. the length parameter shows the length of the data
+   * that must be read from this InputStream before the PMPInputStream is
+   * unlocked.
+   */
+  public FileReader(PMPInputStream is) {
+    this.is = is;
+    is.lock();
+    checkEOF();
+  }
 
-	/**
-	 * Constructs a FileReader. the length parameter shows the length of the
-	 * data that must be read from this InputStream before the PMPInputStream is
-	 * unlocked.
-	 */
-	public FileReader(PMPInputStream is) {
-		this.is = is;
-		is.lock();
-		checkEOF();
-	}
+  /**
+   * Reads a single byte. If all the data is read unlocks the PMPInputStream.
+   */
+  /* (non-Javadoc)
+   * @see java.io.InputStream#read()
+   */
+  public synchronized int read() throws IOException {
+    if (locked) {
+      if (length > 0) {
+        try {
+          int read = is.read();
+          length--;
+          return read;
+        } catch (IOException ioExc) {
+          close();
+          throw ioExc;
+        }
+      }
+      checkEOF();
+      return read();
+    }
+    return -1;
+  }
 
-	/**
-	 * Reads a single byte. If all the data is read unlocks the PMPInputStream.
-	 */
-	public synchronized int read() throws IOException {
-		if (locked) {
-			if (length > 0)
-				try {
-					int read = is.read();
-					length--;
-					return read;
-				} catch (IOException ioExc) {
-					close();
-					throw ioExc;
-				}
-			checkEOF();
-			return read();
-		}
-		return -1;
-	}
+  /**
+   * Reads into a byte[]. If all the data is read unlocks the PMPInputStream.
+   */
+  /* (non-Javadoc)
+   * @see java.io.InputStream#read(byte[], int, int)
+   */
+  public synchronized int read(byte[] buff, int off, int len) throws IOException {
+    if (locked) {
+      if (length > 0) {
+        try {
+          int read = is.read(buff, off, length >= len ? len : length);
+          length -= read;
+          return read;
+        } catch (IOException ioExc) {
+          close();
+          throw ioExc;
+        }
+      }
+      checkEOF();
+      return read(buff, off, len);
+    }
+    return -1;
+  }
 
-	/**
-	 * Reads into a byte[]. If all the data is read unlocks the PMPInputStream.
-	 */
-	public synchronized int read(byte[] buff, int off, int len) throws IOException {
-		if (locked) {
-			if (length > 0)
-				try {
-					int read = is.read(buff, off, length >= len ? len : length);
-					length -= read;
-					return read;
-				} catch (IOException ioExc) {
-					close();
-					throw ioExc;
-				}
-			checkEOF();
-			return read(buff, off, len);
-		}
-		return -1;
-	}
+  /**
+   * Unlocks the PMPInputStream. If not all the data was read, skips it first.
+   */
+  /* (non-Javadoc)
+   * @see java.io.InputStream#close()
+   */
+  public synchronized void close() {
+    if (locked) {
+      locked = false;
+      if (length > 0) {
+        try {
+          is.skip(length);
+        } catch (IOException ioExc) {
+        }
+      }
+      is.unlock();
+    }
+  }
 
-	private void checkEOF() {
-		if (length == 0) {
-			try {
-				length = PMPData.readShort(is);
-				if (length == -1)
-					close();
-			} catch (IOException ioExc) {
-				close();
-			}
-		}
-	}
+  /**
+   * Added to ensure that the PMPInputStream will be unlocked.
+   */
+  /* (non-Javadoc)
+   * @see java.lang.Object#finalize()
+   */
+  protected void finalize() {
+    close();
+  }
 
-	/**
-	 * Unlocks the PMPInputStream. If not all the data was read, skips it first.
-	 */
-	public synchronized void close() {
-		if (locked) {
-			locked = false;
-			if (length > 0)
-				try {
-					is.skip(length);
-				} catch (IOException ioExc) {
-				}
-			is.unlock();
-		}
-	}
-
-	/**
-	 * Added to ensure that the PMPInputStream will be unlocked.
-	 */
-	public void finalize() {
-		try {
-			close();
-		} catch (Exception exc) {
-		}
-	}
+  private void checkEOF() {
+    if (length == 0) {
+      try {
+        length = PMPData.readShort(is);
+        if (length == -1) {
+          close();
+        }
+      } catch (IOException ioExc) {
+        close();
+      }
+    }
+  }
 }
