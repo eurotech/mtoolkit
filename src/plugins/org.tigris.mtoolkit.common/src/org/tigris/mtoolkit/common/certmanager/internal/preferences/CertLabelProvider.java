2cd14a7cdbd889b0993612fb8f4e595067b6b17f
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-10-07 07:08:11 +0000

    Minor code updates

--- src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/certmanager/internal/preferences/CertLabelProvider.java
+++ src/plugins/org.tigris.mtoolkit.common/src/org/tigris/mtoolkit/common/certmanager/internal/preferences/CertLabelProvider.java
@@ -21,18 +21,24 @@ import org.eclipse.swt.graphics.Image;
 import org.tigris.mtoolkit.common.certificates.ICertificateDescriptor;
 import org.tigris.mtoolkit.common.images.UIResources;
 
-public class CertLabelProvider extends LabelProvider implements ITableLabelProvider {
+public final class CertLabelProvider extends LabelProvider implements ITableLabelProvider {
   private Image iconCertMissing;
 
   public CertLabelProvider() {
     super();
-    Image iconCert = UIResources.getImage(UIResources.CERTIFICATE_ICON);
+    final Image iconCert = UIResources.getImage(UIResources.CERTIFICATE_ICON);
     ImageDescriptor overlay = UIResources.getImageDescriptor(UIResources.OVR_ERROR_ICON);
     iconCertMissing = new DecorationOverlayIcon(iconCert, overlay, IDecoration.BOTTOM_LEFT).createImage();
   }
 
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnImage(java.lang.Object, int)
+   */
   public Image getColumnImage(Object element, int columnIndex) {
-    if (!(element instanceof ICertificateDescriptor) || columnIndex > 0) {
+    if (columnIndex > 0) {
+      return null;
+    }
+    if (!(element instanceof ICertificateDescriptor)) {
       return null;
     }
     ICertificateDescriptor cert = (ICertificateDescriptor) element;
@@ -43,23 +49,27 @@ public class CertLabelProvider extends LabelProvider implements ITableLabelProvi
     return UIResources.getImage(UIResources.CERTIFICATE_ICON);
   }
 
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnText(java.lang.Object, int)
+   */
   public String getColumnText(Object element, int columnIndex) {
     if (!(element instanceof ICertificateDescriptor)) {
       return null;
     }
     ICertificateDescriptor cert = (ICertificateDescriptor) element;
-    String columnText = null;
     switch (columnIndex) {
     case 0:
-      columnText = cert.getAlias();
-      break;
+      return cert.getAlias();
     case 1:
-      columnText = cert.getStoreLocation();
-      break;
+      return cert.getStoreLocation();
+    default:
+      return null;
     }
-    return columnText;
   }
 
+  /* (non-Javadoc)
+   * @see org.eclipse.jface.viewers.BaseLabelProvider#dispose()
+   */
   @Override
   public void dispose() {
     iconCertMissing.dispose();
