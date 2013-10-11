ed857a171e2bd074216ce383f3048816847939e8
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-10-11 08:35:38 +0000

    Minor code updates

--- src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/event/RemoteApplicationEvent.java
+++ src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/event/RemoteApplicationEvent.java
@@ -12,72 +12,80 @@ package org.tigris.mtoolkit.iagent.event;
 
 import org.tigris.mtoolkit.iagent.RemoteApplication;
 
-public class RemoteApplicationEvent extends RemoteEvent {
+public final class RemoteApplicationEvent extends RemoteEvent {
+  /**
+   * Constant indicating that an application has been installed.
+   */
+  public static final int         INSTALLED   = 1 << 0;
 
-	/**
-	 * Constant indicating that an application has been installed.
-	 */
-	public static final int INSTALLED = 1 << 0;
+  /**
+   * Constant indicating that an application has been started.
+   */
+  public static final int         STARTED     = 1 << 2;
 
-	/**
-	 * Constant indicating that an application has been started.
-	 */
-	public static final int STARTED = 1 << 2;
+  /**
+   * Constant indicating that an application has been stopped.
+   */
+  public static final int         STOPPED     = 1 << 3;
 
-	/**
-	 * Constant indicating that an application has been stopped.
-	 */
-	public static final int STOPPED = 1 << 3;
+  /**
+   * Constant indicating that an application has been uninstalled.
+   */
+  public static final int         UNINSTALLED = 1 << 4;
 
-	/**
-	 * Constant indicating that an application has been uninstalled.
-	 */
-	public static final int UNINSTALLED = 1 << 4;
+  private final RemoteApplication remoteApplication;
 
-	private RemoteApplication remoteApplication;
+  /**
+   * Constructs a new remote application event object.
+   *
+   * @param application
+   *          - the application that has been changed.
+   * @param type
+   *          -the type of Event {@link RemoteApplicationEvent#INSTALLED},
+   *          {@link RemoteApplicationEvent#UNINSTALLED},
+   *          {@link RemoteApplicationEvent#STARTED},
+   *          {@link RemoteApplicationEvent#STOPPED}
+   */
+  public RemoteApplicationEvent(RemoteApplication application, int type) {
+    super(type);
+    this.remoteApplication = application;
+  }
 
-	/**
-	 * Constructs a new remote application event object.
-	 * 
-	 * @param application
-	 *            - the application that has been changed.
-	 * @param type
-	 *            -the type of Event {@link RemoteApplicationEvent#INSTALLED},
-	 *            {@link RemoteApplicationEvent#UNINSTALLED},
-	 *            {@link RemoteApplicationEvent#STARTED},
-	 *            {@link RemoteApplicationEvent#STOPPED}
-	 */
-	public RemoteApplicationEvent(RemoteApplication application, int type) {
-		super(type);
-		this.remoteApplication = application;
-	}
+  /**
+   * Returns a reference to the application which has been changed.
+   *
+   * @return a {@link RemoteApplication} object associated with this event
+   */
+  public RemoteApplication getApplication() {
+    return remoteApplication;
+  }
 
-	/**
-	 * Returns a reference to the application which has been changed.
-	 * 
-	 * @return a {@link RemoteApplication} object associated with this event
-	 */
-	public RemoteApplication getApplication() {
-		return remoteApplication;
-	}
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  public String toString() {
+    return "RemoteBundleEvent[bundle=" + remoteApplication + ";type=" + convertType(getType()) + "]";
+  }
 
-	public String toString() {
-		return "RemoteBundleEvent[bundle=" + remoteApplication + ";type="
-				+ convertType(getType()) + "]";
-	}
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  public int hashCode() {
+    return super.hashCode();
+  }
 
-	private String convertType(int type) {
-		switch (type) {
-		case INSTALLED:
-			return "INSTALLED";
-		case STARTED:
-			return "STARTED";
-		case STOPPED:
-			return "STOPPED";
-		case UNINSTALLED:
-			return "UNINSTALLED";
-		default:
-			return "UNKNOWN(" + type + ")";
-		}
-	}
+  private String convertType(int type) {
+    switch (type) {
+    case INSTALLED:
+      return "INSTALLED";
+    case STARTED:
+      return "STARTED";
+    case STOPPED:
+      return "STOPPED";
+    case UNINSTALLED:
+      return "UNINSTALLED";
+    default:
+      return "UNKNOWN(" + type + ")";
+    }
+  }
 }
