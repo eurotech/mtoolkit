b6093544dbf6aed29aa33390671e14fc6bb26cf5
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-10-16 14:59:04 +0000

    Minor code updates

--- src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/Error.java
+++ src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/Error.java
@@ -19,91 +19,113 @@ import java.io.OutputStream;
 
 import org.tigris.mtoolkit.iagent.rpc.Externalizable;
 
-public class Error implements Externalizable {
+public final class Error implements Externalizable {
+  /**
+   * This code is only valid with {@link #BUNDLE_EXCEPTION_TYPE} and means that
+   * the bundle has been previously uninstalled
+   */
+  public static final int   BUNDLE_UNINSTALLED_CODE     = -10000;
 
-	/**
-	 * This code is only valid with {@link #BUNDLE_EXCEPTION_TYPE} and means
-	 * that the bundle has been previously uninstalled
-	 */
-	public static final int BUNDLE_UNINSTALLED_CODE = -10000;
+  /**
+   * This code is only valid when type has value
+   * {@link #DEPLOYMENT_EXCEPTION_TYPE} and means that DeploymentPackage has
+   * been uninstalled or updated with different version
+   */
+  public static final int   DEPLOYMENT_UNINSTALLED_CODE = -10001;
 
-	/**
-	 * This code is only valid when type has value
-	 * {@link #DEPLOYMENT_EXCEPTION_TYPE} and means that DeploymentPackage has
-	 * been uninstalled or updated with different version
-	 */
-	public static final int DEPLOYMENT_UNINSTALLED_CODE = -10001;
+  private static final byte CODE_PRESENT                = 0x02;
+  private static final byte MESSAGE_PRESENT             = 0x04;
+  private static final byte DETAILS_PRESENT             = 0x01;
 
-	private static final byte CODE_PRESENT = 0x02;
-	private static final byte MESSAGE_PRESENT = 0x04;
-	private static final byte DETAILS_PRESENT = 0x01;
+  private int               code;
+  private String            message;
+  private String            details;
 
-	private int code;
-	private String message;
-	private String details;
+  public Error(int code, String message, String details) {
+    this.code = code;
+    this.message = message;
+    this.details = details;
+  }
 
-	public Error(int code, String message, String details) {
-		this.code = code;
-		this.message = message;
-		this.details = details;
-	}
+  public Error(int code, String message) {
+    this(code, message, null);
+  }
 
-	public Error(int code, String message) {
-		this(code, message, null);
-	}
+  public Error(int code) {
+    this(code, null, null);
+  }
 
-	public Error(int code) {
-		this(code, null, null);
-	}
+  public Error() {
+  }
 
-	public Error() {
-	}
+  public int getCode() {
+    return code;
+  }
 
-	public int getCode() {
-		return code;
-	}
+  public String getMessage() {
+    return message;
+  }
 
-	public String getMessage() {
-		return message;
-	}
+  public String getDetails() {
+    return details;
+  }
 
-	public String getDetails() {
-		return details;
-	}
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.iagent.rpc.Externalizable#readObject(java.io.InputStream)
+   */
+  public void readObject(InputStream is) throws Exception {
+    final DataInput in = new DataInputStream(is);
+    byte objectHeader = in.readByte();
 
-	public void readObject(InputStream is) throws Exception {
-		DataInput in = new DataInputStream(is);
-		byte objectHeader = in.readByte();
+    if ((objectHeader & CODE_PRESENT) != 0) {
+      code = in.readInt();
+    }
+    if ((objectHeader & MESSAGE_PRESENT) != 0) {
+      message = in.readUTF();
+    }
+    if ((objectHeader & DETAILS_PRESENT) != 0) {
+      details = in.readUTF();
+    }
+  }
 
-		if ((objectHeader & CODE_PRESENT) != 0)
-			code = in.readInt();
-		if ((objectHeader & MESSAGE_PRESENT) != 0)
-			message = in.readUTF();
-		if ((objectHeader & DETAILS_PRESENT) != 0)
-			details = in.readUTF();
-	}
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.iagent.rpc.Externalizable#writeObject(java.io.OutputStream)
+   */
+  public void writeObject(OutputStream os) throws Exception {
+    final DataOutput out = new DataOutputStream(os);
+    byte objectHeader = 0;
+    if (code != 0) {
+      objectHeader |= CODE_PRESENT;
+    }
+    if (message != null && message.length() > 0) {
+      objectHeader |= MESSAGE_PRESENT;
+    }
+    if (details != null && details.length() > 0) {
+      objectHeader |= DETAILS_PRESENT;
+    }
+    out.writeByte(objectHeader);
+    if (code != 0) {
+      out.writeInt(code);
+    }
+    if (message != null && message.length() > 0) {
+      out.writeUTF(message);
+    }
+    if (details != null && details.length() > 0) {
+      out.writeUTF(details);
+    }
+  }
 
-	public void writeObject(OutputStream os) throws Exception {
-		DataOutput out = new DataOutputStream(os);
+  /* (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  public String toString() {
+    return "Error[code=" + code + ";message=" + message + ";details=" + details + "]";
+  }
 
-		byte objectHeader = 0;
-		if (code != 0)
-			objectHeader |= CODE_PRESENT;
-		if (message != null && message.length() > 0)
-			objectHeader |= MESSAGE_PRESENT;
-		if (details != null && details.length() > 0)
-			objectHeader |= DETAILS_PRESENT;
-		out.writeByte(objectHeader);
-
-		if (code != 0)
-			out.writeInt(code);
-		if (message != null && message.length() > 0)
-			out.writeUTF(message);
-		if (details != null && details.length() > 0)
-			out.writeUTF(details);
-	}
-
-	public String toString() {
-		return "Error[code=" + code + ";message=" + message + ";details=" + details + "]";
-	}
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  public int hashCode() {
+    return super.hashCode();
+  }
 }
