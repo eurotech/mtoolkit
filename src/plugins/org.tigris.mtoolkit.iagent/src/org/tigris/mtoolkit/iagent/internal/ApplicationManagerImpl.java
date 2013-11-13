0f8a11ab53b7594f1ac02d3708aa9413bb8cdffb
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2013-11-13 08:33:43 +0000

    Added common IAgent manager model

--- src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/internal/ApplicationManagerImpl.java
+++ src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/internal/ApplicationManagerImpl.java
@@ -11,8 +11,7 @@
 package org.tigris.mtoolkit.iagent.internal;
 
 import java.util.Dictionary;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.EventListener;
 import java.util.Map;
 
 import org.tigris.mtoolkit.iagent.ApplicationManager;
@@ -21,21 +20,12 @@ import org.tigris.mtoolkit.iagent.IAgentException;
 import org.tigris.mtoolkit.iagent.RemoteApplication;
 import org.tigris.mtoolkit.iagent.event.RemoteApplicationEvent;
 import org.tigris.mtoolkit.iagent.event.RemoteApplicationListener;
-import org.tigris.mtoolkit.iagent.pmp.EventListener;
-import org.tigris.mtoolkit.iagent.pmp.RemoteObject;
-import org.tigris.mtoolkit.iagent.spi.ConnectionEvent;
-import org.tigris.mtoolkit.iagent.spi.ConnectionListener;
-import org.tigris.mtoolkit.iagent.spi.ConnectionManager;
-import org.tigris.mtoolkit.iagent.spi.DeviceConnectorSpi;
-import org.tigris.mtoolkit.iagent.spi.IAgentManager;
+import org.tigris.mtoolkit.iagent.spi.AbstractManager;
 import org.tigris.mtoolkit.iagent.spi.MethodSignature;
 import org.tigris.mtoolkit.iagent.spi.PMPConnection;
 import org.tigris.mtoolkit.iagent.util.DebugUtils;
 
-public final class ApplicationManagerImpl implements ApplicationManager, IAgentManager, EventListener,
-    ConnectionListener {
-
-  private static final String   SYNCH_APPLICATION_EVENT  = "synch_application_event";
+public final class ApplicationManagerImpl extends AbstractManager implements ApplicationManager {
   private static final String   EVENT_APPLICATION_ID_KEY = "application_id";
   private static final String   EVENT_TYPE_KEY           = "type";
 
@@ -48,38 +38,11 @@ public final class ApplicationManagerImpl implements ApplicationManager, IAgentM
   private final MethodSignature GET_STATE                = new MethodSignature("getState", String.class);
   private final MethodSignature GET_PROPERTIES           = new MethodSignature("getProperties", String.class);
 
-  private DeviceConnectorSpi    connectorSpi;
-  private final List            applicationListeners     = new LinkedList();
-
-  /* (non-Javadoc)
-   * @see org.tigris.mtoolkit.iagent.spi.IAgentManager#dispose()
+  /**
+   * @param remoteAdmin
    */
-  public void dispose() {
-    try {
-      synchronized (applicationListeners) {
-        applicationListeners.clear();
-        PMPConnection connection = (PMPConnection) connectorSpi.getConnectionManager().getActiveConnection(
-            ConnectionManager.PMP_CONNECTION);
-        if (connection != null) {
-          DebugUtils
-              .debug(this,
-                  "[removeListeners] PMP connection is available, remove event listener for synchronous application events...");
-          connection.removeEventListener(this, new String[] {
-            SYNCH_APPLICATION_EVENT
-          });
-        }
-        DebugUtils.debug(this, "[removeListeners] application listeners removed");
-      }
-    } catch (IAgentException e) {
-      DebugUtils.error(this, "[dispose] Exception while disposing application manager", e);
-    }
-  }
-
-  /* (non-Javadoc)
-   * @see org.tigris.mtoolkit.iagent.spi.IAgentManager#init(org.tigris.mtoolkit.iagent.spi.DeviceConnectorSpi)
-   */
-  public void init(DeviceConnectorSpi connector) {
-    this.connectorSpi = connector;
+  public ApplicationManagerImpl() {
+    super(PMPConnection.REMOTE_APPLICATION_ADMIN_NAME, "synch_application_event");
   }
 
   /* (non-Javadoc)
@@ -87,22 +50,15 @@ public final class ApplicationManagerImpl implements ApplicationManager, IAgentM
    */
   public void addRemoteApplicationListener(RemoteApplicationListener listener) throws IAgentException {
     DebugUtils.debug(this, "[addRemoteApplicaionListener] >>> listener: " + listener);
-    connectorSpi.getConnectionManager().addConnectionListener(this);
-    synchronized (applicationListeners) {
-      if (!applicationListeners.contains(listener)) {
-        PMPConnection connection = (PMPConnection) connectorSpi.getConnectionManager().getActiveConnection(
-            ConnectionManager.PMP_CONNECTION);
-        if (connection != null) {
-          DebugUtils.debug(this, "[addRemoteApplicationListener] PMP connection is available, add event listener");
-          connection.addEventListener(this, new String[] {
-            SYNCH_APPLICATION_EVENT
-          });
-        }
-        applicationListeners.add(listener);
-      } else {
-        DebugUtils.debug(this, "[addRemoteApplicationListener] Listener already present");
-      }
-    }
+    addRemoteEventListener(listener);
+  }
+
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.iagent.ApplicationManager#removeRemoteApplicationListener(org.tigris.mtoolkit.iagent.event.RemoteApplicationListener)
+   */
+  public void removeRemoteApplicationListener(RemoteApplicationListener listener) throws IAgentException {
+    DebugUtils.debug(this, "[removeApplicationListener] >>> listener: " + listener);
+    removeRemoteEventListener(listener);
   }
 
   /* (non-Javadoc)
@@ -110,7 +66,7 @@ public final class ApplicationManagerImpl implements ApplicationManager, IAgentM
    */
   public RemoteApplication[] listApplications() throws IAgentException {
     DebugUtils.debug(this, "[listApplications] >>>");
-    String[] applicationIDs = (String[]) GET_APPLICATIONS.call(getApplicationAdmin());
+    String[] applicationIDs = (String[]) GET_APPLICATIONS.call(getRemoteAdmin());
     DebugUtils.debug(this,
         "[listApplications] Returned applications list: " + DebugUtils.convertForDebug(applicationIDs));
     int appsLength = (applicationIDs != null) ? applicationIDs.length : 0;
@@ -123,76 +79,17 @@ public final class ApplicationManagerImpl implements ApplicationManager, IAgentM
     return applications;
   }
 
-  public void removeRemoteApplicationListener(RemoteApplicationListener listener) throws IAgentException {
-    DebugUtils.debug(this, "[removeApplicationListener] >>> listener: " + listener);
-    synchronized (applicationListeners) {
-      if (applicationListeners.contains(listener)) {
-        applicationListeners.remove(listener);
-        if (applicationListeners.size() == 0) {
-          DebugUtils.debug(this,
-              "[removeRemoteApplicationListener] No more listeners in the list, try to remove PMP event listener");
-          PMPConnection connection = (PMPConnection) connectorSpi.getConnectionManager().getActiveConnection(
-              ConnectionManager.PMP_CONNECTION);
-          if (connection != null) {
-            DebugUtils.debug(this,
-                "[removeRemoteApplicationListener] PMP connection is available, remove event listener");
-            connection.removeEventListener(this, new String[] {
-              SYNCH_APPLICATION_EVENT
-            });
-          }
-        }
-      } else {
-        DebugUtils.debug(this, "[removeRemoteApplicationListener] Listener not found in the list");
-      }
-    }
-  }
-
   /* (non-Javadoc)
-   * @see org.tigris.mtoolkit.iagent.pmp.EventListener#event(java.lang.Object, java.lang.String)
+   * @see org.tigris.mtoolkit.iagent.spi.AbstractManager#remoteEvent(java.util.Dictionary)
    */
-  public void event(Object event, String eventType) {
-    try {
-      DebugUtils.debug(this, "[event] >>> event: " + event + "; type: " + eventType);
-      if (SYNCH_APPLICATION_EVENT.equals(eventType)) {
-        Dictionary eventProps = (Dictionary) event;
-        if (eventProps == null) {
-          return;
-        }
-        int type = ((Integer) eventProps.get(EVENT_TYPE_KEY)).intValue();
-        String applicationId = (String) eventProps.get(EVENT_APPLICATION_ID_KEY);
-        fireApplicationEvent(applicationId, type);
-      }
-    } catch (Throwable e) {
-      DebugUtils.error(this, "[event] Failed to process PMP event: " + event + "; type: " + eventType, e);
-    }
+  protected void remoteEvent(Dictionary eventProps) {
+    int type = ((Integer) eventProps.get(EVENT_TYPE_KEY)).intValue();
+    String applicationId = (String) eventProps.get(EVENT_APPLICATION_ID_KEY);
+    fireApplicationEvent(applicationId, type);
   }
 
-  /* (non-Javadoc)
-   * @see org.tigris.mtoolkit.iagent.spi.ConnectionListener#connectionChanged(org.tigris.mtoolkit.iagent.spi.ConnectionEvent)
-   */
-  public void connectionChanged(ConnectionEvent event) {
-    DebugUtils.debug(this, "[connectionChanged] >>> event: " + event);
-    if (event.getType() == ConnectionEvent.CONNECTED
-        && event.getConnection().getType() == ConnectionManager.PMP_CONNECTION) {
-      DebugUtils.debug(this, "[connectionChanged] New PMP connection created, restore event listeners");
-      synchronized (applicationListeners) {
-        PMPConnection connection = (PMPConnection) event.getConnection();
-        if (applicationListeners.size() > 0) {
-          DebugUtils.debug(this, "[connectionChanged] Restoring application listeners...");
-          try {
-            connection.addEventListener(this, new String[] {
-              SYNCH_APPLICATION_EVENT
-            });
-          } catch (IAgentException e) {
-            DebugUtils.error(this, "[connectionChanged] Failed to add event listener to PMP connection", e);
-          }
-        }
-      }
-    }
-  }
-
-  public void startApplication(String applicationID, Map properties) throws IAgentException {
-    Object result = START.call(getApplicationAdmin(), new Object[] {
+  void startApplication(String applicationID, Map properties) throws IAgentException {
+    Object result = START.call(getRemoteAdmin(), new Object[] {
         applicationID, properties
     });
     if (result instanceof Error) {
@@ -202,8 +99,8 @@ public final class ApplicationManagerImpl implements ApplicationManager, IAgentM
     }
   }
 
-  public void stopApplication(String applicationID) throws IAgentException {
-    Object result = STOP.call(getApplicationAdmin(), applicationID);
+  void stopApplication(String applicationID) throws IAgentException {
+    Object result = STOP.call(getRemoteAdmin(), applicationID);
     if (result instanceof Error) {
       Error err = (Error) result;
       throw new IAgentException("Failed to stop application"
@@ -211,41 +108,29 @@ public final class ApplicationManagerImpl implements ApplicationManager, IAgentM
     }
   }
 
-  public String getApplicationState(String applicationID) throws IAgentException {
-    return (String) GET_STATE.call(getApplicationAdmin(), applicationID);
+  String getApplicationState(String applicationID) throws IAgentException {
+    return (String) GET_STATE.call(getRemoteAdmin(), applicationID);
   }
 
-  public Map getApplicationProperties(String applicationId) throws IAgentException {
-    Object result = GET_PROPERTIES.call(getApplicationAdmin(), applicationId);
+  Map getApplicationProperties(String applicationId) throws IAgentException {
+    Object result = GET_PROPERTIES.call(getRemoteAdmin(), applicationId);
     if (result instanceof Error) {
       throw new IAgentException((Error) result);
     }
     return (Map) result;
   }
 
-  private RemoteObject getApplicationAdmin() throws IAgentException {
-    PMPConnection connection = (PMPConnection) connectorSpi.getConnectionManager().createConnection(
-        ConnectionManager.PMP_CONNECTION);
-    RemoteObject applicationAdmin = connection.getRemoteApplicationAdmin();
-    return applicationAdmin;
-  }
-
   private void fireApplicationEvent(String applicationID, int type) {
     DebugUtils.debug(this, "[fireApplicationEvent] >>> applicationId: " + applicationID + "; type: " + type);
-    RemoteApplicationListener[] listeners;
-    synchronized (applicationListeners) {
-      if (applicationListeners.size() != 0) {
-        listeners = (RemoteApplicationListener[]) applicationListeners
-            .toArray(new RemoteApplicationListener[applicationListeners.size()]);
-      } else {
-        return;
-      }
+    EventListener[] listeners = getEventListeners();
+    if (listeners == null) {
+      return;
     }
     RemoteApplication application = new RemoteApplicationImpl(this, applicationID);
     RemoteApplicationEvent event = new RemoteApplicationEvent(application, type);
     DebugUtils.debug(this, "[fireApplicationEvent] " + listeners.length + " listeners found.");
     for (int i = 0; i < listeners.length; i++) {
-      RemoteApplicationListener listener = listeners[i];
+      RemoteApplicationListener listener = (RemoteApplicationListener) listeners[i];
       try {
         DebugUtils.debug(this, "[fireApplicationEvent] deliver event: " + event + " to listener: " + listener);
         listener.applicationChanged(event);
---
+++ src/plugins/org.tigris.mtoolkit.iagent/src/org/tigris/mtoolkit/iagent/spi/AbstractManager.java
@@ -0,0 +1,192 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2013 ProSyst Software GmbH and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ProSyst Software GmbH - initial API and implementation
+ ****************************************************************************/
+package org.tigris.mtoolkit.iagent.spi;
+
+import java.util.Dictionary;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.tigris.mtoolkit.iagent.IAgentErrors;
+import org.tigris.mtoolkit.iagent.IAgentException;
+import org.tigris.mtoolkit.iagent.pmp.EventListener;
+import org.tigris.mtoolkit.iagent.pmp.RemoteObject;
+import org.tigris.mtoolkit.iagent.util.DebugUtils;
+
+public abstract class AbstractManager implements IAgentManager, ConnectionListener, EventListener {
+  private final String         remoteAdminClass;
+
+  protected final String       remoteEventType;
+
+  protected DeviceConnectorSpi connectorSpi;
+
+  protected final List         remoteEventListeners = new LinkedList();
+
+  public AbstractManager(String remoteAdmin) {
+    this(remoteAdmin, null);
+  }
+
+  public AbstractManager(String remoteAdminClass, String remoteEventType) {
+    this.remoteAdminClass = remoteAdminClass;
+    this.remoteEventType = remoteEventType;
+  }
+
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.iagent.spi.IAgentManager#init(org.tigris.mtoolkit.iagent.spi.DeviceConnectorSpi)
+   */
+  public void init(DeviceConnectorSpi connector) {
+    if (connector == null) {
+      throw new IllegalArgumentException();
+    }
+    this.connectorSpi = connector;
+  }
+
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.iagent.spi.IAgentManager#dispose()
+   */
+  public void dispose() {
+    try {
+      synchronized (remoteEventListeners) {
+        remoteEventListeners.clear();
+      }
+      if (remoteEventType != null) {
+        final PMPConnection connection = getPMPConnection();
+        if (connection != null) {
+          DebugUtils
+              .debug(this,
+                  "[removeListeners] PMP connection is available, remove event listener for synchronous application events...");
+          connection.removeEventListener(this, new String[] {
+            remoteEventType
+          });
+        }
+        DebugUtils.debug(this, "[removeListeners] application listeners removed");
+      }
+    } catch (IAgentException e) {
+      DebugUtils.error(this, "[dispose] Exception while disposing application manager", e);
+    }
+  }
+
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.iagent.spi.ConnectionListener#connectionChanged(org.tigris.mtoolkit.iagent.spi.ConnectionEvent)
+   */
+  public void connectionChanged(ConnectionEvent event) {
+    if (remoteEventType == null) {
+      return;
+    }
+    DebugUtils.debug(this, "[connectionChanged] >>> event: " + event);
+    if (event.getType() == ConnectionEvent.CONNECTED) {
+      if (event.getConnection().getType() == ConnectionManager.PMP_CONNECTION) {
+        DebugUtils.debug(this, "[connectionChanged] New PMP connection created, restore event listeners");
+        synchronized (remoteEventListeners) {
+          PMPConnection connection = (PMPConnection) event.getConnection();
+          if (!remoteEventListeners.isEmpty()) {
+            DebugUtils.debug(this, "[connectionChanged] Restoring event listeners...");
+            try {
+              connection.addEventListener(this, new String[] {
+                remoteEventType
+              });
+            } catch (IAgentException e) {
+              DebugUtils.error(this, "[connectionChanged] Failed to add event listener to PMP connection", e);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.iagent.pmp.EventListener#event(java.lang.Object, java.lang.String)
+   */
+  public void event(Object event, String eventType) {
+    if (remoteEventType == null) {
+      return;
+    }
+    DebugUtils.debug(this, "[event] >>> event: " + event + "; type: " + eventType);
+    if (remoteEventType.equals(eventType)) {
+      Dictionary eventProps = (Dictionary) event;
+      if (eventProps == null) {
+        return;
+      }
+      remoteEvent(eventProps);
+    }
+  }
+
+  public RemoteObject getRemoteAdmin() throws IAgentException {
+    DebugUtils.debug(this, "[getRemoteAdmin] >>>", null);
+    final PMPConnection pmpConnection = getPMPConnection();
+    if (pmpConnection == null || !pmpConnection.isConnected()) {
+      DebugUtils.debug(this, "[getRemoteAdmin] The connecton has been closed!", null);
+      throw new IAgentException("The connecton has been closed!", IAgentErrors.ERROR_DISCONNECTED);
+    }
+    RemoteObject remoteConfigAdmin = pmpConnection.getRemoteAdmin(remoteAdminClass);
+    return remoteConfigAdmin;
+  }
+
+  protected void remoteEvent(Dictionary eventProps) {
+  }
+
+  protected void addRemoteEventListener(java.util.EventListener listener) throws IAgentException {
+    getConnectionManager().addConnectionListener(this);
+    synchronized (remoteEventListeners) {
+      if (!remoteEventListeners.contains(listener)) {
+        final PMPConnection connection = getPMPConnection();
+        if (connection != null) {
+          DebugUtils.debug(this, "[addRemoteEventListener] PMP connection is available, add event listener");
+          connection.addEventListener(this, new String[] {
+            remoteEventType
+          });
+        }
+        remoteEventListeners.add(listener);
+      } else {
+        DebugUtils.debug(this, "[addRemoteEventListener] Listener already present");
+      }
+    }
+  }
+
+  protected void removeRemoteEventListener(java.util.EventListener listener) throws IAgentException {
+    DebugUtils.debug(this, "[removeRemoteEventListener] >>> listener: " + listener);
+    synchronized (remoteEventListeners) {
+      if (remoteEventListeners.contains(listener)) {
+        remoteEventListeners.remove(listener);
+        if (remoteEventListeners.size() == 0) {
+          DebugUtils.debug(this,
+              "[removeRemoteEventListener] No more listeners in the list, try to remove PMP event listener");
+          final PMPConnection connection = getPMPConnection();
+          if (connection != null) {
+            DebugUtils.debug(this, "[removeRemoteEventListener] PMP connection is available, remove event listener");
+            connection.removeEventListener(this, new String[] {
+              remoteEventType
+            });
+          }
+        }
+      } else {
+        DebugUtils.debug(this, "[removeRemoteApplicationListener] Listener not found in the list");
+      }
+    }
+  }
+
+  protected java.util.EventListener[] getEventListeners() {
+    synchronized (remoteEventListeners) {
+      if (!remoteEventListeners.isEmpty()) {
+        return (java.util.EventListener[]) remoteEventListeners
+            .toArray(new java.util.EventListener[remoteEventListeners.size()]);
+      }
+    }
+    return null;
+  }
+
+  protected PMPConnection getPMPConnection() throws IAgentException {
+    return (PMPConnection) connectorSpi.getConnectionManager().getActiveConnection(ConnectionManager.PMP_CONNECTION);
+  }
+
+  protected ConnectionManager getConnectionManager() {
+    return connectorSpi.getConnectionManager();
+  }
+}
