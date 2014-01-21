4d8262f8ac7477c51edd2f1e07cfc5f6a35f1125
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2014-01-21 10:24:04 +0000

    Minor abstraction fixes

--- src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/internal/pmp/RemoteInputStream.java
+++ src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/internal/pmp/RemoteInputStream.java
@@ -15,100 +15,111 @@ import java.io.InputStream;
 
 import org.tigris.mtoolkit.iagent.rpc.Remote;
 
-public class RemoteInputStream implements Remote {
+public final class RemoteInputStream implements Remote {
+  private static final int     BUF_SIZE = 2048;
 
-	private static final int BUF_SIZE = 2048;
+  private static final Class[] remote   = {
+                                          RemoteInputStream.class
+                                        };
 
-	private InputStream is;
-	private byte[] buff = new byte[BUF_SIZE];
-	private int off = 0;
-	private int len = 0;
-	private boolean eof = false;
+  private int                  off      = 0;
+  private int                  len      = 0;
+  private boolean              eof      = false;
+  private final byte[]         buff     = new byte[BUF_SIZE];
 
-	public RemoteInputStream(InputStream is) {
-		this.is = is;
-	}
+  private InputStream          is;
 
-	public synchronized String readln() throws IOException {
-		return readln(null);
-	}
+  public RemoteInputStream(InputStream is) {
+    this.is = is;
+  }
 
-	private String readln(StringBuffer sbuffer) throws IOException {
-		if (len == off) {
-			if (eof)
-				return sbuffer == null ? null : sbuffer.toString();
-			fill();
-			return readln(sbuffer);
-		}
-		boolean two = false;
-		int i = off;
-		for (; i < len; i++) {
-			if (buff[i] == '\n')
-				break;
-			if (buff[i] == '\r') {
-				if (buff[i + 1] == '\n')
-					two = true;
-				break;
-			}
-		}
-		if (sbuffer == null)
-			sbuffer = new StringBuffer(new String(buff, off, i - off));
-		else
-			sbuffer.append(new String(buff, off, i - off));
-		if (i == BUF_SIZE) {
-			fill();
-			return readln(sbuffer);
-		}
-		off = two ? i + 2 : i + 1;
-		return sbuffer.toString();
-	}
+  /* (non-Javadoc)
+   * @see org.tigris.mtoolkit.iagent.rpc.Remote#remoteInterfaces()
+   */
+  public Class[] remoteInterfaces() {
+    return remote;
+  }
 
-	public synchronized byte[] read(int size) throws IOException {
-		if (len == off) {
-			if (eof)
-				return null;
-			fill();
-			return read(size);
-		}
-		int s = size > len - off ? len - off : size;
-		byte[] toReturn = new byte[s];
-		System.arraycopy(buff, off, toReturn, 0, s);
-		off += s;
-		return toReturn;
-	}
+  public synchronized String readln() throws IOException {
+    return readln(null);
+  }
 
-	private void fill() throws IOException {
-		off = len = 0;
-		do {
-			int read = 0;
-			try {
-				read = is.read(buff, len, BUF_SIZE - len);
-			} catch (IOException ioExc) {
-				close();
-				throw ioExc;
-			}
-			if (read == -1)
-				eof = true;
-			else {
-				len += read;
-				if (len == BUF_SIZE)
-					break;
-			}
-		} while (!eof);
-	}
+  public synchronized void close() {
+    try {
+      is.close();
+    } catch (IOException ignore) {
+    }
+    off = len = 0;
+    eof = true;
+  }
 
-	public synchronized void close() {
-		try {
-			is.close();
-		} catch (IOException ignore) {
-		}
-		off = len = 0;
-		eof = true;
-	}
+  private String readln(StringBuffer sbuffer) throws IOException {
+    if (len == off) {
+      if (eof) {
+        return sbuffer == null ? null : sbuffer.toString();
+      }
+      fill();
+      return readln(sbuffer);
+    }
+    boolean two = false;
+    int i = off;
+    for (; i < len; i++) {
+      if (buff[i] == '\n') {
+        break;
+      }
+      if (buff[i] == '\r') {
+        if (buff[i + 1] == '\n') {
+          two = true;
+        }
+        break;
+      }
+    }
+    if (sbuffer == null) {
+      sbuffer = new StringBuffer(new String(buff, off, i - off));
+    } else {
+      sbuffer.append(new String(buff, off, i - off));
+    }
+    if (i == BUF_SIZE) {
+      fill();
+      return readln(sbuffer);
+    }
+    off = two ? i + 2 : i + 1;
+    return sbuffer.toString();
+  }
 
-	public static Class[] remote = { RemoteInputStream.class };
+  public synchronized byte[] read(int size) throws IOException {
+    if (len == off) {
+      if (eof) {
+        return null;
+      }
+      fill();
+      return read(size);
+    }
+    int s = size > len - off ? len - off : size;
+    byte[] toReturn = new byte[s];
+    System.arraycopy(buff, off, toReturn, 0, s);
+    off += s;
+    return toReturn;
+  }
 
-	public Class[] remoteInterfaces() {
-		return remote;
-	}
+  private void fill() throws IOException {
+    off = len = 0;
+    do {
+      int read = 0;
+      try {
+        read = is.read(buff, len, BUF_SIZE - len);
+      } catch (IOException ioExc) {
+        close();
+        throw ioExc;
+      }
+      if (read == -1) {
+        eof = true;
+      } else {
+        len += read;
+        if (len == BUF_SIZE) {
+          break;
+        }
+      }
+    } while (!eof);
+  }
 }
