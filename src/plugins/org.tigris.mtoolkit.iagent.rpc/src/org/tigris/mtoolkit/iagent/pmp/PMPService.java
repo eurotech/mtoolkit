96d048fce9c0fa8f8d1826daf71dea462276fdb9
h_bojilov <h_bojilov@8c0469b3-3b32-4850-87dc-fad729056409>
2014-01-24 15:08:16 +0000

    Done initial phase of strings externalization

--- src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/pmp/PMPService.java
+++ src/plugins/org.tigris.mtoolkit.iagent.rpc/src/org/tigris/mtoolkit/iagent/pmp/PMPService.java
@@ -16,31 +16,28 @@ import org.tigris.mtoolkit.iagent.transport.Transport;
 
 /**
  * Interface of the PMP Service
+ *
  * @noimplement This interface is not intended to be implemented by clients.
  */
-
 public interface PMPService extends PMPPeer {
+  /**
+   * @since 3.0
+   */
+  public static final String PROP_PMP_PORT = "pmp-port"; //$NON-NLS-1$
 
-	/**
-	 * @since 3.0
-	 */
-	public static final String PROP_PMP_PORT = "pmp-port";
-	
-	/**
-	 * Tries to establish a connection to remote PMP Service.
-	 * 
-	 * @param uri
-	 *            of the remote PMPService host uri is in the following format
-	 *            transportprotocol://host:port
-	 * @param login
-	 *            the login certificates
-	 * @return PMPConnection
-	 * @exception PMPException
-	 *                If an IOException, protocol or authentication error
-	 *                occured.
-	 * @since 3.0
-	 */
+  /**
+   * Tries to establish a connection to remote PMP Service.
+   *
+   * @param uri
+   *          of the remote PMPService host uri is in the following format
+   *          protocol://host:port
+   * @param login
+   *          the login certificates
+   * @return PMPConnection
+   * @exception PMPException
+   *              If an IOException, protocol or authentication error occured.
+   * @since 3.0
+   */
 
-	public PMPConnection connect(Transport transport, Dictionary props) throws PMPException;
-
+  public PMPConnection connect(Transport transport, Dictionary props) throws PMPException;
 }
